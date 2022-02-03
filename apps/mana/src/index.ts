8301ad84d8e5dc961f0afe1f07b5802bcf132d89
bonustrack <fabien@bonustrack.co>
2022-02-03 17:42:16 +0700

    Add main route

--- apps/mana/src/index.ts
+++ apps/mana/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import rpc from './rpc';
+import { version } from '../package.json';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,5 +11,6 @@ app.use(bodyParser.json({ limit: '4mb' }));
 app.use(bodyParser.urlencoded({ limit: '4mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
 app.use('/', rpc);
+app.get('/', (req, res) => res.json({ version, port: PORT }));
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
