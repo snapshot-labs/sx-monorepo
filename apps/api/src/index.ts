258936a99451c97a26edd3fc249e206ba015a73f
bonustrack <fabien@bonustrack.co>
2022-03-10 15:38:29 +0700

    Fix path gql file

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -9,11 +9,12 @@ import Checkpoint from './checkpoint';
 import config from './config.json';
 import * as writer from './writer';
 
-const schemaFile = path.join(__dirname, './schema.gql');
+const file = __dirname.endsWith('build/src') ? '../../src/schema.gql' : '../src/schema.gql';
+const schemaFile = path.join(__dirname, file);
 const schema = fs.readFileSync(schemaFile, 'utf8');
 
 const checkpoint = new Checkpoint(config, writer, schema);
-checkpoint.reset();
+// checkpoint.reset();
 checkpoint.start();
 
 const app = express();
--- apps/api/src/writer.ts
+++ apps/api/src/writer.ts
@@ -1,23 +1,5 @@
-import { BigNumber } from '@ethersproject/bignumber';
-import { getAddress } from '@ethersproject/address';
 import mysql from './checkpoint/mysql';
-
-export function feltToString(events) {
-  if (!events || events.length < 1 || !events[0].data || events[0].data.length < 8) return '';
-  const str = events[0].data
-    .slice(5, 7)
-    .map(str => BigInt(str).toString(16))
-    .join('');
-  return `0x${str}`;
-}
-
-function toAddress(bn) {
-  try {
-    return getAddress(BigNumber.from(bn).toHexString());
-  } catch (e) {
-    return bn;
-  }
-}
+import { toAddress } from './utils';
 
 export async function handleProposalCreated({ block, tx, receipt }) {
   console.log('handleProposalCreated', receipt.events);
