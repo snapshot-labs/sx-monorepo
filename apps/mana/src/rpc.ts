9b65098326601b87219357a51026fbc9667241d5
bonustrack <fabien@bonustrack.co>
2022-03-11 03:03:02 +0700

    Relay for proposal and vote

--- apps/mana/src/rpc.ts
+++ apps/mana/src/rpc.ts
@@ -1,37 +1,28 @@
 import express from 'express';
-import { defaultProvider as provider, stark } from 'starknet';
-import constants from './constants.json';
+import { rpcError, rpcSuccess } from './utils';
+import { propose, vote } from '../sx.js/src';
 
 const router = express.Router();
-const { getSelectorFromName } = stark;
 
-router.post('/', async (req, res) => {
-  const { method, params, id } = req.body;
+router.all('/', async (req, res) => {
+  const { id } = req.body;
   try {
-    console.time('addTransaction');
-    const receipt = await provider.invokeFunction(
-      constants.votingContract,
-      getSelectorFromName('propose'),
-      ['123', '456']
-    );
+    const { types } = req.body.params.envelop;
+    let receipt;
+    console.time('Send');
+    if (types.Proposal) {
+      const space = '0x06d6de57282e6798ab5a1fa56686b65fc0282b1c4b95b86df85f4a44ee5dc8ae';
+      receipt = await propose(space);
+    }
+    if (types.Vote) {
+      const space = '0x06d6de57282e6798ab5a1fa56686b65fc0282b1c4b95b86df85f4a44ee5dc8ae';
+      receipt = await vote(space);
+    }
+    console.timeEnd('Send');
     console.log('Receipt', receipt);
-    console.timeEnd('addTransaction');
-    return res.json({
-      jsonrpc: '2.0',
-      result: receipt,
-      id
-    });
+    return rpcSuccess(res, receipt, id);
   } catch (e) {
-    const code = 500;
-    return res.status(code).json({
-      jsonrpc: '2.0',
-      error: {
-        code,
-        message: 'unauthorized',
-        data: e
-      },
-      id: null
-    });
+    return rpcError(res, 500, e, id);
   }
 });
 
