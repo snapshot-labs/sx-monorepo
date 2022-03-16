c94c1965c4cbdec7f87d99fe86af70e60580d08c
bonustrack <fabien@bonustrack.co>
2022-03-17 03:05:26 +0700

    Add correct address for actions

--- apps/mana/src/rpc.ts
+++ apps/mana/src/rpc.ts
@@ -7,12 +7,13 @@ const router = express.Router();
 router.post('/', async (req, res) => {
   const { id } = req.body;
   try {
+    const { address } = req.body.params.envelop;
     const { types, message } = req.body.params.envelop.data;
     let receipt;
     console.time('Send');
     if (types.Propose)
-      receipt = await propose(message.space, message.executionHash, message.metadataURI);
-    if (types.Vote) receipt = await vote(message.space, message.proposal, message.choice);
+      receipt = await propose(address, message.space, message.executionHash, message.metadataURI);
+    if (types.Vote) receipt = await vote(address, message.space, message.proposal, message.choice);
     console.timeEnd('Send');
     console.log('Receipt', receipt);
     return rpcSuccess(res, receipt, id);
