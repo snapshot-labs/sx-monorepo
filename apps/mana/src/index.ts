d678bf4f846bc14387052510f81dd2688f59466a
bonustrack <fabien@bonustrack.co>
2022-09-23 19:35:44 +0700

    Expose latest commit hash

--- apps/mana/src/index.ts
+++ apps/mana/src/index.ts
@@ -3,10 +3,12 @@ import express from 'express';
 import cors from 'cors';
 import rpc from './rpc';
 import fossil from './fossil';
-import { version } from '../package.json';
+import pkg from '../package.json';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const commit = process.env.COMMIT_HASH || '';
+const version = commit ? `${pkg.version}#${commit.substr(0, 7)}` : pkg.version;
 
 app.use(express.json({ limit: '4mb' }));
 app.use(express.urlencoded({ limit: '4mb', extended: false }));
