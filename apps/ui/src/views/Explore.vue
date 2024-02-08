0e933fb1d553a4228b72f613c2b6dede7a7601c8
Sam <samuv.eth@gmail.com>
2024-02-08 14:25:44 +0700

    refactor: rename SpaceCard to SpaceListItem

--- apps/ui/src/views/Explore.vue
+++ apps/ui/src/views/Explore.vue
@@ -14,7 +14,7 @@ watchEffect(() => setTitle('Explore'));
       <UiLoading v-if="!spacesStore.loaded" class="block mb-2" />
       <div v-if="spacesStore.loaded" class="max-w-screen-md">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-3">
-          <SpaceCard v-for="space in spacesStore.spaces" :key="space.id" :space="space" />
+          <SpaceListItem v-for="space in spacesStore.spaces" :key="space.id" :space="space" />
         </div>
       </div>
     </UiContainer>
