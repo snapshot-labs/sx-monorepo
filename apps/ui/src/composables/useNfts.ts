7daff8ed8fa081307b41ac17a5529e9f4102126d
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2024-02-13 10:12:19 +0100

    fix: migrate to OpenSea API v2 (#11)
    * fix: migrate to OpenSea API v2
    
    OpenSea v1 API was shutdown in December.
    
    * refactor: use production api when needed
    
    * fix: use VITE_OPENSEA_API_KEY for opensea

--- apps/ui/src/composables/useNfts.ts
+++ apps/ui/src/composables/useNfts.ts
@@ -1,35 +1,14 @@
-const SUPPORTED_ABIS = ['ERC721', 'ERC1155'];
+import { getNfts } from '@/helpers/opensea';
 
 export function useNfts() {
   const nfts: Ref<any[]> = ref([]);
   const loading = ref(true);
   const loaded = ref(false);
 
-  async function loadNfts(address) {
+  async function loadNfts(address: string, chainId: number) {
     loading.value = true;
 
-    const url = `https://testnets-api.opensea.io/api/v1/assets?owner=${address}&order_direction=desc&offset=0&limit=20&include_orders=false`;
-    const res = await fetch(url);
-    const { assets } = await res.json();
-
-    nfts.value = assets
-      .filter(asset => SUPPORTED_ABIS.includes(asset.asset_contract?.schema_name))
-      .map(asset => {
-        const tokenId = asset.token_id;
-        const title = asset.name ?? 'Untitled';
-        const displayTitle = title.match(/(#[0-9]+)$/) || !tokenId ? title : `${title} #${tokenId}`;
-
-        return {
-          ...asset,
-          type: asset.asset_contract.schema_name.toLowerCase(),
-          tokenId,
-          title,
-          displayTitle,
-          image: asset.image_url,
-          collectionName: asset.collection.name,
-          contractAddress: asset.asset_contract.address
-        };
-      });
+    nfts.value = await getNfts(address, chainId);
     loading.value = false;
     loaded.value = true;
   }
