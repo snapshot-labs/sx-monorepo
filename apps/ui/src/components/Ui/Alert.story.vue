d1f2dc3753bf7533a9ab413014d19515e62f77d1
Sam <samuv.eth@gmail.com>
2024-02-14 11:00:04 +0700

    refactor: add Alert story

---
+++ apps/ui/src/components/Ui/Alert.story.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <Story :layout="{ type: 'grid', width: 680 }">
+    <div class="space-y-[22px]">
+      <Variant title="warning">
+        <UiAlert type="warning">
+          The contracts have not yet undergone an audit. Use at own risk.
+        </UiAlert>
+      </Variant>
+      <Variant title="error">
+        <UiAlert type="error">
+          The contracts have not yet undergone an audit. Use at own risk.
+        </UiAlert>
+      </Variant>
+      <Variant title="success">
+        <UiAlert type="success">
+          The contracts have not yet undergone an audit. Use at own risk.
+        </UiAlert>
+      </Variant>
+    </div>
+  </Story>
+</template>
