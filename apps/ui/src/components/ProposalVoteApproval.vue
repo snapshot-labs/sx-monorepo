aec7f63cacc19ef4855b4f319a39f7bcb61597e6
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-02-21 19:31:01 +0800

    fix(ui): remove unused class

--- apps/ui/src/components/ProposalVoteApproval.vue
+++ apps/ui/src/components/ProposalVoteApproval.vue
@@ -26,7 +26,7 @@ function toggleSelectedChoice(choice: number) {
     <UiButton
       v-for="(choice, index) in proposal.choices"
       :key="index"
-      class="!h-[48px] text-left w-full flex items-center align-middle"
+      class="!h-[48px] text-left w-full flex items-center"
       :class="{ 'border-skin-text': selectedChoices.includes(index + 1) }"
       @click="toggleSelectedChoice(index + 1)"
     >
