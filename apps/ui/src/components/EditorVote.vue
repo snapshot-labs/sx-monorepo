4bea6e9d10dd54975e12ed402c1d6bcf7effbbba
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-02-23 19:20:53 +0800

    fix(ui): add support for dark theme

--- apps/ui/src/components/EditorVote.vue
+++ apps/ui/src/components/EditorVote.vue
@@ -43,10 +43,10 @@ function handleVoteTypeSelected(type: VoteType) {
         v-for="(type, index) in SUPPORTED_VOTING_TYPES as VoteType[]"
         :key="index"
         class="border rounded-lg p-2.5 flex gap-3 cursor-pointer text-left"
-        :class="{ 'border-[#111111] bg-[#FBFBFB]': proposal.type === type }"
+        :class="{ 'border-skin-text bg-skin-input-bg': proposal.type === type }"
         @click="handleVoteTypeSelected(type)"
       >
-        <div class="h-[82px] w-[122px] block rounded-lg shrink-0 bg-[#EDEDED]"></div>
+        <div class="h-[82px] w-[122px] block rounded-lg shrink-0 bg-skin-active-bg"></div>
         <div class="grow">
           <span class="text-skin-heading">{{ VOTE_TYPES[type].label }}</span>
           <div>
@@ -67,7 +67,7 @@ function handleVoteTypeSelected(type: VoteType) {
         <template #item="{ index: index }">
           <div>
             <div
-              class="flex border items-center rounded-lg bg-[#FBFBFB] h-[40px] gap-[12px] pl-2.5"
+              class="flex border items-center rounded-lg bg-skin-input-bg h-[40px] gap-[12px] pl-2.5"
               :class="{ 'border-skin-danger': error && index === 0 }"
             >
               <div
@@ -83,18 +83,17 @@ function handleVoteTypeSelected(type: VoteType) {
                 <input
                   v-model.trim="proposal.choices[index]"
                   type="text"
-                  class="w-full rounded-lg h-[40px] py-[10px] bg-transparent"
+                  class="w-full rounded-lg h-[40px] py-[10px] bg-transparent text-skin-heading"
                   :class="{
-                    '!cursor-not-allowed': proposal.type === 'basic'
+                    '!cursor-not-allowed opacity-40': proposal.type === 'basic'
                   }"
                   :placeholder="error && index === 0 ? 'Please type a choice' : '(optional)'"
                   :disabled="proposal.type === 'basic'"
                 />
               </div>
               <UiButton
-                v-if="proposal.choices.length > 1"
+                v-if="proposal.choices.length > 1 && proposal.type !== 'basic'"
                 class="border-0 rounded-l-none rounded-r-lg border-l bg-transparent h-[40px] w-[40px] !px-0 text-center text-skin-text"
-                :disabled="proposal.type === 'basic'"
                 @click="proposal.choices.splice(index, 1)"
               >
                 <IH-trash class="inline-block" />
@@ -107,8 +106,8 @@ function handleVoteTypeSelected(type: VoteType) {
         </template>
       </Draggable>
       <UiButton
+        v-if="proposal.type !== 'basic'"
         class="w-full border-dashed !rounded-lg flex items-center justify-center space-x-1"
-        :disabled="proposal.type === 'basic'"
         @click="proposal.choices.push('')"
       >
         <IH-plus-sm />
