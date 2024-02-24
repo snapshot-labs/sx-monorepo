47f60dec0d109e2150b3262b3535130e113e7ace
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2024-02-24 10:25:04 +0100

    chore: improve typing in api repo (#81)
    Using shared config with `noImplicitAny` and `noUncheckedIndexedAccess`.

--- apps/api/src/currentConfig.ts
+++ apps/api/src/currentConfig.ts
@@ -4,11 +4,13 @@ import { networkNodeUrl, networkProperties } from './overrrides';
 export default {
   ...baseConfig,
   network_node_url: networkNodeUrl,
-  sources: [
-    {
-      ...baseConfig.sources[0],
+  sources: baseConfig.sources.map((source, i) => {
+    if (i !== 0) return source;
+
+    return {
+      ...source,
       contract: networkProperties.factoryAddress,
       start: networkProperties.startBlock
-    }
-  ]
+    };
+  })
 };
