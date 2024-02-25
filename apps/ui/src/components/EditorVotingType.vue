4eff648b7fb53ce5ce8b420d9c4d9913aa4b321a
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-02-25 17:15:12 +0800

    fix(ui): default to 2 empty choices

--- apps/ui/src/components/EditorVotingType.vue
+++ apps/ui/src/components/EditorVotingType.vue
@@ -27,7 +27,7 @@ function handleVoteTypeSelected(type: VoteType) {
   if (!proposal.value) return;
 
   if (proposal.value.type === 'basic') {
-    proposal.value.choices = Array(3).fill('');
+    proposal.value.choices = Array(2).fill('');
   }
 
   proposal.value.type = type;
