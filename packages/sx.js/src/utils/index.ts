7fdabfe2d99f123978f5d38796b36711e59e6e2c
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2024-03-06 10:26:57 +0100

    chore: remove unused strings and words64 utils (#139)
    starknet.js has string helpers (shortString). words64 is unused and
    I'm not sure how was it used.

--- packages/sx.js/src/utils/index.ts
+++ packages/sx.js/src/utils/index.ts
@@ -1,6 +1,4 @@
-export * as strings from './strings';
 export * as bytes from './bytes';
-export * as words64 from './words64';
 export * as intsSequence from './ints-sequence';
 export * as merkle from './merkletree';
 export * as splitUint256 from './split-uint256';
