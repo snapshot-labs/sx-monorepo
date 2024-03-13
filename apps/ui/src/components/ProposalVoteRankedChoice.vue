f36433cbbb7f997b1373edbaf31384c36f2ffb7d
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-03-13 22:50:36 +0400

    fix(ui): UI update

--- apps/ui/src/components/ProposalVoteRankedChoice.vue
+++ apps/ui/src/components/ProposalVoteRankedChoice.vue
@@ -15,28 +15,29 @@ const selectedChoices = ref<number[]>(props.proposal.choices.map((_, i) => i + 1
 </script>
 
 <template>
-  <div class="flex flex-col space-y-3">
-    <div class="flex flex-col space-y-2">
-      <Draggable
-        v-model="selectedChoices"
-        v-bind="{ animation: 200 }"
-        handle=".handle"
-        class="flex flex-col gap-2 mb-3"
-        item-key="id"
-      >
-        <template #item="{ element, index }">
-          <UiButton class="!h-[48px] text-left w-full flex items-center handle cursor-grab gap-2">
-            <div class="truncate">#{{ index + 1 }}</div>
-            <div class="grow truncate">
-              {{ proposal.choices[element - 1] }}
-            </div>
-            <div class="text-skin-text">
-              <IC-drag />
-            </div>
-          </UiButton>
-        </template>
-      </Draggable>
-    </div>
+  <div class="flex flex-col gap-3">
+    <Draggable
+      v-model="selectedChoices"
+      v-bind="{ animation: 200 }"
+      handle=".handle"
+      class="flex flex-col gap-2"
+      item-key="id"
+    >
+      <template #item="{ element, index }">
+        <UiButton class="!h-[48px] text-left w-full flex items-center handle cursor-grab gap-2">
+          <IC-drag class="text-skin-text" />
+
+          <div class="grow truncate">
+            {{ proposal.choices[element - 1] }}
+          </div>
+          <div
+            class="h-[18px] min-w-[18px] rounded-full leading-[18px] text-[13px] text-white bg-skin-border px-2 text-center inline-block"
+          >
+            #{{ index + 1 }}
+          </div>
+        </UiButton>
+      </template>
+    </Draggable>
     <UiButton
       primary
       class="!h-[48px] w-full"
