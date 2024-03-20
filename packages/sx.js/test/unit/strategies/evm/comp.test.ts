925fc872d76862fd8dc3c1ada2050dee16a47e15
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2024-03-20 17:28:59 +0100

    test: update comp and ozVotes tests to use mocks on sepolia

--- packages/sx.js/test/unit/strategies/evm/comp.test.ts
+++ packages/sx.js/test/unit/strategies/evm/comp.test.ts
@@ -1,12 +1,30 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeAll, afterAll, vi } from 'vitest';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import createCompStrategy from '../../../../src/strategies/evm/comp';
 
 describe('compStrategy', () => {
   const compStrategy = createCompStrategy();
 
-  const provider = new JsonRpcProvider('https://rpc.brovider.xyz/5');
-  const params = '0x3587b2f7e0e2d6166d6c14230e7fe160252b0ba4';
+  const provider = new JsonRpcProvider('https://rpc.brovider.xyz/11155111');
+  const params = '0x6Fd821e79cDf212aD8b06C59B28FE8C2185291d4';
+
+  beforeAll(() => {
+    vi.mock('@ethersproject/contracts', () => ({
+      Contract: class {
+        async getCurrentVotes(voterAddress: string) {
+          if (voterAddress === '0x556B14CbdA79A36dC33FcD461a04A5BCb5dC2A70') {
+            return '440540570656796043262';
+          }
+
+          return '0';
+        }
+      }
+    }));
+  });
+
+  afterAll(() => {
+    vi.resetAllMocks();
+  });
 
   it('should return type', () => {
     expect(compStrategy.type).toBe('comp');
