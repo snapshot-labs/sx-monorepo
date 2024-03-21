360ea93633c7aff7f8c97c23abb5f69548757c99
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-03-21 19:59:37 +0400

    feat(ux): add chart count to input with maxLength

--- apps/ui/src/components/Ui/WrapperInput.vue
+++ apps/ui/src/components/Ui/WrapperInput.vue
@@ -1,18 +1,41 @@
 <script setup lang="ts">
-const props = defineProps<{
-  definition: any;
-  error?: string;
-  dirty?: boolean;
-}>();
+const props = withDefaults(
+  defineProps<{
+    definition: any;
+    inputLength?: number;
+    error?: string;
+    dirty?: boolean;
+  }>(),
+  { inputLength: 0 }
+);
 
 const showError = computed(() => props.error && props.dirty);
 </script>
 
 <template>
   <div class="s-base" :class="showError ? 's-error' : ''">
-    <label v-if="definition.title" class="s-label" v-text="definition.title" />
+    <div class="!flex s-label w-full">
+      <label v-if="definition.title" class="grow" v-text="definition.title" />
+      <div
+        v-if="inputLength >= 0 && definition.maxLength"
+        class="text-sm hidden s-label-char-count"
+      >
+        {{ inputLength }}/{{ definition.maxLength }}
+      </div>
+    </div>
     <slot />
     <span v-if="showError" class="s-input-error-message">{{ error }}</span>
     <legend v-if="definition.description" v-text="definition.description" />
   </div>
 </template>
+
+<style lang="scss">
+.s-base {
+  .s-label:has(+ input:focus),
+  &.s-error {
+    .s-label-char-count {
+      display: block;
+    }
+  }
+}
+</style>
