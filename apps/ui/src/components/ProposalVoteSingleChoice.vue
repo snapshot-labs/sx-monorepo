48c027ae57fd32556bc3ea0b85341e9bb09e35b3
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-04-06 15:41:06 +0400

    feat: add VOTE button to single choice voting form

--- apps/ui/src/components/ProposalVoteSingleChoice.vue
+++ apps/ui/src/components/ProposalVoteSingleChoice.vue
@@ -9,18 +9,32 @@ defineProps<{
 const emit = defineEmits<{
   (e: 'vote', value: number);
 }>();
+
+const selectedChoice = ref<number | null>(null);
 </script>
 
 <template>
-  <div class="flex flex-col space-y-2">
+  <div class="flex flex-col space-y-3">
+    <div class="flex flex-col space-y-2">
+      <UiButton
+        v-for="(choice, index) in proposal.choices"
+        :key="index"
+        class="!h-[48px] text-left w-full flex items-center"
+        :class="{ 'border-skin-text': selectedChoice === index + 1 }"
+        @click="selectedChoice = index + 1"
+      >
+        <div class="grow truncate">{{ choice }}</div>
+        <IH-check v-if="selectedChoice === index + 1" class="shrink-0" />
+      </UiButton>
+    </div>
     <UiButton
-      v-for="(choice, index) in proposal.choices"
-      :key="index"
-      class="!h-[48px] text-left w-full truncate"
-      :loading="sendingType === index + 1"
-      @click="emit('vote', index + 1)"
+      primary
+      class="!h-[48px] w-full"
+      :loading="!!sendingType"
+      :disabled="!selectedChoice"
+      @click="emit('vote', selectedChoice!)"
     >
-      {{ choice }}
+      Vote
     </UiButton>
   </div>
 </template>
