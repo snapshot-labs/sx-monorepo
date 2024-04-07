8cdaa2a0620493a1387bebd7fed9c241fa434027
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-04-07 20:56:34 +0400

    fix: use svg icons from library

--- apps/ui/src/components/Ui/Markdown.vue
+++ apps/ui/src/components/Ui/Markdown.vue
@@ -2,6 +2,7 @@
 import { computed } from 'vue';
 import { Remarkable } from 'remarkable';
 import hljs from 'highlight.js';
+import { icons } from '@iconify-json/heroicons-outline';
 import { linkify } from 'remarkable/linkify';
 import { getUrl } from '@/helpers/utils';
 
@@ -65,8 +66,7 @@ onMounted(() => {
       copyButton.classList.add('copy');
       copyButton.setAttribute('type', 'button');
       copyButton.appendChild(icon);
-      copyButton.innerHTML =
-        '<svg viewBox="0 0 24 24" width="20px" height="20px"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v2m-6 12h8a2 2 0 0 0 2-2v-8a2 2 0 0 0-2-2h-8a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2"></path></svg>';
+      copyButton.innerHTML = `<svg viewBox="0 0 24 24" width="20px" height="20px">${icons.icons.duplicate.body}</svg>`;
 
       copyButton.addEventListener('click', function () {
         if (parent !== null) {
