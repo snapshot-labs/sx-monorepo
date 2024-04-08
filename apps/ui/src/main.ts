1bb3278e3430070dc965eed29c47d3221797287b
ChaituVR <yourchaitu@gmail.com>
2024-04-08 10:11:13 +0530

    Move to ts file

--- apps/ui/src/main.ts
+++ apps/ui/src/main.ts
@@ -34,5 +34,41 @@ const app = createApp({ render: () => h(App) })
 app.use(pinia);
 
 app.mount('#app');
-
+const APP_ID = 'n9dmxtcs';
+window.intercomSettings = {
+  app_id: APP_ID
+};
+(function () {
+  const w = window;
+  const ic = w.Intercom;
+  if (typeof ic === 'function') {
+    ic('reattach_activator');
+    ic('update', w.intercomSettings);
+  } else {
+    const d = document;
+    const i = function () {
+      i.c(arguments);
+    };
+    i.q = [];
+    i.c = function (args) {
+      i.q.push(args);
+    };
+    w.Intercom = i;
+    const l = function () {
+      const s = d.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = `https://widget.intercom.io/widget/${APP_ID}`;
+      const x = d.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+    };
+    if (document.readyState === 'complete') {
+      l();
+    } else if (w.attachEvent) {
+      w.attachEvent('onload', l);
+    } else {
+      w.addEventListener('load', l, false);
+    }
+  }
+})();
 export default app;
