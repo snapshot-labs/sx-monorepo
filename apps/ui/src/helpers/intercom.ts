8ce0bde0dee7cd4e1bf70ba8545b9275260d5100
ChaituVR <yourchaitu@gmail.com>
2024-04-08 18:03:58 +0530

    Remove ts-ignore

--- apps/ui/src/helpers/intercom.ts
+++ apps/ui/src/helpers/intercom.ts
@@ -3,16 +3,13 @@ const APP_ID = import.meta.env.VITE_INTERCOM_APP_ID;
 export function startIntercom() {
   if (!APP_ID || document.body.clientWidth < 544) return;
 
-  // @ts-ignore
-  window.intercomSettings = {
+  const w: any = window;
+  w.intercomSettings = {
     app_id: APP_ID
   };
-  const w = window;
-  // @ts-ignore
   const ic = w.Intercom;
   if (typeof ic === 'function') {
     ic('reattach_activator');
-    // @ts-ignore
     ic('update', w.intercomSettings);
   } else {
     const d = document;
@@ -23,22 +20,18 @@ export function startIntercom() {
     i.c = function (args: any[]) {
       i.q.push(args);
     };
-    // @ts-ignore
     w.Intercom = i;
     const l = function () {
       const s = d.createElement('script');
       s.type = 'text/javascript';
       s.async = true;
       s.src = `https://widget.intercom.io/widget/${APP_ID}`;
-      const x = d.getElementsByTagName('script')[0];
-      // @ts-ignore
+      const x: any = d.getElementsByTagName('script')[0];
       x.parentNode.insertBefore(s, x);
     };
     if (document.readyState === 'complete') {
       l();
-      // @ts-ignore
     } else if (w.attachEvent) {
-      // @ts-ignore
       w.attachEvent('onload', l);
     } else {
       w.addEventListener('load', l, false);
