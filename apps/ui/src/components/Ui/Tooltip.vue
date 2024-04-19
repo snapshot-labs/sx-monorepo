bd661f5f2f41b0f186aed8767d7b6aef29bff55a
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2024-04-19 13:45:38 +0200

    feat: add tooltips with space names on sidebar
    Unless recognizable avatar is used it's hard to tell which space is which
    on the sidebar without visting it manually.
    
    This PR adds tooltips with space names.

--- apps/ui/src/components/Ui/Tooltip.vue
+++ apps/ui/src/components/Ui/Tooltip.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import type { Placement } from 'tippy.js';
+
 defineProps<{
   title: string;
+  placement?: Placement;
 }>();
 </script>
 
 <template>
-  <div v-tippy="{ content: title }" class="inline-block relative">
+  <div v-tippy="{ content: title, placement }" class="inline-block relative">
     <slot />
   </div>
 </template>
