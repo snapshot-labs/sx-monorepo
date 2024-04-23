7f32cc4f09e4c9b232d3c998cd6caacb43b9fa20
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-04-23 17:17:49 +0400

    fix(ui): make sidebar scrollable

--- apps/ui/src/components/App/Sidebar.vue
+++ apps/ui/src/components/App/Sidebar.vue
@@ -6,7 +6,7 @@ const spacesStore = useSpacesStore();
 </script>
 
 <template>
-  <div class="w-[72px] border-r fixed left-0 top-0 bottom-0 text-center">
+  <div class="w-[72px] flex flex-col border-r fixed left-0 top-0 bottom-0 text-center h-screen">
     <router-link :to="{ name: 'landing' }" class="h-[72px] block">
       <IH-stop class="inline-block my-4 w-[32px] h-[32px] text-skin-link" />
     </router-link>
@@ -19,7 +19,7 @@ const spacesStore = useSpacesStore();
       :touch-start-threshold="35"
       :item-key="i => i"
       v-bind="{ animation: 200 }"
-      class="space-y-3 p-2"
+      class="space-y-3 p-2 no-scrollbar overscroll-contain overflow-auto"
     >
       <template #item="{ element }">
         <router-link
