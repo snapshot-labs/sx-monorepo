6ccfb1069f503060ab771c96a13bf9d6e88ba3da
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-05-05 01:05:47 +0400

    feat: support notifications from all networks

--- apps/ui/src/stores/notifications.ts
+++ apps/ui/src/stores/notifications.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia';
-import { enabledNetworks, getNetwork, offchainNetworks } from '@/networks';
-import { ProposalsFilter } from '@/networks/types';
-import { Proposal } from '@/types';
+import { getNetwork } from '@/networks';
+import { Network, ProposalsFilter } from '@/networks/types';
+import { NetworkID, Proposal } from '@/types';
 import pkg from '../../package.json';
 
 type Notification = {
@@ -14,8 +14,6 @@ type Notification = {
 
 const NOTIFICATION_TIME_WINDOW = 60 * 60 * 24 * 14; // 2 weeks
 const REFRESH_INTERVAL = 60 * 15; // 15 minutes
-const offchainNetworkId = offchainNetworks.filter(network => enabledNetworks.includes(network))[0];
-const network = getNetwork(offchainNetworkId);
 let refreshNotificationInterval: NodeJS.Timeout;
 
 export const useNotificationsStore = defineStore('notifications', () => {
@@ -26,23 +24,48 @@ export const useNotificationsStore = defineStore('notifications', () => {
   const bookmarksStore = useBookmarksStore();
   const metaStore = useMetaStore();
 
-  async function loadProposals(state: NonNullable<ProposalsFilter['state']>, pivotTs: number) {
-    return await network.api.loadProposals(
-      bookmarksStore.followedSpacesIds.map(id => id.split(':')[1]),
-      { limit: 100 },
-      metaStore.getCurrent(offchainNetworkId) || 0,
-      { state, start_gte: pivotTs }
-    );
+  async function loadProposals(
+    network: Network,
+    current: number,
+    state: NonNullable<ProposalsFilter['state']>,
+    pivotTs: number,
+    spaceIds: string[]
+  ) {
+    return await network.api.loadProposals(spaceIds, { limit: 100 }, current, {
+      state,
+      start_gte: pivotTs
+    });
   }
 
   async function loadNotifications() {
-    await metaStore.fetchBlock(offchainNetworkId);
     const now = Math.floor(Date.now() / 1e3);
     const pivotTs = now - NOTIFICATION_TIME_WINDOW;
 
-    const proposals = (
-      await Promise.all([loadProposals('active', pivotTs), loadProposals('closed', pivotTs)])
-    ).flat();
+    const followedSpaceIdsByNetwork: Record<NetworkID, string[]> = bookmarksStore.followedSpacesIds
+      .map(id => id.split(':'))
+      .reduce((acc, [networkId, spaceId]) => {
+        acc[networkId] ||= [];
+        acc[networkId].push(spaceId);
+        return acc;
+      }, {});
+
+    (Object.keys(followedSpaceIdsByNetwork) as NetworkID[]).forEach(async networkId => {
+      await metaStore.fetchBlock(networkId);
+    });
+
+    const promises = (Object.entries(followedSpaceIdsByNetwork) as [NetworkID, string[]][])
+      .map(([networkId, spaceIds]) => {
+        const network = getNetwork(networkId);
+        const current = metaStore.getCurrent(networkId) || 0;
+
+        return [
+          loadProposals(network, current, 'active', pivotTs, spaceIds),
+          loadProposals(network, current, 'closed', pivotTs, spaceIds)
+        ];
+      })
+      .flat();
+
+    const proposals = (await Promise.all(promises)).flat();
 
     proposals.forEach(proposal => {
       const timestamp = proposal.min_end < now ? proposal.min_end : proposal.start;
@@ -59,6 +82,8 @@ export const useNotificationsStore = defineStore('notifications', () => {
     });
 
     notifications.value.sort((a, b) => b.timestamp - a.timestamp);
+
+    loading.value = false;
   }
 
   function markAllAsRead() {
@@ -72,15 +97,16 @@ export const useNotificationsStore = defineStore('notifications', () => {
     () => notifications.value.map(n => n.unread).filter(Boolean).length
   );
 
-  watch(bookmarksStore.followedSpacesIds, () => {
-    notifications.value = [];
-    loadNotifications();
-  });
+  watch(
+    () => bookmarksStore.followedSpacesIds,
+    () => {
+      notifications.value = [];
+      loadNotifications();
+    },
+    { immediate: true }
+  );
 
-  onMounted(async () => {
-    await loadNotifications();
-    loading.value = false;
-
+  onMounted(() => {
     refreshNotificationInterval = setInterval(loadNotifications, REFRESH_INTERVAL * 1e3);
   });
 
