da394b5ee4d5e140795e6b81c52af543b7fe44a3
Wan <495709+wa0x6e@users.noreply.github.com>
2024-05-05 23:07:00 +0400

    fix: fix tooltip position not matching content length (#318)

--- apps/ui/src/views/Proposal/Votes.vue
+++ apps/ui/src/views/Proposal/Votes.vue
@@ -223,7 +223,7 @@ watch([sortBy, choiceFilter], () => {
               <div v-else>
                 <UiTooltip
                   v-if="proposal.type !== 'basic'"
-                  class="truncate !block"
+                  class="max-w-[100%] truncate !inline-block"
                   :title="getChoiceText(proposal.choices, vote.choice)"
                 >
                   {{ getChoiceText(proposal.choices, vote.choice) }}
