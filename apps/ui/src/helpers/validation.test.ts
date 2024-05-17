52769b2844aec26064c70921c51baad88bc48262
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-05-17 21:47:17 +0400

    fix: add more test

--- apps/ui/src/helpers/validation.test.ts
+++ apps/ui/src/helpers/validation.test.ts
@@ -14,7 +14,7 @@ describe('validation', () => {
     };
     const validator = getValidator(schema);
 
-    it('should return true for a single valid EVM address with voting power', () => {
+    it('should not return errors for a single valid EVM address with voting power', () => {
       const result = validator.validate({
         whitelist: '0x395ed61716b48dc904140b515e9f682e33330154:1'
       });
@@ -22,7 +22,7 @@ describe('validation', () => {
       expect(result).toEqual({});
     });
 
-    it('should return true for a single valid starknet address with voting power', () => {
+    it('should not return errors for a single valid starknet address with voting power', () => {
       const result = validator.validate({
         whitelist: '0x7d2f37b75a5e779f7da01c22acee1b66c39e8ba470ee5448f05e1462afcedb4:1'
       });
@@ -30,7 +30,7 @@ describe('validation', () => {
       expect(result).toEqual({});
     });
 
-    it('should return true for a list of addresses with voting power', () => {
+    it('should not return errors for a list of addresses with voting power', () => {
       const result = validator.validate({
         whitelist: `
           0x7d2f37b75a5e779f7da01c22acee1b66c39e8ba470ee5448f05e1462afcedb4 :  1
@@ -56,6 +56,14 @@ describe('validation', () => {
     it('should return an error when voting power is empty', () => {
       const result = validator.validate({
         whitelist: '0x395ed61716b48dc904140b515e9f682e33330154:'
+      });
+
+      expect(result).toHaveProperty('whitelist');
+    });
+
+    it('should return an error when voting power is invalid', () => {
+      const result = validator.validate({
+        whitelist: '0x395ed61716b48dc904140b515e9f682e33330154:4xx!'
       });
 
       expect(result).toHaveProperty('whitelist');
