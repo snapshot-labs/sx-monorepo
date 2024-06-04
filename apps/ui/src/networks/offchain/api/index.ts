c844b7233282e00b0c4d0468eadd3c28db87275c
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-06-04 19:28:23 +0400

    fix: use results from ranking directly

--- apps/ui/src/networks/offchain/api/index.ts
+++ apps/ui/src/networks/offchain/api/index.ts
@@ -26,7 +26,6 @@ import {
 } from '@/types';
 import { ApiSpace, ApiProposal, ApiVote } from './types';
 import { DEFAULT_VOTING_DELAY } from '../constants';
-import { clone } from '@/helpers/utils';
 
 const DEFAULT_AUTHENTICATOR = 'OffchainAuthenticator';
 
@@ -331,22 +330,19 @@ export function createApi(uri: string, networkId: NetworkID): NetworkApi {
       { limit, skip = 0 }: PaginationOpts,
       filter?: SpacesFilter
     ): Promise<Space[]> => {
-      let _filters = clone(filter || {});
-
-      if (!_filters?.id_in) {
+      if (!filter?.id_in) {
         const { data } = await apollo.query({
           query: RANKING_QUERY,
           variables: {
-            first: limit,
+            first: Math.min(limit, 20),
             skip,
             where: {
-              ..._filters
+              ...filter
             }
           }
         });
 
-        _filters = { id_in: data.ranking.items.map(space => space.id) };
-        skip = 0;
+        return data.ranking.items.map(space => formatSpace(space, networkId));
       }
 
       const { data } = await apollo.query({
@@ -355,18 +351,12 @@ export function createApi(uri: string, networkId: NetworkID): NetworkApi {
           first: limit,
           skip,
           where: {
-            ..._filters
+            ...filter
           }
         }
       });
 
-      const spaces: Space[] = data.spaces.map(space => formatSpace(space, networkId));
-
-      if (_filters.id_in) {
-        spaces.sort((a, b) => _filters.id_in!.indexOf(a.id) - _filters.id_in!.indexOf(b.id));
-      }
-
-      return spaces;
+      return data.spaces.map(space => formatSpace(space, networkId));
     },
     loadSpace: async (id: string): Promise<Space | null> => {
       const { data } = await apollo.query({
