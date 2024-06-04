61a51c0df0e77012d2d27097948caa76558dff10
Wan <495709+wa0x6e@users.noreply.github.com>
2024-06-04 19:29:40 +0400

    Update apps/ui/src/composables/useSpaces.ts
    Co-authored-by: Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>

--- apps/ui/src/composables/useSpaces.ts
+++ apps/ui/src/composables/useSpaces.ts
@@ -108,7 +108,7 @@ export function useSpaces() {
           filter
         );
 
-        explorePageSpaces.value = [...(overwrite ? [] : explorePageSpaces.value), ...spaces];
+        explorePageSpaces.value = overwrite ? spaces : [explorePageSpaces.value, ...spaces];
 
         return {
           id,
