3f8008588bc1737d8a73c3482cc8fdad6fe856fc
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2024-06-17 19:13:29 +0200

    chore: merge l1 execution-api with subgraph-api

--- apps/subgraph-api/src/starknet-mapping.ts
+++ apps/subgraph-api/src/starknet-mapping.ts
@@ -1,4 +1,4 @@
-import { Execution } from '../generated/schema'
+import { StarknetL1Execution } from '../generated/schema'
 import { ContractDeployed } from '../generated/L1AvatarExecutionStrategyFactory/L1AvatarExecutionStrategyFactory'
 import { ProposalExecuted } from '../generated/templates/L1AvatarExecutionStrategy/L1AvatarExecutionStrategy'
 import { L1AvatarExecutionStrategy as L1AvatarExecutionStrategyTemplate } from '../generated/templates'
@@ -7,14 +7,15 @@ export function handleContractDeployed(event: ContractDeployed): void {
   L1AvatarExecutionStrategyTemplate.create(event.params.contractAddress)
 }
 
-export function handleProposalExecuted(event: ProposalExecuted): void {
+export function handleStarknetProposalExecuted(event: ProposalExecuted): void {
   let space = event.params.space.toHex()
   let paddedSpace = `0x${space.replace('0x', '').padStart(64, '0')}`
   let proposalId = event.params.proposalId.toI32()
 
-  let executionEntity = new Execution(`${paddedSpace}/${proposalId}`)
+  let executionEntity = new StarknetL1Execution(`${paddedSpace}/${proposalId}`)
   executionEntity.space = paddedSpace
   executionEntity.proposalId = proposalId
   executionEntity.created = event.block.timestamp.toI32()
+  executionEntity.tx = event.transaction.hash
   executionEntity.save()
 }
