68fe4feff917572a05aa4f00978a800bc5b50230
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-06-30 12:02:06 +0900

    fix: refresh list on account change

--- apps/ui/src/views/Settings/Spaces.vue
+++ apps/ui/src/views/Settings/Spaces.vue
@@ -1,30 +1,26 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-
 useTitle('My spaces');
+
 const { web3 } = useWeb3();
 const { loaded, spaces, fetch } = useSpaces();
 
-onMounted(() => fetch({ controller: web3.value.account }));
+watch(
+  () => web3.value.account,
+  controller => fetch({ controller }),
+  { immediate: true }
+);
 </script>
 
 <template>
-  <div>
-    <UiContainer class="!max-w-screen-md pt-5">
-      <h2 class="mb-4 mono !text-xl" v-text="'My spaces'" />
-      <UiLoading v-if="!loaded" class="block mb-2" />
-      <div
-        v-if="loaded && !spaces.length && !web3.authLoading"
-        class="py-3 flex items-center text-skin-link"
-      >
-        <IH-exclamation-circle class="inline-block mr-2" />
-        <span v-text="'There are no spaces here.'" />
-      </div>
-      <div v-else-if="loaded" class="max-w-screen-md">
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-3">
-          <SpacesListItem v-for="space in spaces" :key="space.id" :space="space" />
-        </div>
-      </div>
-    </UiContainer>
-  </div>
+  <UiContainer class="!max-w-screen-md pt-5">
+    <h2 class="mb-4 mono !text-xl" v-text="'My spaces'" />
+    <UiLoading v-if="!loaded || web3.authLoading" />
+    <div v-else-if="!spaces.length" class="flex items-center text-skin-link space-x-2">
+      <IH-exclamation-circle class="inline-block shrink-0" />
+      <span v-text="'There are no spaces here.'" />
+    </div>
+    <div v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-3">
+      <SpacesListItem v-for="space in spaces" :key="space.id" :space="space" />
+    </div>
+  </UiContainer>
 </template>
