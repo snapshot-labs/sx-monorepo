e4a69a00923abb35b1b069f7e36f69292088d069
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-07-02 08:22:17 +0900

    fix: avoid fetching spaces for empty controller

--- apps/ui/src/views/Settings/Spaces.vue
+++ apps/ui/src/views/Settings/Spaces.vue
@@ -2,11 +2,19 @@
 useTitle('My spaces');
 
 const { web3 } = useWeb3();
-const { loaded, spaces, fetch } = useSpaces();
+const { loading: spacesLoading, spaces, fetch } = useSpaces();
+
+const loaded = ref(false);
 
 watch(
   () => web3.value.account,
-  controller => fetch({ controller }),
+  async controller => {
+    loaded.value = false;
+
+    controller && (await fetch({ controller }));
+
+    loaded.value = true;
+  },
   { immediate: true }
 );
 </script>
@@ -14,8 +22,11 @@ watch(
 <template>
   <UiContainer class="!max-w-screen-md pt-5">
     <h2 class="mb-4 mono !text-xl" v-text="'My spaces'" />
-    <UiLoading v-if="!loaded || web3.authLoading" />
-    <div v-else-if="!spaces.length" class="flex items-center text-skin-link space-x-2">
+    <UiLoading v-if="!loaded || spacesLoading || web3.authLoading" />
+    <div
+      v-else-if="!spaces.length || !web3.account"
+      class="flex items-center text-skin-link space-x-2"
+    >
       <IH-exclamation-circle class="inline-block shrink-0" />
       <span v-text="'There are no spaces here.'" />
     </div>
