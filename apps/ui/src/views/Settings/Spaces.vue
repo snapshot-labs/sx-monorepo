bc2a1cf510db04cd3abd892f3ab733915015389e
Wan <495709+wa0x6e@users.noreply.github.com>
2024-07-02 18:12:10 +0900

    fix: do not flash "No spaces" message while loading spaces (#453)
    * fix: do not flash "No spaces" message while loading spaces
    
    * fix: refresh list on account change
    
    * fix: avoid fetching spaces for empty controller

--- apps/ui/src/views/Settings/Spaces.vue
+++ apps/ui/src/views/Settings/Spaces.vue
@@ -1,27 +1,37 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-
 useTitle('My spaces');
-const { web3Account } = useWeb3();
-const { loaded, spaces, fetch } = useSpaces();
 
-onMounted(() => fetch({ controller: web3Account.value }));
+const { web3 } = useWeb3();
+const { loading: spacesLoading, spaces, fetch } = useSpaces();
+
+const loaded = ref(false);
+
+watch(
+  () => web3.value.account,
+  async controller => {
+    loaded.value = false;
+
+    controller && (await fetch({ controller }));
+
+    loaded.value = true;
+  },
+  { immediate: true }
+);
 </script>
 
 <template>
-  <div>
-    <UiContainer class="!max-w-screen-md pt-5">
-      <h2 class="mb-4 mono !text-xl" v-text="'My spaces'" />
-      <UiLoading v-if="!loaded" class="block mb-2" />
-      <div v-if="loaded && !spaces.length" class="py-3 flex items-center text-skin-link">
-        <IH-exclamation-circle class="inline-block mr-2" />
-        <span v-text="'There are no spaces here.'" />
-      </div>
-      <div v-else-if="loaded" class="max-w-screen-md">
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-3">
-          <SpacesListItem v-for="space in spaces" :key="space.id" :space="space" />
-        </div>
-      </div>
-    </UiContainer>
-  </div>
+  <UiContainer class="!max-w-screen-md pt-5">
+    <h2 class="mb-4 mono !text-xl" v-text="'My spaces'" />
+    <UiLoading v-if="!loaded || spacesLoading || web3.authLoading" />
+    <div
+      v-else-if="!spaces.length || !web3.account"
+      class="flex items-center text-skin-link space-x-2"
+    >
+      <IH-exclamation-circle class="inline-block shrink-0" />
+      <span v-text="'There are no spaces here.'" />
+    </div>
+    <div v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-3">
+      <SpacesListItem v-for="space in spaces" :key="space.id" :space="space" />
+    </div>
+  </UiContainer>
 </template>
