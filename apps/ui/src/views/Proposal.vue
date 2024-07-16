8118e9cb1f95abe623d30f4684d6ab86fd51994f
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2024-07-16 16:03:17 +0200

    Revert "chore: update help desk link format after merge (#484)"
    This reverts commit 78044d356490e7741cea2b5e7cfefc32086d9284.

--- apps/ui/src/views/Proposal.vue
+++ apps/ui/src/views/Proposal.vue
@@ -204,9 +204,7 @@ watchEffect(() => {
                 <span v-else class="text-skin-link" v-text="props.formattedVotingPower" />
               </a>
               <a
-                v-if="
-                  votingPower?.status === 'success' && votingPower.totalVotingPower === BigInt(0)
-                "
+                v-if="votingPowerStatus === 'success' && props.votingPower === BigInt(0)"
                 href="https://help.snapshot.org/en/articles/9566904-why-do-i-have-0-voting-power"
                 target="_blank"
                 class="ml-1.5"
