7e5ee0af2042a11a57465d563e162aedb7597c6b
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-07-20 10:48:15 +0900

    chore: lint fix

--- apps/ui/src/components/App/Topnav.vue
+++ apps/ui/src/components/App/Topnav.vue
@@ -6,7 +6,7 @@ const route = useRoute();
 const router = useRouter();
 const auth = getInstance();
 const uiStore = useUiStore();
-
+s;
 const { modalAccountOpen } = useModal();
 const { login, web3 } = useWeb3();
 const { toggleSkin, currentMode } = useUserSkin();
