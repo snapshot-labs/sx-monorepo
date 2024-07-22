11d5f8d55272b8cdf4c529a086c3fea1f4d3fc9e
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-07-22 12:41:36 +0900

    fix: revert message

--- apps/ui/src/components/ProposalVote.vue
+++ apps/ui/src/components/ProposalVote.vue
@@ -75,7 +75,7 @@ function handleEditVoteClick() {
   </slot>
 
   <slot v-else-if="pendingVotes[proposal.id]" name="voted-pending">
-    This vote is pending confirmation
+    You have already voted for this proposal
   </slot>
   <slot v-else-if="proposal.state === 'pending'" name="waiting">
     Voting for this proposal hasn't started yet. Voting will start {{ _t(start) }}.
