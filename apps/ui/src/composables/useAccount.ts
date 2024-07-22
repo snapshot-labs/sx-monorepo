7b91e0590a181d60071952fb66be526fa6d49e7d
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-07-22 18:49:50 +0900

    fix: avoid user votes flashing on load

--- apps/ui/src/composables/useAccount.ts
+++ apps/ui/src/composables/useAccount.ts
@@ -16,15 +16,14 @@ watch(
 
 export function useAccount() {
   async function loadVotes(networkId: NetworkID, spaceIds: string[]) {
-    votes.value = {};
-
     const account = web3.value.account;
-    if (!account) return;
+    if (!account) {
+      votes.value = {};
+      return;
+    }
 
     const network = getNetwork(networkId);
-    const userVotes = await network.api.loadUserVotes(spaceIds, account);
-
-    votes.value = { ...votes.value, ...userVotes };
+    votes.value = await network.api.loadUserVotes(spaceIds, account);
   }
 
   function addPendingVote(proposalId: string) {
