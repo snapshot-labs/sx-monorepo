snapshot-labs/sx-monorepo

commit e088ca6221c9ed9b04ced22476f6fed22181e20f

feat: add support for starknet account to offchain actions (#383)
* feat: add setAlias to starknet eth sign

* fix: use starknet-sig instead of ethereum-sig

* fix: fix signature schema

* fix: update type to include optional properties

* fix: more robust starknet wallet detection

* fix: enable back follow button for starknet wallet

* fix: fix typing

* feat: add support for starknet signature for offchain `send`

* fix: use string type to allow starknet address

* fix: enable space following/unfollowing for starknet wallets

* fix: enable user profile edition for starknet users

* fix: use formatted address in alias from

* fix: always use padded starknet address

* fix: sign sn createAlias on the correct chainId

* chore: fix tests

* fix: avoid loading votes from incompatible networks

* fix: add types and domain to starknet envelope

* Update apps/ui/src/composables/useActions.ts

Co-authored-by: Chaitanya <yourchaitu@gmail.com>

* fix: revert unrelated change

* chore: removed unused import

* chore: add comments about confusing extra starknet params on evm envelope

* chore: fix import order

* fix: sign alias with first available starknet network

* chore: lint fix

* chore: add changesets

* revert: revert back ot use correct starknet chaind ID

* fix: finish revert

* fix: fix leftover from merge

* refactor: simplify starknetNetworkId lookup

---------

Co-authored-by: Chaitanya <yourchaitu@gmail.com>
Co-authored-by: Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>

apps/ui/src/composables/useActions.ts
@@ -1,7 +1,14 @@
 import { getInstance } from '@snapshot-labs/lock/plugins/vue3';
 import { registerTransaction } from '@/helpers/mana';
 import { convertToMetaTransactions } from '@/helpers/transactions';
-import { getNetwork, getReadWriteNetwork, metadataNetwork } from '@/networks';
+import {
+  enabledNetworks,
+  getNetwork,
+  getReadWriteNetwork,
+  metadataNetwork,
+  starknetNetworks
+} from '@/networks';
+import { STARKNET_CONNECTORS } from '@/networks/common/constants';
 import { Connector, StrategyConfig } from '@/networks/types';
 import {
   Choice,
@@ -15,6 +22,13 @@ import {
   User,
   VoteType
 } from '@/types';
+
+const offchainToStarknetIds: Record<string, NetworkID> = {
+  s: 'sn',
+  's-tn': 'sn-sep'
+};
+
+const starknetNetworkId = offchainToStarknetIds[metadataNetwork];
 
 export function useActions() {
   const { mixpanel } = useMixpanel();
@@ -126,7 +140,11 @@ export function useActions() {
   }
 
   async function getAliasSigner() {
-    const network = getNetwork(metadataNetwork);
+    const network = getNetwork(
+      STARKNET_CONNECTORS.includes(web3.value.type as Connector)
+        ? starknetNetworkId
+        : metadataNetwork
+    );
 
     return alias.getAliasWallet(address =>
       wrapPromise(metadataNetwork, network.actions.setAlias(auth.web3, address))

apps/ui/src/components/ButtonFollow.vue
@@ -12,12 +12,10 @@ const { isSafeWallet } = useSafeWallet(
   props.space.snapshot_chain_id
 );
 const followedSpacesStore = useFollowedSpacesStore();
-const { web3 } = useWeb3();
 
 const spaceFollowed = computed(() =>
   followedSpacesStore.isFollowed(spaceIdComposite)
 );
-const hidden = computed(() => web3.value?.type === 'argentx');
 
 const loading = computed(
   () =>
@@ -28,7 +26,6 @@ const loading = computed(
 
 <template>
   <UiButton
-    v-if="!hidden"
     :disabled="loading || isSafeWallet"
     class="group"
     :class="{ 'hover:border-skin-danger': spaceFollowed }"

apps/ui/src/networks/index.ts
@@ -29,6 +29,7 @@ export const evmNetworks: NetworkID[] = [
   'linea-testnet'
 ];
 export const offchainNetworks: NetworkID[] = ['s', 's-tn'];
+export const starknetNetworks: NetworkID[] = ['sn', 'sn-sep'];
 // This network is used for aliases/follows/profiles/explore page.
 export const metadataNetwork: NetworkID =
   import.meta.env.VITE_METADATA_NETWORK || 's';

apps/ui/src/networks/starknet/actions.ts
@@ -658,7 +658,12 @@ export function createActions(
     },
     followSpace: () => {},
     unfollowSpace: () => {},
-    setAlias: () => {},
+    setAlias(web3: any, alias: string) {
+      return starkSigClient.setAlias({
+        signer: web3.provider.account,
+        data: { alias }
+      });
+    },
     updateUser: () => {},
     updateStatement: () => {},
     send: (envelope: any) => starkSigClient.send(envelope) // TODO: extract it out of client to common helper

apps/ui/src/stores/followedSpaces.ts
@@ -128,16 +128,15 @@ export const useFollowedSpacesStore = defineStore('followedSpaces', () => {
   watch(
     [
       () => web3.value.account,
-      () => web3.value.type,
       () => web3.value.authLoading,
       () => authInitiated.value
     ],
-    async ([web3, walletType, authLoading, authInitiated]) => {
+    async ([web3, authLoading, authInitiated]) => {
       if (!authInitiated || authLoading) return;
 
       followedSpacesLoaded.value = false;
 
-      if (!web3 || walletType === 'argentx') {
+      if (!web3) {
         followedSpacesIds.value = [];
         followedSpacesLoaded.value = true;
         return;

apps/ui/src/views/User.vue
@@ -130,10 +130,7 @@ watchEffect(() => setTitle(`${user.value?.name || id.value} user profile`));
       />
       <div class="absolute right-4 top-4 space-x-2 flex">
         <DropdownShare :message="shareMsg" class="!px-0 w-[46px]" />
-        <UiTooltip
-          v-if="web3.account === user.id && web3.type !== 'argentx'"
-          title="Edit profile"
-        >
+        <UiTooltip v-if="web3.account === user.id" title="Edit profile">
           <UiButton class="!px-0 w-[46px]" @click="modalOpenEditUser = true">
             <IH-cog class="inline-block" />
           </UiButton>

packages/sx.js/src/clients/offchain/ethereum-sig/index.ts
@@ -21,7 +21,7 @@ import {
   weightedVoteTypes
 } from './types';
 import { offchainGoerli } from '../../../offchainNetworks';
-import { OffchainNetworkConfig } from '../../../types';
+import { OffchainNetworkConfig, SignatureData } from '../../../types';
 import {
   CancelProposal,
   EIP712CancelProposalMessage,
@@ -38,7 +38,6 @@ import {
   FollowSpace,
   Propose,
   SetAlias,
-  SignatureData,
   UnfollowSpace,
   UpdateProposal,
   UpdateStatement,
@@ -120,9 +119,10 @@ export class EthereumSig {
       signature: sig,
       domain,
       types,
+      primaryType,
       message
     } = envelope.signatureData!;
-    const payload = {
+    const payload: any = {
       address,
       sig,
       data: {
@@ -131,6 +131,9 @@ export class EthereumSig {
         message
       }
     };
+
+    // primaryType needs to be attached when sending starknet-sig generated payload
+    if (primaryType) payload.data.primaryType = primaryType;
 
     const body = {
       method: 'POST',

packages/sx.js/src/clients/offchain/ethereum-sig/types.ts
@@ -103,7 +103,7 @@ export const cancelProposalTypes = {
 
 export const followSpaceTypes = {
   Follow: [
-    { name: 'from', type: 'address' },
+    { name: 'from', type: 'string' },
     { name: 'network', type: 'string' },
     { name: 'space', type: 'string' },
     { name: 'timestamp', type: 'uint64' }
@@ -112,7 +112,7 @@ export const followSpaceTypes = {
 
 export const unfollowSpaceTypes = {
   Unfollow: [
-    { name: 'from', type: 'address' },
+    { name: 'from', type: 'string' },
     { name: 'network', type: 'string' },
     { name: 'space', type: 'string' },
     { name: 'timestamp', type: 'uint64' }
@@ -129,7 +129,7 @@ export const aliasTypes = {
 
 export const updateUserTypes = {
   Profile: [
-    { name: 'from', type: 'address' },
+    { name: 'from', type: 'string' },
     { name: 'timestamp', type: 'uint64' },
     { name: 'profile', type: 'string' }
   ]

commit 00d3fd4151cf080d893d5d447802113563c03583

chore: remove unused imports
Regression from: https://github.com/snapshot-labs/sx-monorepo/pull/383
(but broken change commited by me).

apps/ui/src/composables/useActions.ts
@@ -1,13 +1,7 @@
 import { getInstance } from '@snapshot-labs/lock/plugins/vue3';
 import { registerTransaction } from '@/helpers/mana';
 import { convertToMetaTransactions } from '@/helpers/transactions';
-import {
-  enabledNetworks,
-  getNetwork,
-  getReadWriteNetwork,
-  metadataNetwork,
-  starknetNetworks
-} from '@/networks';
+import { getNetwork, getReadWriteNetwork, metadataNetwork } from '@/networks';
 import { STARKNET_CONNECTORS } from '@/networks/common/constants';
 import { Connector, StrategyConfig } from '@/networks/types';
 import {