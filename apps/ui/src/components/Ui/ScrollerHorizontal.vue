b8e7ded3e2f77fb796c9060609d3a21f302e14c5
Wan <495709+wa0x6e@users.noreply.github.com>
2024-08-23 00:58:07 +0900

    feat: add horizontal scroller component to handle x scroll logic (#648)
    * feat: add horizontal scroller component to handle x scroll logic
    
    * fix: migrate more elements to UiScrollHorizontal

---
+++ apps/ui/src/components/Ui/ScrollerHorizontal.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    gradient: false | 'sm' | 'md';
+  }>(),
+  { gradient: false }
+);
+</script>
+
+<template>
+  <div class="relative">
+    <div
+      v-if="gradient"
+      class="bg-gradient-to-r from-skin-bg left-0 top-[1px] bottom-[1px] absolute z-10 pointer-events-none"
+      :class="{
+        'w-2': gradient === 'sm',
+        'w-3': gradient === 'md'
+      }"
+    />
+    <div class="overflow-x-auto no-scrollbar">
+      <div data-no-sidebar-swipe>
+        <slot />
+      </div>
+    </div>
+    <div
+      v-if="gradient"
+      class="bg-gradient-to-l from-skin-bg right-0 top-[1px] bottom-[1px] absolute z-10 pointer-events-none"
+      :class="{
+        'w-2': gradient === 'sm',
+        'w-3': gradient === 'md'
+      }"
+    />
+  </div>
+</template>
