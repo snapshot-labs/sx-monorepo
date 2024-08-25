411ff2d40914e1959f73249a9c2cf460cc61a2e8
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-08-25 12:43:13 +0900

    fix(ux): move buttons out of SpaceCover

--- apps/ui/src/views/Space/Overview.vue
+++ apps/ui/src/views/Space/Overview.vue
@@ -52,21 +52,6 @@ watchEffect(() => setTitle(props.space.name));
       <div
         class="relative bg-skin-bg h-[16px] -top-3 rounded-t-[16px] md:hidden"
       />
-      <div class="absolute right-4 top-4 space-x-2">
-        <router-link :to="{ name: 'editor' }" tabindex="-1">
-          <UiTooltip title="New proposal">
-            <UiButton class="!px-0 w-[46px]">
-              <IH-pencil-alt class="inline-block" />
-            </UiButton>
-          </UiTooltip>
-        </router-link>
-        <UiTooltip v-if="isController" title="Edit profile">
-          <UiButton class="!px-0 w-[46px]" @click="editSpaceModalOpen = true">
-            <IH-cog class="inline-block" />
-          </UiButton>
-        </UiTooltip>
-        <ButtonFollow :space="space" />
-      </div>
     </div>
     <div class="px-4">
       <div class="mb-4 relative">
@@ -77,13 +62,33 @@ watchEffect(() => setTitle(props.space.name));
             class="relative mb-2 border-4 border-skin-bg !rounded-lg -left-1"
           />
         </router-link>
-        <div class="flex items-center">
-          <h1 v-text="space.name" />
-          <UiBadgeVerified
-            class="ml-1 top-0.5"
-            :verified="space.verified"
-            :turbo="space.turbo"
-          />
+        <div class="flex justify-between flex-wrap-reverse gap-2">
+          <div class="flex items-center space-x-1">
+            <h1 v-text="space.name" />
+            <UiBadgeVerified
+              class="top-0.5"
+              :verified="space.verified"
+              :turbo="space.turbo"
+            />
+          </div>
+          <div class="flex space-x-2">
+            <router-link :to="{ name: 'editor' }" tabindex="-1">
+              <UiTooltip title="New proposal">
+                <UiButton class="!px-0 w-[46px]">
+                  <IH-pencil-alt class="inline-block" />
+                </UiButton>
+              </UiTooltip>
+            </router-link>
+            <UiTooltip v-if="isController" title="Edit profile">
+              <UiButton
+                class="!px-0 w-[46px]"
+                @click="editSpaceModalOpen = true"
+              >
+                <IH-cog class="inline-block" />
+              </UiButton>
+            </UiTooltip>
+            <ButtonFollow :space="space" />
+          </div>
         </div>
         <div class="mb-3">
           <b class="text-skin-link">{{ _n(space.proposal_count) }}</b> proposals
