3220ac40f597eb31211b4629a332b9dd3c67016b
ChaituVR <yourchaitu@gmail.com>
2024-08-30 01:00:32 +0530

    hide entire div instead of menu

--- apps/ui/src/views/My/Explore.vue
+++ apps/ui/src/views/My/Explore.vue
@@ -41,7 +41,7 @@ watchEffect(() => setTitle('Explore'));
 </script>
 
 <template>
-  <div class="flex justify-between">
+  <div class="flex justify-between hidden">
     <div class="flex flex-row p-4 space-x-2">
       <UiSelectDropdown
         v-model="protocol"
@@ -49,7 +49,6 @@ watchEffect(() => setTitle('Explore'));
         gap="12px"
         placement="left"
         :items="protocols"
-        class="invisible"
       />
     </div>
   </div>
