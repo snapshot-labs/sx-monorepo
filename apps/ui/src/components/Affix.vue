ccac6f9de5fe5428786d2639e0b9fac530cc44d6
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-09-02 02:22:18 +0900

    feat: new affix component

---
+++ apps/ui/src/components/Affix.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+const wrapperEl = ref<HTMLDivElement | null>(null);
+const lastScrollY = ref(0);
+const initialTopOffset = ref<number | null>(null);
+const topOffset = ref(0);
+const init = ref<boolean>(false);
+
+function handleScroll() {
+  if (!wrapperEl.value) return;
+
+  const innerEl = wrapperEl.value.getElementsByTagName(
+    'div'
+  )[0] as HTMLDivElement;
+  const windowHeight = window.innerHeight;
+  const innerHeight = innerEl.getBoundingClientRect().height;
+  const innerBottom = innerEl.getBoundingClientRect().bottom;
+  const scrollOffset = window.scrollY - lastScrollY.value;
+  const scrollDirection = lastScrollY.value > window.scrollY ? 'up' : 'down';
+
+  lastScrollY.value = window.scrollY;
+
+  if (!init.value) {
+    initialTopOffset.value = wrapperEl.value.getBoundingClientRect().top;
+    topOffset.value = initialTopOffset.value;
+    innerEl.classList.add('relative', 'md:sticky');
+    init.value = true;
+  }
+
+  if (innerHeight >= windowHeight) {
+    const newTopOffset = topOffset.value - scrollOffset;
+
+    if (scrollDirection === 'down' && innerBottom > windowHeight) {
+      topOffset.value = Math.max(newTopOffset, -(innerHeight - windowHeight));
+    } else if (scrollDirection === 'up') {
+      topOffset.value = Math.min(newTopOffset, initialTopOffset.value!);
+    }
+  } else {
+    topOffset.value = initialTopOffset.value!;
+  }
+
+  innerEl.style.top = `${topOffset.value}px`;
+}
+
+onMounted(() => {
+  handleScroll();
+  window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => window.removeEventListener('scroll', handleScroll));
+</script>
+
+<template>
+  <div ref="wrapperEl">
+    <slot />
+  </div>
+</template>
