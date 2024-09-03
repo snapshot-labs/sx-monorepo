e1be652e83652b71f45576bb31e9c4accfa4392b
Chaitanya <yourchaitu@gmail.com>
2024-09-03 19:45:06 +0530

    feat: Show discussions in app (#691)
    * feat: Show discussions in app
    
    * add header to discussion topic
    
    * Move onMounted to last
    
    * fix(ux): button should be focusable only once, and icons not shrinkable
    
    * fix(ui): center content
    
    * refactor: avoid duplicate code
    
    * fix: reload discussion on url topic ID change
    
    * fix(ux): show message on load error
    
    * revert: revert center alignement
    
    * fix: fix typo
    
    * fix: keep sidebar discussion link as active on topic page
    
    * Fix max width
    
    * Change max width similar to proposal page and center align
    
    * combine in to single component
    
    * Remove unused component
    
    * get posts in single call
    
    ---------
    
    Co-authored-by: Wan <495709+wa0x6e@users.noreply.github.com>
    Co-authored-by: Less <bonustrack@users.noreply.github.com>

--- apps/ui/src/router.ts
+++ apps/ui/src/router.ts
@@ -9,7 +9,6 @@ import Home from '@/views/My/Home.vue';
 import Notifications from '@/views/My/Notifications.vue';
 import My from '@/views/My.vue';
 import Network from '@/views/Network.vue';
-import ProposalDiscussion from '@/views/Proposal/Discussion.vue';
 import ProposalOverview from '@/views/Proposal/Overview.vue';
 import ProposalVotes from '@/views/Proposal/Votes.vue';
 import Proposal from '@/views/Proposal.vue';
@@ -30,6 +29,7 @@ import SpaceUserProposals from '@/views/SpaceUser/Proposals.vue';
 import SpaceUserStatement from '@/views/SpaceUser/Statement.vue';
 import SpaceUserVotes from '@/views/SpaceUser/Votes.vue';
 import SpaceUser from '@/views/SpaceUser.vue';
+import Topic from '@/views/Topic.vue';
 import User from '@/views/User.vue';
 
 const routes: any[] = [
@@ -45,6 +45,11 @@ const routes: any[] = [
         path: 'discussions',
         name: 'space-discussions',
         component: SpaceDiscussions
+      },
+      {
+        path: 'discussions/:topic',
+        name: 'space-discussions-topic',
+        component: Topic
       },
       { path: 'search', name: 'space-search', component: SpaceSearch },
       {
@@ -99,7 +104,7 @@ const routes: any[] = [
       {
         path: 'discussion',
         name: 'proposal-discussion',
-        component: ProposalDiscussion
+        component: Topic
       }
     ]
   },
---
+++ apps/ui/src/views/Topic.vue
@@ -0,0 +1,146 @@
+<script setup lang="ts">
+import {
+  loadSingleTopic,
+  Reply,
+  SPACES_DISCUSSIONS,
+  Topic
+} from '@/helpers/discourse';
+import turndownService from '@/helpers/turndownService';
+import { _rt, sanitizeUrl } from '@/helpers/utils';
+import { Proposal, Space } from '@/types';
+
+const props = defineProps<{ proposal?: Proposal; space?: Space }>();
+
+const { setTitle } = useTitle();
+const route = useRoute();
+
+const replies = ref<Reply[]>([]);
+const topic = ref<Topic | null>(null);
+const failed = ref(false);
+const loading = ref(false);
+const loaded = ref(false);
+
+const topicId = computed(() => route.params.topic as string);
+
+const discussion = computed(() => {
+  if (props.proposal) return sanitizeUrl(props.proposal.discussion);
+  if (props.space)
+    return SPACES_DISCUSSIONS[
+      `${props.space.network}:${props.space.id}`
+    ]?.replace(/\/c\/[^\/]+\/\d+$/, `/t/${topicId.value}`);
+  return '';
+});
+
+const toMarkdown = computed(() =>
+  turndownService({ discussion: discussion.value || '' })
+);
+
+async function loadTopic(url: string) {
+  try {
+    failed.value = false;
+    loaded.value = false;
+    loading.value = true;
+    topic.value = await loadSingleTopic(url);
+    replies.value = topic.value.posts;
+  } catch (e) {
+    console.error(e);
+    failed.value = true;
+  } finally {
+    loaded.value = true;
+    loading.value = false;
+  }
+}
+
+watchEffect(() => setTitle(`${topic.value?.title || 'Discussions'}`));
+watch(discussion, async newDiscussion => {
+  if (newDiscussion) await loadTopic(newDiscussion);
+});
+
+onMounted(async () => {
+  if (discussion.value) await loadTopic(discussion.value);
+});
+</script>
+
+<template>
+  <div>
+    <div v-if="discussion" class="p-4 bg-skin-bg border-b">
+      <div class="max-w-[710px] mx-auto">
+        <a :href="discussion" target="_blank" tabindex="-1">
+          <UiButton class="flex items-center gap-2 w-full justify-center">
+            <IC-discourse class="size-[22px] shrink-0" />
+            Join the discussion
+            <IH-arrow-sm-right class="-rotate-45 shrink-0" />
+          </UiButton>
+        </a>
+      </div>
+    </div>
+    <div v-if="loading" class="text-center p-4">
+      <UiLoading />
+    </div>
+    <div
+      v-else-if="failed"
+      class="flex items-center text-skin-link space-x-2 p-4"
+    >
+      <IH-exclamation-circle class="shrink-0" />
+      <span>Error while loading the topic.</span>
+    </div>
+    <div v-if="loaded && !failed" class="px-4">
+      <h1 class="text-[40px] leading-[1.1em] max-w-[710px] mx-auto pt-5">
+        {{ topic?.title }}
+      </h1>
+      <div
+        v-for="(reply, i) in replies"
+        :key="i"
+        class="py-4 border-b last:border-b-0"
+      >
+        <div class="max-w-[710px] mx-auto">
+          <div class="flex gap-2.5 items-center">
+            <a
+              :href="reply.user_url"
+              target="_blank"
+              class="shrink-0 rounded-full"
+            >
+              <img
+                :src="reply.avatar_template"
+                class="rounded-full inline-block bg-skin-border size-[32px]"
+              />
+            </a>
+            <div class="flex flex-col leading-4 gap-1">
+              <a :href="reply.user_url" target="_blank" v-text="reply.name" />
+              <span
+                class="text-skin-text text-sm"
+                v-text="_rt(reply.created_at)"
+              />
+            </div>
+          </div>
+          <UiMarkdown
+            class="text-md pt-3 pb-2"
+            :body="toMarkdown(reply.cooked)"
+          />
+          <div class="text-sm space-x-2.5 flex">
+            <div class="items-center flex gap-1">
+              <IH-thumb-up /> {{ reply.like_count }}
+            </div>
+            <div class="items-center flex gap-1">
+              <IH-annotation /> {{ reply.reply_count }}
+            </div>
+            <div class="items-center flex gap-1">
+              <IH-eye /> {{ reply.reads }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="loaded && !failed && discussion" class="p-4 bg-skin-bg border-y">
+      <div class="max-w-[710px] mx-auto">
+        <a :href="discussion" target="_blank" tabindex="-1">
+          <UiButton class="flex items-center gap-2 w-full justify-center">
+            <IC-discourse class="size-[22px] shrink-0" />
+            Reply
+            <IH-arrow-sm-right class="-rotate-45 shrink-0" />
+          </UiButton>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
