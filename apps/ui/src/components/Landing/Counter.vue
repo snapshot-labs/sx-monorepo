fcc53aa44900ee085f25f1237724866535de3354
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-09-03 18:18:58 +0900

    fix(ux): make Landing/Counter responsive

--- apps/ui/src/components/Landing/Counter.vue
+++ apps/ui/src/components/Landing/Counter.vue
@@ -1,33 +1,26 @@
 <template>
   <div class="border-b">
-    <UiContainer class="!max-w-screen-lg">
-      <div class="grid grid-cols-4 text-md -mx-4">
-        <div class="border-r">
-          <div class="p-4">
-            <h3 class="inline-block disp !text-[36px] mb-2">5k</h3>
-            <div class="eyebrow">DAOs</div>
-          </div>
+    <UiContainer class="!max-w-screen-lg !px-0">
+      <div class="grid grid-cols-2 sm:grid-cols-4 text-md">
+        <div class="border-r border-b sm:border-b-0 p-4 space-y-2">
+          <h3 class="disp !text-[36px]">5k</h3>
+          <div class="eyebrow">DAOs</div>
         </div>
-        <div class="border-r">
-          <div class="p-4">
-            <h3 class="inline-block disp !text-[36px] mb-2">41k</h3>
-            <div class="eyebrow">Proposals</div>
-          </div>
+        <div
+          class="border-r-0 sm:border-r border-b sm:border-b-0 p-4 space-y-2"
+        >
+          <h3 class="disp !text-[36px]">41k</h3>
+          <div class="eyebrow">Proposals</div>
         </div>
-        <div class="border-r">
-          <div class="p-4">
-            <h3 class="inline-block disp !text-[36px] mb-2">31m</h3>
-            <div class="eyebrow">Votes</div>
-          </div>
+        <div class="border-r p-4 space-y-2">
+          <h3 class="disp !text-[36px]">31m</h3>
+          <div class="eyebrow">Votes</div>
         </div>
-        <div>
-          <div class="p-4">
-            <h3 class="inline-block disp !text-[36px] mb-2">3.6m</h3>
-            <div class="eyebrow">Voters</div>
-          </div>
+        <div class="p-4 space-y-2">
+          <h3 class="disp !text-[36px]">3.6m</h3>
+          <div class="eyebrow">Voters</div>
         </div>
       </div>
     </UiContainer>
   </div>
 </template>
-<script setup lang="ts"></script>
