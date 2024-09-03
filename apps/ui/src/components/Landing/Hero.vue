338c4259354043532863655e5d420836d231ae77
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-09-03 23:56:32 +0900

    fix: remove useless div

--- apps/ui/src/components/Landing/Hero.vue
+++ apps/ui/src/components/Landing/Hero.vue
@@ -9,9 +9,6 @@
       class="relative w-full !max-w-screen-lg flex items-center py-8"
     >
       <div class="max-w-[560px]">
-        <div
-          class="absolute top-[200px] right-[300px] space-y-4 text-center font-bold"
-        ></div>
         <div class="eyebrow mb-3">Turn chaos to consensus</div>
         <h1 class="mb-5 mono !text-[56px]">
           The governance stack for your organization
