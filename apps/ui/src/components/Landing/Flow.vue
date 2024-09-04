6ea51c8afb3e7745f5aa3c58c069eaa59b13a8f9
less <fabien@bonustrack.co>
2024-09-04 17:38:00 +0700

    chore: add ecosystem image, improve flow

--- apps/ui/src/components/Landing/Flow.vue
+++ apps/ui/src/components/Landing/Flow.vue
@@ -8,23 +8,25 @@ const STEPS = [
   {
     title: 'Discuss',
     about:
-      'Follow your ongoing community discussions from within the Snapshot app.',
+      "Stay connected to your community's conversations with our Discourse integration. Access forum discussions directly within Snapshot, participate in debates and refine ideas before they become proposals.",
     icon: ICAnnotation
   },
   {
     title: 'Delegate',
-    about: 'Become a delegate and get voting power from your peers.',
+    about:
+      'Amplify your governance impact. Become a delegate or choose one to represent your interests. Track delegate performance, read their statements, and make informed decisions about voting power allocation.',
     icon: ISLightningBolt
   },
   {
     title: 'Vote',
     about:
-      'Exercise your decision making power to shape the future of your DAO.',
+      "Shape your DAO's future with ease. Cast votes on key proposals through our intuitive interface. Stay informed with AI summaries, analyze trends, and see real-time results – all in one place.",
     icon: ISCheck
   },
   {
     title: 'Execute',
-    about: 'Execute transactions onchain once a proposal is passed.',
+    about:
+      'Transform decisions into action. After a proposal passes, seamlessly implement changes on-chain. Create and review transaction batches, then execute with confidence.',
     icon: ISPlay
   }
 ];
@@ -39,18 +41,23 @@ const STEPS = [
     </UiContainer>
     <div class="border-y">
       <UiContainer class="!max-w-screen-lg !px-0">
-        <div class="grid grid-cols-1 lg:grid-cols-4 text-md">
+        <div class="grid grid-cols-1 lg:grid-cols-2 text-md">
           <div
             v-for="(step, i) in STEPS"
             :key="i"
-            class="border-b last:border-b-0 lg:border-b-0 lg:border-r lg:last:border-r-0"
+            class="border-b border-r-0 last:border-b-0 lg:border-r lg:[&:nth-child(2n)]:border-r-0 lg:[&:nth-child(n+3)]:border-b-0"
           >
-            <div class="px-4 py-5">
-              <component
-                :is="step.icon"
-                class="text-skin-link size-[24px] mb-4"
-              />
-              <h3 class="mb-2 font-heading text-[19px]" v-text="step.title" />
+            <div class="px-4 pt-5 pb-7">
+              <div class="flex space-x-2.5">
+                <component
+                  :is="step.icon"
+                  class="text-skin-link size-[24px] mb-4 inline-block"
+                />
+                <h2
+                  class="mb-3 font-heading text-[22px] inline-block"
+                  v-text="step.title"
+                />
+              </div>
               <div v-text="step.about" />
             </div>
           </div>
