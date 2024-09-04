c33a695f2821d430ecd3c8e819112ddd0a698de7
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-09-04 23:16:01 +0900

    Revert "fix: revert to backdrop-filter"
    This reverts commit 1989fdf8c67eb066ed0c9cf57e9cc5e014dd7a2a.

--- apps/ui/src/components/SpaceCover.vue
+++ apps/ui/src/components/SpaceCover.vue
@@ -29,34 +29,16 @@ const cb = computed(() => getCacheHash(props.space.cover));
   />
   <div v-else class="w-full h-full overflow-hidden">
     <div
-      class="space-fallback-cover"
-      :class="[size]"
+      class="bg-cover bg-center w-full h-full"
       :style="{
         'background-image': `url(${getStampUrl(
           offchainNetworks.includes(space.network) ? 'space' : 'space-sx',
           space.id,
           50,
           getCacheHash(space.avatar)
-        )}`
+        )}`,
+        filter: `blur(${size === 'lg' ? '100' : '50'}px) contrast(0.9) saturate(1.3)`
       }"
     />
   </div>
 </template>
-
-<style lang="scss" scoped>
-.space-fallback-cover {
-  @apply bg-cover bg-center w-full h-full;
-
-  &.lg:after {
-    backdrop-filter: blur(150px) contrast(0.9) saturate(1.3);
-  }
-
-  &::after {
-    // NOTE: Width and height are scaled up so that blur will always cover underlying images
-    // At certain sizes we get 1px that is not blurred
-    @apply absolute h-[101%] w-[101%] pointer-events-none;
-    content: '';
-    backdrop-filter: blur(50px) contrast(0.9) saturate(1.3);
-  }
-}
-</style>
