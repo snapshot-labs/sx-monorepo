ca609d62919183fd9901b617dfe7312f2f343ce4
Chaitanya <yourchaitu@gmail.com>
2024-09-05 16:36:03 +0530

    fix: handle empty post (#715)
    * fix: handle empty post
    
    * handle posts check inside formatPosts
    
    * handle posts check inside formatPosts

--- apps/ui/src/helpers/discourse.ts
+++ apps/ui/src/helpers/discourse.ts
@@ -109,7 +109,9 @@ export async function loadTopics(url: string): Promise<Topic[]> {
   });
 }
 
-function formatPosts(posts, baseUrl) {
+function formatPosts(posts: any[], baseUrl: string): Reply[] {
+  if (!posts?.length) return [];
+
   return posts.map(post => {
     post.avatar_template = post.avatar_template.replace('{size}', '64');
     if (post.avatar_template.startsWith('/'))
@@ -135,8 +137,7 @@ export async function loadSingleTopic(url: string): Promise<Topic> {
   const topic = await res.json();
 
   topic.posts_count--;
-  topic.posts = formatPosts(topic.post_stream.posts, baseUrl);
-
+  topic.posts = formatPosts(topic.post_stream?.posts, baseUrl);
   return topic;
 }
 
@@ -150,5 +151,5 @@ export async function loadReplies(url: string): Promise<Reply[]> {
   );
   const data = await res.json();
 
-  return formatPosts(data.post_stream.posts, baseUrl);
+  return formatPosts(data?.post_stream?.posts, baseUrl);
 }
