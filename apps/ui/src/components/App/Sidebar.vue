5a15be2096036cd5e094f58b4f21de0c73e35455
ChaituVR <yourchaitu@gmail.com>
2024-09-09 20:04:40 +0530

    fix: Change home icon in sidebar

--- apps/ui/src/components/App/Sidebar.vue
+++ apps/ui/src/components/App/Sidebar.vue
@@ -10,7 +10,7 @@ const followedSpacesStore = useFollowedSpacesStore();
     class="w-[72px] flex flex-col border-r fixed left-0 inset-y-0 text-center h-screen"
   >
     <router-link :to="{ name: 'my-home' }" class="h-[72px] block">
-      <IH-stop class="inline-block my-4 size-[32px] text-skin-link" />
+      <IH-home class="inline-block my-4 size-[32px] text-skin-link" />
     </router-link>
     <div
       class="bg-gradient-to-b from-skin-bg top-[72px] h-[8px] w-[71px] absolute z-10"
