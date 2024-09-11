275802268a60ac88b8ed76bf8c71292b29039c96
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2024-09-11 13:44:43 +0200

    feat: disable estimation for initialize EthTx calls
    This allows those calls to be set up in Safe even without funds.

--- packages/sx.js/src/clients/starknet/ethereum-tx/index.ts
+++ packages/sx.js/src/clients/starknet/ethereum-tx/index.ts
@@ -339,7 +339,8 @@ export class EthereumTx {
     const { overall_fee } = await this.estimateProposeFee(signer, data);
 
     const promise = commitContract.commit(data.authenticator, hash, {
-      value: overall_fee
+      value: overall_fee,
+      gasLimit: opts.noWait ? 0 : undefined
     });
     const res = opts.noWait ? null : await promise;
 
@@ -369,7 +370,8 @@ export class EthereumTx {
     const { overall_fee } = await this.estimateVoteFee(signer, data);
 
     const promise = commitContract.commit(data.authenticator, hash, {
-      value: overall_fee
+      value: overall_fee,
+      gasLimit: opts.noWait ? 0 : undefined
     });
     const res = opts.noWait ? null : await promise;
 
@@ -399,7 +401,8 @@ export class EthereumTx {
     const { overall_fee } = await this.estimateUpdateProposalFee(signer, data);
 
     const promise = commitContract.commit(data.authenticator, hash, {
-      value: overall_fee
+      value: overall_fee,
+      gasLimit: opts.noWait ? 0 : undefined
     });
     const res = opts.noWait ? null : await promise;
 
