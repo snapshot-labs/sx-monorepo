bc14ba7f736f9fa20523d8247ed3eec8a4dc9ca7
Wan <495709+wa0x6e@users.noreply.github.com>
2024-09-12 23:35:51 +0900

    style: fix lint (#752)
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -1,13 +1,13 @@
 import path from 'path';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
-import Icons from 'unplugin-icons/vite';
-import IconsResolver from 'unplugin-icons/resolver';
-import { FileSystemIconLoader } from 'unplugin-icons/loaders';
-import visualizer from 'rollup-plugin-visualizer';
 import inject from '@rollup/plugin-inject';
+import vue from '@vitejs/plugin-vue';
+import visualizer from 'rollup-plugin-visualizer';
+import AutoImport from 'unplugin-auto-import/vite';
+import { FileSystemIconLoader } from 'unplugin-icons/loaders';
+import IconsResolver from 'unplugin-icons/resolver';
+import Icons from 'unplugin-icons/vite';
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
 
 const ELECTRON = process.env.ELECTRON || false;
 
