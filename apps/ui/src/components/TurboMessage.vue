6a337d141b2c4ce05241e5a9401e781dc8f4a0e4
ChaituVR <yourchaitu@gmail.com>
2024-09-16 20:03:02 +0530

    fix alignments  of turbo hint message

--- apps/ui/src/components/TurboMessage.vue
+++ apps/ui/src/components/TurboMessage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { TURBO_URL } from '@/helpers/turbo';
+
 defineProps<{
   text: string;
 }>();
@@ -7,14 +8,17 @@ defineProps<{
 
 <template>
   <div
-    class="my-3 border bg-purple-300/20 border-purple-300 text-purple-400 rounded-lg px-3 py-2"
+    class="flex items-center gap-2 mb-3 border bg-purple-300/20 border-purple-300 text-purple-400 rounded-lg px-3 py-2"
   >
-    <IH-fire class="inline-block mr-1" /> {{ text }}
-    <a
-      :href="TURBO_URL"
-      target="_blank"
-      class="text-purple-400 underline font-semibold"
-      >Turbo</a
-    >.
+    <IH-fire class="shrink-0" />
+    <div class="leading-6">
+      {{ text }}
+      <a
+        :href="TURBO_URL"
+        target="_blank"
+        class="text-purple-400 underline font-semibold"
+        >Turbo</a
+      >.
+    </div>
   </div>
 </template>
