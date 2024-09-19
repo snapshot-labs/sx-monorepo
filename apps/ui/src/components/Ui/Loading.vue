cc22f5ca2c44f3ad0e64177b5f1390759b799bc0
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-09-19 22:03:00 +0900

    feat: add size props to UiLoading

--- apps/ui/src/components/Ui/Loading.vue
+++ apps/ui/src/components/Ui/Loading.vue
@@ -4,11 +4,13 @@ withDefaults(
     inverse?: boolean;
     width?: number;
     height?: number;
+    size?: number;
   }>(),
   {
     inverse: false,
     width: 20,
-    height: 20
+    height: 20,
+    size: 20
   }
 );
 </script>
@@ -17,8 +19,8 @@ withDefaults(
   <span class="loading" :class="{ inverse: !!inverse }">
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      :width="width"
-      :height="height"
+      :width="width || size"
+      :height="height || size"
       class="inline-block align-middle"
       viewBox="0 0 24 24"
     >
