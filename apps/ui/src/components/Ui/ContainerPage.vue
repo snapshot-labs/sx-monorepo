3b371aad87fe182117b2ec059e6fe66f33479750
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-09-23 14:51:40 +0400

    fix: fix sidebar not taking full height

--- apps/ui/src/components/Ui/ContainerPage.vue
+++ apps/ui/src/components/Ui/ContainerPage.vue
@@ -10,6 +10,8 @@ defineProps<{
       <UiLoading v-if="loading" class="block p-4" />
       <slot v-else />
     </div>
-    <div class="hidden xl:block w-[240px] border-l" />
+    <div class="hidden xl:block w-[240px]">
+      <div class="w-[240px] fixed border-l top-[72px] bottom-0 right-0" />
+    </div>
   </div>
 </template>
