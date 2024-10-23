d550b6efc09d6c42030245e42e452ed9de9c9415
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-10-23 12:52:44 +0400

    fix: use lower z-index

--- apps/ui/src/components/PickerLabel.vue
+++ apps/ui/src/components/PickerLabel.vue
@@ -54,7 +54,7 @@ const filteredLabels = computed(() =>
     >
       <PopoverPanel
         focus
-        class="absolute z-[9999] left-0 -mt-2 mx-4 pb-3"
+        class="absolute z-[11] left-0 -mt-2 mx-4 pb-3"
         style="width: calc(100% - 48px)"
         v-bind="panelProps"
       >
