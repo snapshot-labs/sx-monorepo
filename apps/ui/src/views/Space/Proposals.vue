3bc4bab2e6427e3fec1570cc81efa59fa79bff9a
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2024-11-04 14:45:42 +0100

    Revert "feat: filters proposals by label (#910)"
    This reverts commit 23d407ad21d1f7089cb1f1cf6f2e608f1be7d43f.

--- apps/ui/src/views/Space/Proposals.vue
+++ apps/ui/src/views/Space/Proposals.vue
@@ -18,7 +18,6 @@ const route = useRoute();
 const proposalsStore = useProposalsStore();
 
 const state = ref<NonNullable<ProposalsFilter['state']>>('any');
-const labels = ref<string[]>([]);
 
 const selectIconBaseProps = {
   size: 16
@@ -27,17 +26,6 @@ const selectIconBaseProps = {
 const proposalsRecord = computed(
   () => proposalsStore.proposals[`${props.space.network}:${props.space.id}`]
 );
-
-const spaceLabels = computed(() => {
-  if (!props.space.labels) return {};
-
-  return Object.fromEntries(props.space.labels.map(label => [label.id, label]));
-});
-
-function handleClearLabelsFilter(close: () => void) {
-  labels.value = [];
-  close();
-}
 
 async function handleEndReached() {
   if (!proposalsRecord.value?.hasMoreProposals) return;
@@ -49,44 +37,25 @@ function handleFetchVotingPower() {
   fetchVotingPower(props.space);
 }
 
-watchThrottled(
-  [
-    () => route.query.state as string,
-    () => route.query.labels as string[] | string
-  ],
-  ([toState, toLabels]) => {
+watch(
+  [() => route.query.state as string],
+  ([toState]) => {
     state.value = ['any', 'active', 'pending', 'closed'].includes(toState)
       ? (toState as NonNullable<ProposalsFilter['state']>)
       : 'any';
-    let normalizedLabels = toLabels || [];
-    normalizedLabels = Array.isArray(normalizedLabels)
-      ? normalizedLabels
-      : [normalizedLabels];
-    labels.value = normalizedLabels.filter(id => spaceLabels.value[id]);
-
     proposalsStore.reset(props.space.id, props.space.network);
-    proposalsStore.fetch(
-      props.space.id,
-      props.space.network,
-      state.value,
-      labels.value
-    );
+    proposalsStore.fetch(props.space.id, props.space.network, state.value);
   },
-  { throttle: 1000, immediate: true }
+  { immediate: true }
 );
 
 watch(
-  [props.space, state, labels],
-  ([toSpace, toState, toLabels], [fromSpace, fromState, fromLabels]) => {
-    if (
-      toSpace.id !== fromSpace?.id ||
-      toState !== fromState ||
-      toLabels !== fromLabels
-    ) {
+  [props.space, state],
+  ([toSpace, toState], [fromSpace, fromState]) => {
+    if (toSpace.id !== fromSpace?.id || toState !== fromState) {
       const query: LocationQueryRaw = {
         ...route.query,
-        state: toState === 'any' ? undefined : toState,
-        labels: !toLabels?.length ? undefined : toLabels
+        state: toState === 'any' ? undefined : toState
       };
 
       router.push({ query });
@@ -114,10 +83,7 @@ watchEffect(() => setTitle(`Proposals - ${props.space.name}`));
 
 <template>
   <div>
-    <div
-      class="flex justify-between p-4 gap-2 gap-y-3 flex-row"
-      :class="{ 'flex-col-reverse sm:flex-row': space.labels?.length }"
-    >
+    <div class="flex justify-between p-4 gap-2">
       <div class="flex gap-2">
         <UiSelectDropdown
           v-model="state"
@@ -149,56 +115,6 @@ watchEffect(() => setTitle(`Proposals - ${props.space.name}`));
             }
           ]"
         />
-        <div v-if="space.labels?.length" class="sm:relative">
-          <PickerLabel
-            v-model="labels"
-            :labels="space.labels"
-            :button-props="{
-              class: [
-                'flex items-center gap-2 relative rounded-full leading-[100%] min-w-[75px] max-w-[230px] border button h-[42px] top-1 text-skin-link bg-skin-bg'
-              ]
-            }"
-            :panel-props="{ class: 'sm:min-w-[290px] sm:ml-0 !mt-3' }"
-          >
-            <template #button="{ close }">
-              <div
-                class="absolute top-[-10px] bg-skin-bg px-1 left-2.5 text-sm text-skin-text"
-              >
-                Labels
-              </div>
-              <div
-                v-if="labels.length"
-                class="flex gap-1 mx-2.5 overflow-hidden items-center"
-              >
-                <ul v-if="labels.length" class="flex gap-1 mr-4">
-                  <li v-for="id in labels" :key="id">
-                    <UiProposalLabel
-                      :label="spaceLabels[id].name"
-                      :color="spaceLabels[id].color"
-                    />
-                  </li>
-                </ul>
-                <div
-                  class="flex items-center absolute rounded-r-full right-[1px] pr-2 h-[23px] bg-skin-bg"
-                >
-                  <div
-                    class="block w-2 -ml-2 h-full bg-gradient-to-l from-skin-bg"
-                  />
-                  <button
-                    v-if="labels.length"
-                    class="text-skin-text rounded-full hover:text-skin-link"
-                    title="Clear all labels"
-                    @click.stop="handleClearLabelsFilter(close)"
-                    @keydown.enter.stop="handleClearLabelsFilter(close)"
-                  >
-                    <IH-x-circle size="16" />
-                  </button>
-                </div>
-              </div>
-              <span v-else class="px-3 text-skin-link">Any</span>
-            </template>
-          </PickerLabel>
-        </div>
       </div>
       <div class="flex gap-2 truncate">
         <IndicatorVotingPower
