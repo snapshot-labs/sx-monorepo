707bc7d8b497016bc3566405275eca1b96e1dfce
bartosz-io <pietrucha.bartosz@gmail.com>
2024-11-07 18:00:56 +0100

    Display names, remove label, load members at once

--- apps/ui/src/views/Space/Members.vue
+++ apps/ui/src/views/Space/Members.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import removeMarkdown from 'remove-markdown';
+import { getNames } from '@/helpers/stamp';
 import { shorten } from '@/helpers/utils';
 import { getNetwork } from '@/networks';
 import { Space } from '@/types';
@@ -10,7 +11,8 @@ const { addNotification } = useUiStore();
 const { setTitle } = useTitle();
 
 const loaded = ref(false);
-const controller = ref<string>('');
+const roles = ref<{ data: string[]; label: string }[]>([]);
+const names = ref<Record<string, string>>({});
 const statements = ref<Record<string, string>>({});
 const abouts = ref<Record<string, string>>({});
 const network = computed(() => getNetwork(props.space.network));
@@ -21,15 +23,15 @@ const authors = props.space.additionalRawData?.members || [];
 
 async function loadMembersData() {
   try {
-    controller.value = await network.value.helpers
+    const controller = await network.value.helpers
       .getSpaceController(props.space)
       .then(c => c.toLowerCase());
 
-    const allUsers = [...admins, ...moderators, ...authors].map(u =>
+    const allUsers = [controller, ...admins, ...moderators, ...authors].map(u =>
       u.toLowerCase()
     );
 
-    if (!allUsers.length) return;
+    names.value = await getNames(allUsers);
 
     const { statements: userStatements, users } =
       await network.value.api.loadStatementsAndUsers(
@@ -54,6 +56,13 @@ async function loadMembersData() {
       {} as Record<string, string>
     );
 
+    roles.value = [
+      { data: [controller], label: 'Controller' },
+      { data: admins, label: 'Admin(s)' },
+      { data: moderators, label: 'Moderator(s)' },
+      { data: authors, label: 'Authors(s)' }
+    ] as const;
+
     loaded.value = true;
   } catch (e) {
     addNotification('error', 'Failed to load members data.');
@@ -63,57 +72,51 @@ async function loadMembersData() {
 watchEffect(() => setTitle(`Memebers - ${props.space.name}`));
 
 onMounted(loadMembersData);
-
-const roles = [
-  { data: admins, label: 'Admin(s)' },
-  { data: moderators, label: 'Moderator(s)' },
-  { data: authors, label: 'Authors(s)' }
-] as const;
 </script>
 
 <template>
-  <template v-for="role in roles" :key="role.label">
+  <div v-if="!loaded" class="px-4 py-4 block">
+    <UiLoading class="block" />
+  </div>
+  <template v-for="role in roles" v-else :key="role.label">
     <UiLabel v-if="role.data?.length" :label="role.label" />
-    <div
-      v-for="(user, i) in role.data"
-      :key="`${role.label}-${i}`"
-      class="border-b flex space-x-1"
-    >
+    <div class="text-left table-fixed w-full">
       <div
-        class="flex items-center pl-4 py-3 gap-x-3 leading-[22px] min-w-[200px] truncate"
+        v-for="(user, i) in role.data"
+        :key="`${role.label}-${i}`"
+        class="border-b flex space-x-1"
       >
-        <UiStamp :id="user" :size="32" />
-        <AppLink
-          :to="{
-            name: 'space-user-statement',
-            params: { space: `${space.network}:${space.id}`, user: user }
-          }"
-          class="overflow-hidden"
+        <div
+          class="flex items-center pl-4 py-3 gap-x-3 leading-[22px] min-w-[220px] truncate"
         >
-          <div
-            class="text-[17px] text-skin-heading truncate"
-            v-text="shorten(user)"
-          />
-          <div
-            v-if="user === controller"
-            class="rounded-full w-fit shrink-0 flex bg-orange-500 text-white"
+          <UiStamp :id="user" :size="32" />
+          <AppLink
+            :to="{
+              name: 'space-user-statement',
+              params: { space: `${space.network}:${space.id}`, user: user }
+            }"
+            class="overflow-hidden"
           >
-            <span class="text-sm leading-[10px] px-2 py-[4px]">
-              Controller
-            </span>
-          </div>
-        </AppLink>
-      </div>
-      <div
-        class="hidden sm:flex items-center grow text-[17px] overflow-hidden leading-[22px] text-skin-text"
-      >
-        <UiLoading v-if="!loaded" class="px-4 py-3 block" />
-        <span
-          v-else-if="statements[user] || abouts[user]"
-          class="line-clamp-2 max-h-[44px]"
+            <h4
+              class="text-skin-link truncate"
+              v-text="names[user] || shorten(user)"
+            />
+            <h4
+              class="text-[17px] text-skin-text truncate"
+              v-text="shorten(user)"
+            />
+          </AppLink>
+        </div>
+        <div
+          class="hidden sm:flex items-center grow text-[17px] px-4 overflow-hidden leading-[22px] text-skin-text"
         >
-          {{ shorten(removeMarkdown(statements[user] || abouts[user]), 250) }}
-        </span>
+          <span
+            v-if="statements[user] || abouts[user]"
+            class="line-clamp-2 max-h-[44px]"
+          >
+            {{ shorten(removeMarkdown(statements[user] || abouts[user]), 250) }}
+          </span>
+        </div>
       </div>
     </div>
   </template>
