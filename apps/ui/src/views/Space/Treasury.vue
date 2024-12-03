7253f12afa8dca4bd3eca421c7929231031a97a3
Wan <495709+wa0x6e@users.noreply.github.com>
2024-12-03 17:26:07 +0800

    fix: fix gap in proposal sidebar bottom (#1003)
    * fix: fix gap in proposal sidebar bottom
    
    * fix: move global bottom padding to each view
    
    * fix: fix gap in votes page bottom
    
    ---------
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- apps/ui/src/views/Space/Treasury.vue
+++ apps/ui/src/views/Space/Treasury.vue
@@ -49,42 +49,44 @@ watchEffect(() => {
 </script>
 
 <template>
-  <UiScrollerHorizontal
-    v-if="props.space.treasuries.length !== 1"
-    ref="treasuriesList"
-    class="z-40 sticky top-[71px] lg:top-[72px]"
-    with-buttons
-    gradient="xxl"
-  >
-    <div class="flex px-4 space-x-3 bg-skin-bg border-b min-w-max">
-      <AppLink
-        v-for="(treasury, i) in props.space.treasuries"
-        :key="i"
-        :to="{
-          name: 'space-treasury',
-          params: {
-            index: i + 1,
-            tab: 'tokens'
-          }
-        }"
-        :aria-active="activeTreasuryId === i"
-      >
-        <UiLink
-          :is-active="activeTreasuryId === i"
-          :text="treasury.name || shorten(treasury.address)"
-        />
-      </AppLink>
+  <div>
+    <UiScrollerHorizontal
+      v-if="props.space.treasuries.length !== 1"
+      ref="treasuriesList"
+      class="z-40 sticky top-[71px] lg:top-[72px]"
+      with-buttons
+      gradient="xxl"
+    >
+      <div class="flex px-4 space-x-3 bg-skin-bg border-b min-w-max">
+        <AppLink
+          v-for="(treasury, i) in props.space.treasuries"
+          :key="i"
+          :to="{
+            name: 'space-treasury',
+            params: {
+              index: i + 1,
+              tab: 'tokens'
+            }
+          }"
+          :aria-active="activeTreasuryId === i"
+        >
+          <UiLink
+            :is-active="activeTreasuryId === i"
+            :text="treasury.name || shorten(treasury.address)"
+          />
+        </AppLink>
+      </div>
+    </UiScrollerHorizontal>
+    <SpaceTreasury
+      v-if="treasuryData"
+      :key="activeTreasuryId"
+      :space="space"
+      :treasury-data="treasuryData"
+      :extra-contacts="props.space.treasuries"
+    />
+    <div v-else class="flex items-center px-4 py-3 text-skin-link gap-2">
+      <IH-exclamation-circle />
+      <span v-text="'Treasury not found.'" />
     </div>
-  </UiScrollerHorizontal>
-  <SpaceTreasury
-    v-if="treasuryData"
-    :key="activeTreasuryId"
-    :space="space"
-    :treasury-data="treasuryData"
-    :extra-contacts="props.space.treasuries"
-  />
-  <div v-else class="flex items-center px-4 py-3 text-skin-link gap-2">
-    <IH-exclamation-circle />
-    <span v-text="'Treasury not found.'" />
   </div>
 </template>
