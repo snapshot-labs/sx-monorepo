44c9e3d5b3dffb35c0a774dbb3f8d27b390449a1
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2025-01-15 01:58:00 +0400

    feat(ui): add new step 0 to select onchain/offchain type when creating a space

---
+++ apps/ui/src/components/SpaceCreateOffchain.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { NetworkID } from '@/types';
+
+const { createSpaceRaw } = useActions();
+
+const networkId = ref<NetworkID>('s');
+const spaceId = ref<string>('');
+
+async function create() {
+  const result = await createSpaceRaw(
+    networkId.value,
+    spaceId.value,
+    JSON.stringify({})
+  );
+}
+</script>
+
+<template>
+  <div></div>
+</template>
