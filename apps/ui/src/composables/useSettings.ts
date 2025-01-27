756d84f354d20cf2d3c0285b966a4fb58afbfe69
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2025-01-27 18:07:55 +0400

    fix: enforce settings types

--- apps/ui/src/composables/useSettings.ts
+++ apps/ui/src/composables/useSettings.ts
@@ -1,7 +1,32 @@
 import { clone } from '@/helpers/utils';
 import { getNetwork, metadataNetwork } from '@/networks';
 
-const DEFAULT_LIMITS_SETTINGS: Record<string, number> = {
+type LIMITS =
+  | 'space.active_proposal_limit_per_author'
+  | 'space.default.body_limit'
+  | 'space.default.choices_limit'
+  | 'space.default.proposal_limit_per_day'
+  | 'space.default.proposal_limit_per_month'
+  | 'space.default.strategies_limit'
+  | 'space.ecosystem.proposal_limit_per_day'
+  | 'space.ecosystem.proposal_limit_per_month'
+  | 'space.flagged.proposal_limit_per_day'
+  | 'space.flagged.proposal_limit_per_month'
+  | 'space.turbo.body_limit'
+  | 'space.turbo.choices_limit'
+  | 'space.turbo.proposal_limit_per_day'
+  | 'space.turbo.proposal_limit_per_month'
+  | 'space.turbo.strategies_limit'
+  | 'space.verified.body_limit'
+  | 'space.verified.choices_limit'
+  | 'space.verified.proposal_limit_per_day'
+  | 'space.verified.proposal_limit_per_month'
+  | 'space.verified.strategies_limit'
+  | 'user.default.follow_limit';
+
+type LISTS = 'space.ecosystem.list';
+
+const DEFAULT_LIMITS_SETTINGS: Record<LIMITS, number> = {
   'space.active_proposal_limit_per_author': 0,
   'space.default.body_limit': 0,
   'space.default.choices_limit': 0,
@@ -25,7 +50,7 @@ const DEFAULT_LIMITS_SETTINGS: Record<string, number> = {
   'user.default.follow_limit': 0
 };
 
-const DEFAULT_LISTS_SETTINGS: Record<string, string[]> = {
+const DEFAULT_LISTS_SETTINGS: Record<LISTS, string[]> = {
   'space.ecosystem.list': []
 };
 
