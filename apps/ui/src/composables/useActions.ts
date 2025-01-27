b12c842d32e3a561dfce2388e6d5301846e67f57
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-01-27 16:09:50 +0100

    feat: handle transactions through Safe app (#1041)
    * fix: do not add pending vote for safe votes
    
    * feat: add ModalVoteConfirmOnSafe
    
    * feat: handle proposing through Safe
    
    * fix: only show verifier link if voting on starknet spaces
    
    * refactor: make safeModal global
    
    * feat: use non-blocking flow for updating space settings via Safe
    
    * feat: handle commit tx

--- apps/ui/src/composables/useActions.ts
+++ apps/ui/src/composables/useActions.ts
@@ -57,14 +57,25 @@ export function useActions() {
     };
   }
 
-  function handleSafeEnvelope(envelope: any) {
+  function handleSafeEnvelope(
+    envelope: any,
+    safeAppContext: 'vote' | 'propose' | 'transaction'
+  ) {
     if (envelope !== null) return false;
 
-    uiStore.addNotification('success', 'Transaction set up.');
+    uiStore.openSafeModal({
+      type: safeAppContext,
+      showVerifierLink: false
+    });
+
     return true;
   }
 
-  async function handleCommitEnvelope(envelope: any, networkId: NetworkID) {
+  async function handleCommitEnvelope(
+    envelope: any,
+    networkId: NetworkID,
+    safeAppContext: 'vote' | 'propose' | 'transaction'
+  ) {
     // TODO: it should work with WalletConnect, should be done before L1 transaction is broadcasted
     const network = getNetwork(networkId);
 
@@ -83,10 +94,17 @@ export function useActions() {
         );
       }
 
-      uiStore.addNotification(
-        'success',
-        'Transaction set up. It will be processed once received on L2 network automatically.'
-      );
+      if (envelope.signatureData.commitTxId) {
+        uiStore.addNotification(
+          'success',
+          'Transaction set up. It will be processed once received on L2 network automatically.'
+        );
+      } else {
+        uiStore.openSafeModal({
+          type: safeAppContext,
+          showVerifierLink: true
+        });
+      }
 
       return true;
     }
@@ -97,14 +115,27 @@ export function useActions() {
   async function wrapPromise(
     networkId: NetworkID,
     promise: Promise<any>,
-    opts: { transactionNetworkId?: NetworkID } = {}
+    opts: {
+      transactionNetworkId?: NetworkID;
+      safeAppContext?: 'vote' | 'propose' | 'transaction';
+    } = {}
   ): Promise<string | null> {
     const network = getNetwork(networkId);
 
     const envelope = await promise;
 
-    if (handleSafeEnvelope(envelope)) return null;
-    if (await handleCommitEnvelope(envelope, networkId)) return null;
+    if (handleSafeEnvelope(envelope, opts.safeAppContext ?? 'transaction')) {
+      return null;
+    }
+    if (
+      await handleCommitEnvelope(
+        envelope,
+        networkId,
+        opts.safeAppContext ?? 'transaction'
+      )
+    ) {
+      return null;
+    }
 
     let hash;
     // TODO: unify send/soc to both return txHash under same property
@@ -256,10 +287,13 @@ export function useActions() {
         choice,
         reason,
         app
-      )
+      ),
+      {
+        safeAppContext: 'vote'
+      }
     );
 
-    addPendingVote(proposal.id);
+    if (txHash) addPendingVote(proposal.id);
 
     return txHash;
   }
@@ -287,7 +321,7 @@ export function useActions() {
 
     const network = getNetwork(space.network);
 
-    await wrapPromise(
+    const txHash = await wrapPromise(
       space.network,
       network.actions.propose(
         auth.web3,
@@ -307,10 +341,13 @@ export function useActions() {
         min_end,
         max_end,
         executions
-      )
+      ),
+      {
+        safeAppContext: 'propose'
+      }
     );
 
-    return true;
+    return txHash;
   }
 
   async function updateProposal(
@@ -348,7 +385,10 @@ export function useActions() {
         privacy,
         labels,
         executions
-      )
+      ),
+      {
+        safeAppContext: 'propose'
+      }
     );
 
     return true;
