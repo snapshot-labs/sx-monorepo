1557837c0786a09a147393aaf2f3a9d8183088c2
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2025-01-28 21:08:14 +0400

    revert: revert unrelated changes

--- apps/ui/src/components/FormSpaceStrategies.vue
+++ apps/ui/src/components/FormSpaceStrategies.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { MAX_STRATEGIES } from '@/helpers/turbo';
 import { StrategyConfig } from '@/networks/types';
-import { NetworkID } from '@/types';
+import { NetworkID, Space } from '@/types';
 
 const snapshotChainId = defineModel<number>('snapshotChainId', {
   required: true
@@ -13,7 +13,7 @@ const strategies = defineModel<StrategyConfig[]>('strategies', {
 const props = defineProps<{
   networkId: NetworkID;
   isTicketValid: boolean;
-  space: { turbo: boolean; verified: boolean };
+  space: Space;
 }>();
 
 const strategiesLimit = computed(() => {
@@ -28,42 +28,40 @@ const strategiesLimit = computed(() => {
 </script>
 
 <template>
-  <div>
-    <h4 class="eyebrow mb-2 font-medium">Strategies</h4>
-    <div class="s-box mb-4">
-      <UiSelectorNetwork
-        v-model="snapshotChainId"
-        :definition="{
-          type: 'number',
-          title: 'Network',
-          tooltip:
-            'The default network used for this space. Networks can also be specified in individual strategies',
-          examples: ['Select network'],
-          networkId,
-          networksListKind: 'offchain'
-        }"
-      />
-    </div>
-    <UiContainerSettings
-      :title="`Select up to ${strategiesLimit} strategies`"
-      description="(Voting power is cumulative)"
-    >
-      <UiMessage
-        v-if="!isTicketValid"
-        type="danger"
-        learn-more-link="https://snapshot.mirror.xyz/-uSylOUP82hGAyWUlVn4lCg9ESzKX9QCvsUgvv-ng84"
-        class="mb-3"
-      >
-        In order to use the "ticket" strategy you are required to set a voting
-        validation strategy. This combination reduces the risk of spam and sybil
-        attacks.
-      </UiMessage>
-      <UiStrategiesConfiguratorOffchain
-        v-model:model-value="strategies"
-        :network-id="networkId"
-        :default-chain-id="snapshotChainId"
-        :limit="strategiesLimit"
-      />
-    </UiContainerSettings>
+  <h4 class="eyebrow mb-2 font-medium">Strategies</h4>
+  <div class="s-box mb-4">
+    <UiSelectorNetwork
+      v-model="snapshotChainId"
+      :definition="{
+        type: 'number',
+        title: 'Network',
+        tooltip:
+          'The default network used for this space. Networks can also be specified in individual strategies',
+        examples: ['Select network'],
+        networkId,
+        networksListKind: 'offchain'
+      }"
+    />
   </div>
+  <UiContainerSettings
+    :title="`Select up to ${strategiesLimit} strategies`"
+    description="(Voting power is cumulative)"
+  >
+    <UiMessage
+      v-if="!isTicketValid"
+      type="danger"
+      learn-more-link="https://snapshot.mirror.xyz/-uSylOUP82hGAyWUlVn4lCg9ESzKX9QCvsUgvv-ng84"
+      class="mb-3"
+    >
+      In order to use the "ticket" strategy you are required to set a voting
+      validation strategy. This combination reduces the risk of spam and sybil
+      attacks.
+    </UiMessage>
+    <UiStrategiesConfiguratorOffchain
+      v-model:model-value="strategies"
+      :network-id="networkId"
+      :default-chain-id="snapshotChainId"
+      :limit="strategiesLimit"
+    />
+  </UiContainerSettings>
 </template>
