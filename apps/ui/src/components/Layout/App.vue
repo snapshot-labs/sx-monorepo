02085bbbb24c9ce8f577d49c3bac7d800c0bbf86
Wan <495709+wa0x6e@users.noreply.github.com>
2025-01-30 19:17:41 +0400

    feat: load space's custom colors (#1090)
    * feat: apply custom colors from space
    
    * feat: load dark/light theme for skin
    
    * feat: disable dark/light theme toggler on custom skin
    
    * refactor: use a dedicated function to set skin
    
    * refactor: fix types
    
    ---------
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- apps/ui/src/components/Layout/App.vue
+++ apps/ui/src/components/Layout/App.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup>
 import resolveConfig from 'tailwindcss/resolveConfig';
+import { Skin } from '@/composables/useUserSkin';
 import { APP_NAME } from '@/helpers/constants';
 import {
+  clone,
   getCacheHash,
   getStampUrl,
+  hexToRgb,
   whiteLabelAwareParams
 } from '@/helpers/utils';
 import { Transaction } from '@/types';
@@ -21,6 +24,7 @@ const router = useRouter();
 const uiStore = useUiStore();
 const { modalOpen } = useModal();
 const { init, setAppName, app } = useApp();
+const { DEFAULT_SKIN, setSkin } = useUserSkin();
 const { isWhiteLabel, space: whiteLabelSpace } = useWhiteLabel();
 const { setFavicon } = useFavicon();
 const { web3 } = useWeb3();
@@ -42,6 +46,7 @@ const {
   transaction,
   reset
 } = useWalletConnectTransaction();
+const { css } = useStyleTag('', { id: 'skin' });
 
 provide('web3', web3);
 
@@ -65,6 +70,25 @@ const hasPlaceHolderSidebar = computed(
 
 const hasTopNav = computed(() => {
   return 'space-editor' !== String(route.matched[1]?.name);
+});
+
+const skinVariables = computed(() => {
+  if (!whiteLabelSpace.value?.additionalRawData?.skinSettings) return {};
+
+  const colors = clone(whiteLabelSpace.value?.additionalRawData?.skinSettings);
+
+  const result = Object.entries(colors).reduce((acc, [colorName, hex]) => {
+    if (!hex || !colorName.includes('_color')) return acc;
+
+    const rgb = hexToRgb(hex.slice(1));
+    acc[`--${colorName.replace('_color', '')}`] = `${rgb.r},${rgb.g},${rgb.b}`;
+    return acc;
+  }, {});
+
+  if (result['--content']) {
+    result['--content'] = `rgb(${result['--content']})`;
+  }
+  return result;
 });
 
 async function handleTransactionAccept() {
@@ -145,6 +169,15 @@ watch(
     setFavicon(faviconUrl);
 
     setAppName(whiteLabelSpace.value.name);
+
+    css.value = `:root { ${Object.entries(skinVariables.value)
+      .map(([key, val]) => `${key}:${val}`)
+      .join(';')};  }`;
+
+    setSkin(
+      (whiteLabelSpace.value.additionalRawData?.skinSettings?.theme as Skin) ||
+        DEFAULT_SKIN
+    );
   },
   { immediate: true }
 );
