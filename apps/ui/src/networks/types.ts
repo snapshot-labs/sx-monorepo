aabd4f736cdd9f3b70f0d25b30e3be1dec758888
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-02-05 21:00:54 +0100

    feat: add Sequence WaaS support (#1150)
    * feat: add Sequence WaaS connector
    
    * feat: support Sequence WaaS in wallet actions
    
    * chore: rename to Sequence
    
    * chore: update project access key
    
    * fix: verify network in offchain
    
    Sequence is contract so we need to verify network to match.
    
    * chore: rename sequence-waas to sequence
    
    * Revert "chore: update project access key"
    
    This reverts commit 01d1ea9c038b421d6d8b54810684548ab9fa64da.

--- apps/ui/src/networks/types.ts
+++ apps/ui/src/networks/types.ts
@@ -41,7 +41,8 @@ export type ConnectorType =
   | 'injected'
   | 'walletconnect'
   | 'walletlink'
-  | 'gnosis';
+  | 'gnosis'
+  | 'sequence';
 export type Connector = {
   id: string;
   type: ConnectorType;
@@ -188,7 +189,11 @@ export type ReadOnlyNetworkActions = {
     executions: ExecutionInfo[] | null
   ): Promise<any>;
   flagProposal(web3: Web3Provider, proposal: Proposal);
-  cancelProposal(web3: Web3Provider, proposal: Proposal);
+  cancelProposal(
+    web3: Web3Provider,
+    connectorType: ConnectorType,
+    proposal: Proposal
+  );
   vote(
     web3: Web3Provider,
     connectorType: ConnectorType,
@@ -217,7 +222,12 @@ export type ReadOnlyNetworkActions = {
     statement: Statement,
     from?: string
   );
-  transferOwnership(web3: Web3Provider, space: Space, owner: string);
+  transferOwnership(
+    web3: Web3Provider,
+    connectorType: ConnectorType,
+    space: Space,
+    owner: string
+  );
   updateSettingsRaw(web3: Web3Provider, space: Space, settings: string);
   deleteSpace(web3: Web3Provider, space: Space);
   send(envelope: any): Promise<any>;
@@ -259,6 +269,7 @@ export type NetworkActions = ReadOnlyNetworkActions & {
   vetoProposal(web3: Web3Provider, proposal: Proposal);
   updateSettings(
     web3: Web3Provider,
+    connectorType: ConnectorType,
     space: Space,
     metadata: SpaceMetadata,
     authenticatorsToAdd: StrategyConfig[],
