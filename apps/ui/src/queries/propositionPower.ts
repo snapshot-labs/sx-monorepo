8e14c70fce2c33c791bcc640285292c53eff3fa5
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2025-02-06 22:05:06 +0400

    fix: remove unecessary default value

--- apps/ui/src/queries/propositionPower.ts
+++ apps/ui/src/queries/propositionPower.ts
@@ -72,7 +72,6 @@ export function usePropositionPowerQuery(space: Space) {
     queryKey: ['propositionPower', () => web3.value.account, space.id, block],
     queryFn: async () => getPropositionPower(space, block),
     enabled: !!web3.value.account && !web3.value.authLoading,
-    refetchOnMount: true,
     staleTime: 60 * 1000
   });
 }
