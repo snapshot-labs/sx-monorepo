37e0829630cf2cde5b59e034e2ddf27343de3887
ChaituVR <yourchaitu@gmail.com>
2025-02-08 18:39:08 +0530

    use AppLink for everything

--- apps/ui/src/components/OnboardingTask.vue
+++ apps/ui/src/components/OnboardingTask.vue
@@ -14,14 +14,9 @@ defineProps<{ task: Task }>();
   <div class="border-b mx-4 py-[14px] flex gap-x-2.5">
     <IS-flag class="text-skin-link mt-1 shrink-0" />
     <div>
-      <template v-if="task.link">
-        <AppLink :to="task.link">{{ task.description }}</AppLink>
-      </template>
-      <template v-else>
-        {{ task.description }}
-      </template>
+      <AppLink :to="task.link" class="inline">{{ task.description }}</AppLink>
       <div
-        v-if="!!task.currentStep && !!task.totalSteps"
+        v-if="task.totalSteps"
         class="text-skin-link inline-block bg-skin-border text-[13px] rounded-full px-1.5 ml-1"
       >
         {{ task.currentStep }}/{{ task.totalSteps }}
