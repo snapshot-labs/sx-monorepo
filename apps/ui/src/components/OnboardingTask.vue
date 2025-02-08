c8cbef44feb5b50e8cec8a17eeefa8b9669332bf
ChaituVR <yourchaitu@gmail.com>
2025-02-08 18:58:19 +0530

    fallback for currentstep

--- apps/ui/src/components/OnboardingTask.vue
+++ apps/ui/src/components/OnboardingTask.vue
@@ -19,7 +19,7 @@ defineProps<{ task: Task }>();
         v-if="task.totalSteps"
         class="text-skin-link inline-block bg-skin-border text-[13px] rounded-full px-1.5 ml-1"
       >
-        {{ task.currentStep }}/{{ task.totalSteps }}
+        {{ task.currentStep || 0 }}/{{ task.totalSteps }}
       </div>
     </div>
   </div>
