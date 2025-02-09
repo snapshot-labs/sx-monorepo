6440a11f731422871b7ffb3227074f5f99cd316b
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2025-02-10 01:12:51 +0400

    fix: add cb params to stamp logo url

--- apps/ui/src/components/Breadcrumb.vue
+++ apps/ui/src/components/Breadcrumb.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { getCacheHash } from '@/helpers/utils';
 import { NetworkID } from '@/types';
 
 defineOptions({ inheritAttrs: false });
@@ -27,6 +28,14 @@ const space = computed(() => {
 
   return spacesStore.spacesMap.get(`${networkId.value}:${spaceAddress.value}`);
 });
+
+const logo = computed(() => {
+  if (!isWhiteLabel.value) return;
+
+  return space.value?.additionalRawData?.skinSettings?.logo;
+});
+
+const cb = computed(() => (logo.value ? getCacheHash(logo.value) : undefined));
 </script>
 
 <template>
@@ -39,12 +48,13 @@ const space = computed(() => {
     v-bind="$attrs"
   >
     <UiStamp
-      v-if="isWhiteLabel && space.additionalRawData?.skinSettings?.logo"
+      v-if="logo"
       :id="space.id"
       type="space-logo"
       :width="190"
       :height="38"
       class="rounded-none border-none"
+      :cb="cb"
     />
     <template v-else>
       <div class="shrink-0">
