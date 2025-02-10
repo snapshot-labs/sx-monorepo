281b9007b456c82a0fb8fb6e64788ea9f60a767f
Wan <495709+wa0x6e@users.noreply.github.com>
2025-02-10 21:45:13 +0400

    fix: remove ENS registration form (#1168)
    * fix: remove ENS registration form
    
    * fix: remove used ens name from list of invalid domains

--- apps/ui/src/components/EnsConfiguratorOffchain.vue
+++ apps/ui/src/components/EnsConfiguratorOffchain.vue
@@ -21,16 +21,15 @@ const {
   load,
   refresh
 } = useWalletEns(props.networkId);
-const { resume: resumeEnsMonitoring } = useIntervalFn(() => refresh, 5000, {
-  immediate: false
-});
 
 const validNames = computed(() => {
   return Object.values(names.value || {}).filter(d => d.status === 'AVAILABLE');
 });
 
 const invalidNames = computed(() => {
-  return Object.values(names.value || {}).filter(d => d.status !== 'AVAILABLE');
+  return Object.values(names.value || {}).filter(
+    d => !['AVAILABLE', 'USED'].includes(d.status)
+  );
 });
 
 const isTestnet = computed(() => {
@@ -113,19 +112,7 @@ function handleSelect(value: string) {
             <IH-Exclamation class="mt-[5px] text-skin-danger shrink-0" />
             <div class="flex flex-col">
               <div class="text-skin-danger" v-text="name.name" />
-              <div v-if="name.status === 'USED'">
-                ENS name already attached to a
-                <AppLink
-                  :to="{
-                    name: 'space',
-                    params: { space: `${networkId}:${name.name}` }
-                  }"
-                  class="text-skin-link"
-                >
-                  space
-                </AppLink>
-              </div>
-              <div v-else-if="name.status === 'TOO_LONG'">
+              <div v-if="name.status === 'TOO_LONG'">
                 ENS name is too long. It must be less than
                 {{ MAX_ENS_NAME_LENGTH }} characters
               </div>
@@ -147,10 +134,9 @@ function handleSelect(value: string) {
       <UiMessage v-else type="danger">
         No ENS names found for the current wallet.
       </UiMessage>
-    </div>
-    <div class="space-y-3">
-      <h4 class="eyebrow">Register new ENS name</h4>
-      <FormEnsRegistration @submit="resumeEnsMonitoring" />
+      <AppLink to="https://app.ens.domains" class="inline-block">
+        Register a new ENS name <IH-arrow-sm-right class="-rotate-45 inline" />
+      </AppLink>
     </div>
   </div>
 </template>
