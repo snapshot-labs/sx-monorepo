f0ef8684c2309d8905d681ca688799cf3754287e
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2025-02-13 02:31:09 +0400

    fix: fix empty color input marked as errored

--- apps/ui/src/components/FormSpaceWhitelabel.vue
+++ apps/ui/src/components/FormSpaceWhitelabel.vue
@@ -81,14 +81,14 @@ const formErrors = computed(() => {
     required: [],
     properties: {
       customDomain: CUSTOM_DOMAIN_DEFINITION,
-      skinSettings: SKIN_DEFINITION
+      ...SKIN_DEFINITION.properties
     }
   });
 
   const errors = validator.validate(
     {
       customDomain: customDomain.value,
-      skinSettings: skinSettings.value
+      ...skinSettings.value
     },
     {
       skipEmptyOptionalFields: true
@@ -137,7 +137,7 @@ const disabled = computed(() => {
       <UiForm
         v-model="skinSettings"
         :definition="SKIN_DEFINITION"
-        :error="formErrors.skinSettings"
+        :error="formErrors"
         :disabled="disabled"
       />
     </div>
