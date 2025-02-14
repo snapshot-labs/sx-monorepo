a901721ec185d78853c3dd236b696dc4cf5a5398
Wan <495709+wa0x6e@users.noreply.github.com>
2025-02-14 22:17:18 +0400

    Revert "feat: add form to update skin settings (#1148)" (#1192)
    This reverts commit 91f9cecc3751ea812969acd10395725dbf41f4ad.

--- apps/ui/src/components/FormSpaceWhitelabel.vue
+++ apps/ui/src/components/FormSpaceWhitelabel.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { TURBO_URL } from '@/helpers/constants';
 import { getValidator } from '@/helpers/validation';
-import { SkinSettings, Space } from '@/types';
+import { Space } from '@/types';
 
 const CUSTOM_DOMAIN_DEFINITION = {
   type: 'string',
@@ -11,88 +10,26 @@ const CUSTOM_DOMAIN_DEFINITION = {
   examples: ['vote.balancer.fi']
 };
 
-const COLOR_VALIDATION = {
-  type: 'string',
-  format: 'color',
-  examples: ['#FF0000']
-};
-
-const SKIN_DEFINITION = {
-  type: 'object',
-  title: 'Skin settings',
-  additionalProperties: false,
-  required: [],
-  properties: {
-    bg_color: {
-      ...COLOR_VALIDATION,
-      title: 'Background color'
-    },
-    text_color: {
-      ...COLOR_VALIDATION,
-      title: 'Text color'
-    },
-    link_color: {
-      ...COLOR_VALIDATION,
-      title: 'Link color'
-    },
-    content_color: {
-      ...COLOR_VALIDATION,
-      title: 'Content color',
-      tooltip: 'Proposal text color'
-    },
-    border_color: {
-      ...COLOR_VALIDATION,
-      title: 'Border color'
-    },
-    heading_color: {
-      ...COLOR_VALIDATION,
-      title: 'Heading color'
-    },
-    primary_color: {
-      ...COLOR_VALIDATION,
-      title: 'Primary color'
-    },
-    theme: {
-      type: 'string',
-      title: 'Base theme',
-      enum: ['light', 'dark'],
-      options: [
-        { id: 'light', name: 'Light' },
-        { id: 'dark', name: 'Dark' }
-      ]
-    }
-  }
-};
-
 const customDomain = defineModel<string>('customDomain', { required: true });
-const skinSettings = defineModel<SkinSettings>('skinSettings', {
-  required: true
-});
 
-const props = defineProps<{
+defineProps<{
   space: Space;
-}>();
-
-const emit = defineEmits<{
-  (e: 'errors', value: any);
 }>();
 
 const formErrors = computed(() => {
   const validator = getValidator({
     type: 'object',
-    title: 'Whitelabel',
+    title: 'White Label',
     additionalProperties: false,
     required: [],
     properties: {
-      customDomain: CUSTOM_DOMAIN_DEFINITION,
-      ...SKIN_DEFINITION.properties
+      customDomain: CUSTOM_DOMAIN_DEFINITION
     }
   });
 
   const errors = validator.validate(
     {
-      customDomain: customDomain.value,
-      ...skinSettings.value
+      customDomain: customDomain.value
     },
     {
       skipEmptyOptionalFields: true
@@ -100,76 +37,24 @@ const formErrors = computed(() => {
   );
   return errors;
 });
-
-const disabled = computed(() => {
-  return !props.space.turbo && !props.space.additionalRawData?.domain;
-});
-
-watch(formErrors, value => emit('errors', value));
-
-onMounted(() => {
-  emit('errors', formErrors.value);
-});
 </script>
 
 <template>
+  <h4 class="eyebrow mb-2 font-medium mt-4">Custom domain</h4>
   <UiMessage
-    v-if="disabled"
     type="info"
-    :learn-more-link="TURBO_URL"
-    class="mb-4"
+    :learn-more-link="'https://docs.snapshot.box/spaces/add-custom-domain'"
   >
-    Whitelabel features are only available for Turbo subscribers.
+    To set up a custom domain, you must subscribe to the Turbo plan and create a
+    CNAME record pointing to "cname.snapshot.box" with your DNS provider or
+    registrar.
   </UiMessage>
-  <div class="s-box space-y-4">
-    <div>
-      <h4 class="eyebrow mb-2 font-medium">Custom domain</h4>
-      <UiMessage
-        type="info"
-        class="mb-3"
-        :learn-more-link="'https://docs.snapshot.box/spaces/add-custom-domain'"
-      >
-        To set up a custom domain, you need to create a CNAME record pointing to
-        "cname.snapshot.box" with your DNS provider or registrar.
-      </UiMessage>
-      <UiInputString
-        v-model="customDomain"
-        :definition="CUSTOM_DOMAIN_DEFINITION"
-        :error="formErrors.customDomain"
-        :disabled="disabled"
-      />
-    </div>
-    <div>
-      <h4 class="eyebrow font-medium">Skin colors</h4>
-      <div class="mb-2">
-        Empty colors value will fallback to the base theme color.
-      </div>
-      <UiForm
-        v-model="skinSettings"
-        :definition="SKIN_DEFINITION"
-        :error="formErrors"
-        :disabled="disabled"
-      />
-    </div>
-    <div>
-      <h4 class="eyebrow font-medium">Custom logo</h4>
-      <div class="mb-2">
-        You can replace your space name in the upper left corner by a custom
-        logo. Recommended size is 380x76 pixels.
-      </div>
-      <UiInputStamp
-        v-model="skinSettings.logo"
-        :disabled="disabled"
-        :fallback="false"
-        :width="380"
-        :height="76"
-        class="border-0"
-        :definition="{
-          type: 'string',
-          format: 'stamp',
-          title: 'Logo'
-        }"
-      />
-    </div>
+  <div class="s-box mt-3">
+    <UiInputString
+      v-model="customDomain"
+      :definition="CUSTOM_DOMAIN_DEFINITION"
+      :error="formErrors.customDomain"
+      :disabled="!space.turbo && !space.additionalRawData?.domain"
+    />
   </div>
 </template>
--- apps/ui/src/composables/useSpaceSettings.ts
+++ apps/ui/src/composables/useSpaceSettings.ts
@@ -11,7 +11,6 @@ import {
 } from '@/networks/types';
 import {
   Member,
-  SkinSettings,
   Space,
   SpaceMetadata,
   SpaceMetadataLabel,
@@ -53,7 +52,6 @@ export type OffchainSpaceSettings = {
   boost: OffchainApiSpace['boost'];
   validation: OffchainApiSpace['validation'];
   voteValidation: OffchainApiSpace['voteValidation'];
-  skinSettings: SkinSettings;
 };
 
 type Form = SpaceMetadata & {
@@ -77,18 +75,6 @@ const DEFAULT_FORM_STATE: Form = {
   treasuries: [],
   labels: [],
   delegations: []
-};
-
-const DEFAULT_SKIN_SETTINGS = {
-  bg_color: '',
-  link_color: '',
-  text_color: '',
-  content_color: '',
-  border_color: '',
-  heading_color: '',
-  primary_color: '',
-  theme: 'light',
-  logo: undefined
 };
 
 export function useSpaceSettings(space: Ref<Space>) {
@@ -187,7 +173,6 @@ export function useSpaceSettings(space: Ref<Space>) {
   const termsOfServices = ref('');
   const customDomain = ref('');
   const isPrivate = ref(false);
-  const skinSettings = ref<SkinSettings>(clone(DEFAULT_SKIN_SETTINGS));
 
   function currentToMinutesOnly(value: number) {
     const duration = getDurationFromCurrent(space.value.network, value);
@@ -605,9 +590,9 @@ export function useSpaceSettings(space: Ref<Space>) {
           ? space.value.additionalRawData.validation
           : proposalValidation.value,
       voteValidation: voteValidation.value,
-      boost: space.value.additionalRawData.boost,
-      skinSettings: skinSettings.value
+      boost: space.value.additionalRawData.boost
     };
+
     const prunedSaveData: Partial<OffchainSpaceSettings> = clone(saveData);
     Object.entries(prunedSaveData).forEach(([key, value]) => {
       if (value === null || value === '') delete prunedSaveData[key];
@@ -624,13 +609,6 @@ export function useSpaceSettings(space: Ref<Space>) {
       prunedSaveData.voting.quorumType === 'default'
     ) {
       delete prunedSaveData.voting.quorumType;
-    }
-    if (prunedSaveData.skinSettings) {
-      Object.entries(prunedSaveData.skinSettings).forEach(([key, value]) => {
-        if (value === null || value === '') {
-          delete prunedSaveData.skinSettings?.[key];
-        }
-      });
     }
 
     return updateSettingsRaw(space.value, JSON.stringify(prunedSaveData));
@@ -757,9 +735,6 @@ export function useSpaceSettings(space: Ref<Space>) {
       termsOfServices.value = space.value.terms ?? '';
       customDomain.value = space.value.additionalRawData?.domain ?? '';
       isPrivate.value = space.value.additionalRawData?.private ?? false;
-      skinSettings.value = clone(
-        space.value.additionalRawData?.skinSettings || DEFAULT_SKIN_SETTINGS
-      );
     }
   }
 
@@ -793,7 +768,6 @@ export function useSpaceSettings(space: Ref<Space>) {
     const termsOfServicesValue = termsOfServices.value;
     const customDomainValue = customDomain.value;
     const isPrivateValue = isPrivate.value;
-    const skinSettingsValue = skinSettings.value;
 
     if (loading.value) {
       isModified.value = false;
@@ -948,14 +922,6 @@ export function useSpaceSettings(space: Ref<Space>) {
         isModified.value = true;
         return;
       }
-
-      if (
-        objectHash(space.value.additionalRawData?.skinSettings) !==
-        objectHash(skinSettingsValue)
-      ) {
-        isModified.value = true;
-        return;
-      }
     } else {
       const [authenticatorsToAdd, authenticatorsToRemove] =
         await processChanges(
@@ -1027,7 +993,6 @@ export function useSpaceSettings(space: Ref<Space>) {
     termsOfServices,
     customDomain,
     isPrivate,
-    skinSettings,
     save,
     saveController,
     deleteSpace,
