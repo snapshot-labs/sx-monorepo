4f4f2f68019214b04933b64c2fdd7465812a041f
less <fabien@bonustrack.co>
2025-02-15 10:12:45 +0700

    feat: replace landing page with "home" page

---
+++ apps/ui/src/components/App/Footer.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { HELPDESK_URL } from '@/helpers/constants';
+
+const { isWhiteLabel, resolved } = useWhiteLabel();
+</script>
+
+<template>
+  <div class="hidden md:block fixed bottom-3 right-4 z-40 space-x-2">
+    <UiTooltip v-if="resolved && !isWhiteLabel" title="About Snapshot">
+      <router-link :to="{ name: 'site-landing' }">
+        <UiButton class="!px-0 w-[46px]">
+          <IC-zap class="inline-block size-[24px]" />
+        </UiButton>
+      </router-link>
+    </UiTooltip>
+    <UiTooltip title="Get help">
+      <a :href="HELPDESK_URL" target="_blank">
+        <UiButton class="!px-0 w-[46px]">
+          <IH-chat class="inline-block" />
+        </UiButton>
+      </a>
+    </UiTooltip>
+  </div>
+</template>
