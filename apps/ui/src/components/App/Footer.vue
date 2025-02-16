503c75ca7007d7e6f71958869438c4db5e77e8c8
less <fabien@bonustrack.co>
2025-02-16 19:23:03 +0700

    ux: hide footer buttons on small screen

--- apps/ui/src/components/App/Footer.vue
+++ apps/ui/src/components/App/Footer.vue
@@ -5,7 +5,7 @@ const { isWhiteLabel, resolved } = useWhiteLabel();
 </script>
 
 <template>
-  <div class="hidden md:block fixed bottom-3 right-4 z-40 space-x-2">
+  <div class="hidden xl:block fixed bottom-3 right-4 z-40 space-x-2">
     <UiTooltip v-if="resolved && !isWhiteLabel" title="About Snapshot">
       <router-link :to="{ name: 'site-landing' }">
         <UiButton class="!px-0 w-[46px]">
