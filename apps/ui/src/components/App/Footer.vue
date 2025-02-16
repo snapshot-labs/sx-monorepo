c5c69d289badff64d3fc4b1208b2b7df848d3474
less <fabien@bonustrack.co>
2025-02-17 00:07:24 +0700

    fix: remove "about" footer button on site related pages

--- apps/ui/src/components/App/Footer.vue
+++ apps/ui/src/components/App/Footer.vue
@@ -2,11 +2,17 @@
 import { HELPDESK_URL } from '@/helpers/constants';
 
 const { isWhiteLabel, resolved } = useWhiteLabel();
+const route = useRoute();
+
+const routeName = computed(() => route.name as string);
 </script>
 
 <template>
   <div class="hidden xl:block fixed bottom-3 right-4 z-40 space-x-2">
-    <UiTooltip v-if="resolved && !isWhiteLabel" title="About Snapshot">
+    <UiTooltip
+      v-if="resolved && !isWhiteLabel && !routeName.startsWith('site-')"
+      title="About Snapshot"
+    >
       <router-link :to="{ name: 'site-landing' }" tabindex="-1">
         <UiButton class="!px-0 w-[46px]">
           <IC-zap class="inline-block size-[24px]" />
