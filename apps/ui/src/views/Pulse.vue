91ec501c94ec996be095386eb76923e7f98e9caa
less <fabien@bonustrack.co>
2025-02-21 04:35:45 +0700

    feat: add create and discussion page

--- apps/ui/src/views/Pulse.vue
+++ apps/ui/src/views/Pulse.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
 const title = ref('');
 const discussion = ref('');
+const statement = {
+  id: 1,
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  agree: 90,
+  disagree: 1,
+  pass: 0
+};
 </script>
 
 <template>
@@ -14,7 +21,7 @@ const discussion = ref('');
       </UiContainer>
     </div>
     <UiContainer class="!max-w-[990px] space-y-9 mb-10 text-center">
-      <PulseTitleForm class="-mt-8 text-left" />
+      <PulseTitleForm class="-mt-8 text-left !max-w-[710px]" />
 
       <div class="relative overflow-hidden">
         <div class="grid grid-cols-9 gap-4">
@@ -28,12 +35,18 @@ const discussion = ref('');
             <div class="max-w-[440px]">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              enim ad minim veniam.
+              <div>
+                <router-link :to="{ name: 'pulse-create' }">
+                  Get started
+                </router-link>
+              </div>
             </div>
           </div>
           <div class="col-span-4 relative flex items-center">
-            <div class="absolute left-0 right-0 bottom-0 top-0 z-10 bg-gradient-to-r from-transparent to-skin-bg" />
+            <div
+              class="absolute left-0 right-0 bottom-0 top-0 z-10 bg-gradient-to-r from-transparent to-skin-bg"
+            />
             <div class="-mr-[600px] mt-6">
               <PulseTitleForm />
             </div>
@@ -43,12 +56,6 @@ const discussion = ref('');
 
       <div class="relative overflow-hidden">
         <div class="grid grid-cols-9 gap-4">
-          <div class="col-span-4 relative flex items-center">
-            <div class="absolute left-0 right-0 bottom-0 top-0 z-10 bg-gradient-to-r from-skin-bg to-transparent" />
-            <div class="-ml-[140px] mt-6">
-              <PulseTitleForm />
-            </div>
-          </div>
           <div class="col-span-5 text-md text-left">
             <div class="eyebrow mb-3">Step 2</div>
             <h2
@@ -59,8 +66,22 @@ const discussion = ref('');
             <div class="max-w-[440px]">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              enim ad minim veniam.
+              <div>
+                <router-link
+                  :to="{ name: 'pulse-discussion', params: { id: 1 } }"
+                >
+                  Try demo discussion
+                </router-link>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-4 relative flex items-center">
+            <div
+              class="absolute left-0 right-0 bottom-0 top-0 z-10 bg-gradient-to-l from-skin-bg to-transparent"
+            />
+            <div class="-mr-[180px] mt-6">
+              <PulseStatements class="text-left" />
             </div>
           </div>
         </div>
@@ -78,14 +99,22 @@ const discussion = ref('');
             <div class="max-w-[440px]">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              enim ad minim veniam.
+              <div>
+                <router-link
+                  :to="{ name: 'pulse-discussion', params: { id: 1 } }"
+                >
+                  See example discussion
+                </router-link>
+              </div>
             </div>
           </div>
           <div class="col-span-4 relative flex items-center">
-            <div class="absolute left-0 right-0 bottom-0 top-0 z-10 bg-gradient-to-r from-transparent to-skin-bg" />
-            <div class="-mr-[600px] mt-6">
-              <PulseTitleForm />
+            <div
+              class="absolute left-0 right-0 bottom-0 top-0 z-10 bg-gradient-to-r from-transparent to-skin-bg"
+            />
+            <div class="-mr-[140px] mt-6">
+              <PulseStatementItem :statement="statement" class="text-left" />
             </div>
           </div>
         </div>
