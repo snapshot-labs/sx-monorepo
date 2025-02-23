97208280f83dfdb297705625ac8d9fe4aae10c81
ChaituVR <yourchaitu@gmail.com>
2025-02-23 22:37:17 +0530

    fix logo click in topnav

--- apps/ui/src/components/Ui/Topnav.vue
+++ apps/ui/src/components/Ui/Topnav.vue
@@ -1,7 +1,9 @@
 <template>
-  <header class="fixed top-0 inset-x-0 z-50 max-w-maximum !mx-auto">
+  <header
+    class="fixed top-0 inset-x-0 z-50 max-w-maximum !mx-auto pointer-events-none"
+  >
     <div
-      class="border-b h-[72px] bg-skin-bg flex items-center justify-between pr-4 gap-4"
+      class="border-b h-[72px] bg-skin-bg flex items-center justify-between pr-4 gap-4 pointer-events-auto"
     >
       <slot />
     </div>
