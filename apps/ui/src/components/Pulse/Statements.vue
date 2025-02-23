760cd2ace1b636a8913a63fea84d5dd02ea1a78b
less <fabien@bonustrack.co>
2025-02-23 11:36:08 +0700

    feat: react change live

--- apps/ui/src/components/Pulse/Statements.vue
+++ apps/ui/src/components/Pulse/Statements.vue
@@ -1,30 +1,61 @@
+<script setup lang="ts">
+defineProps<{ statements: any[] }>();
+
+const { sendVote } = usePulse();
+
+const loading = ref(false);
+
+async function handleVote(
+  discussion: number,
+  statement: number,
+  choice: 1 | 2 | 3
+) {
+  loading.value = true;
+
+  await sendVote(discussion, statement, choice);
+
+  loading.value = false;
+}
+</script>
+
 <template>
-  <div>
+  <div v-if="statements[0]">
     <div class="border rounded-md p-4 h-[220px] flex flex-col">
       <div class="text-lg text-skin-link flex-auto">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
+        {{ statements[0].body }}
       </div>
       <div class="items-end space-x-2 grid grid-cols-3">
-        <UiButton class="!border-skin-success !text-skin-success space-x-1">
+        <UiButton
+          :disabled="loading"
+          class="!border-skin-success !text-skin-success space-x-1"
+          @click="handleVote(statements[0].discussion, statements[0].id, 1)"
+        >
           <IH-check class="inline-block" />
           <span class="hidden sm:inline-block" v-text="'Agree'" />
         </UiButton>
-        <UiButton class="!border-skin-danger !text-skin-danger space-x-1">
+        <UiButton
+          :disabled="loading"
+          class="!border-skin-danger !text-skin-danger space-x-1"
+          @click="handleVote(statements[0].discussion, statements[0].id, 2)"
+        >
           <IH-x class="inline-block" />
           <span class="hidden sm:inline-block" v-text="'Disagree'" />
         </UiButton>
-        <UiButton class="!border-skin-text !text-skin-text space-x-1">
+        <UiButton
+          :disabled="loading"
+          class="!border-skin-text !text-skin-text space-x-1"
+          @click="handleVote(statements[0].discussion, statements[0].id, 3)"
+        >
           <IH-minus-sm class="inline-block" />
           <span class="hidden sm:inline-block" v-text="'Pass'" />
         </UiButton>
       </div>
     </div>
-    <div class="mx-1">
+    <div v-if="statements[1]" class="mx-1">
       <div class="border border-t-0 rounded-b-md h-2" />
-      <div class="mx-1">
+      <div v-if="statements[2]" class="mx-1">
         <div class="border border-t-0 rounded-b-md h-2" />
-        <div class="mx-1">
+        <div v-if="statements[3]" class="mx-1">
           <div class="border border-t-0 rounded-b-md h-2" />
         </div>
       </div>
---
+++ apps/ui/src/composables/usePulse.ts
@@ -0,0 +1,93 @@
+import { client } from '@/helpers/kbyte';
+import { Discussion, Vote } from '@/helpers/pulse';
+
+const { web3 } = useWeb3();
+
+const discussions: Ref<{ [key: number]: Discussion }> = ref({});
+const votes: Ref<{ [key: number]: Vote[] }> = ref({});
+
+client.subscribe(([subject, body]) => {
+  if (subject === 'justsaying') {
+    if (body.subject === 'new_statement') {
+      const statement = body.body;
+      discussions.value[statement.discussion].statements.push(statement);
+    }
+
+    if (body.subject === 'new_vote') {
+      const vote = body.body;
+      const statement = discussions.value[vote.discussion].statements.findIndex(
+        s => s.id === vote.statement
+      );
+      discussions.value[vote.discussion].statements[statement].vote_count++;
+      discussions.value[vote.discussion].statements[statement][
+        `scores_${vote.choice}`
+      ]++;
+      votes.value[vote.discussion].push(vote);
+    }
+  }
+});
+
+export function usePulse() {
+  async function loadDiscussion(id: number) {
+    discussions.value[id] = await client.requestAsync('get_discussion', id);
+    await client.requestAsync('subscribe', id);
+  }
+
+  async function loadVotes(id: number) {
+    const voter = web3.value.account;
+
+    if (voter) {
+      votes.value[id] = await client.requestAsync('get_votes', [id, voter]);
+    } else {
+      votes.value[id] = [];
+    }
+  }
+
+  async function sendDiscussion(title: string, body: string) {
+    return send('discussion', {
+      author: web3.value.account,
+      title,
+      body
+    });
+  }
+
+  async function sendStatement(discussion: number, statement: string) {
+    return send('statement', {
+      author: web3.value.account,
+      discussion,
+      statement
+    });
+  }
+
+  async function sendVote(
+    discussion: number,
+    statement: number,
+    choice: 1 | 2 | 3
+  ) {
+    return send('vote', {
+      voter: web3.value.account,
+      discussion,
+      statement,
+      choice
+    });
+  }
+
+  async function send(type: string, payload: any) {
+    return await client.requestAsync('broadcast', {
+      type,
+      from: '0xeF8305E140ac520225DAf050e2f71d5fBcC543e7',
+      sig: '0xf0e14ebfa7f08ee13811725e8171465f710decd56a1e4c67cffa99e2e51acf742ef326836affeb4c8c110e89818895acc24cb0893f4bae47eddbbd57138c6ca41b',
+      payload
+    });
+  }
+
+  return {
+    discussions,
+    votes,
+    loadDiscussion,
+    loadVotes,
+    sendDiscussion,
+    sendStatement,
+    sendVote
+  };
+}
