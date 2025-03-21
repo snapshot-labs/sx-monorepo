9d0e27d7df419d7997a5c07e3759b5eb700dbf8c
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-03-21 09:41:21 +0100

    refactor: migrate to new unified APIs (#1267)
    * chore: bump checkpoint to 0.1.0-beta.53
    
    * feat: add IS_INDEXER flag
    
    * refactor: separate api and indexer
    
    * chore: api. and testnet-api. for unified APIs
    
    * chore: add IS_INDEXER to env example
    
    * chore: bump checkpoint to 0.1.0-beta.54

---
+++ apps/api/src/api.ts
@@ -0,0 +1,54 @@
+import 'dotenv/config';
+import http from 'http';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import Checkpoint, { createGetLoader } from '@snapshot-labs/checkpoint';
+import cors from 'cors';
+import express from 'express';
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+export async function startApiServer(checkpoint: Checkpoint) {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer({
+    schema: checkpoint.getSchema(),
+    plugins: [
+      ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+      ApolloServerPluginDrainHttpServer({ httpServer })
+    ],
+    introspection: true
+  });
+
+  await server.start();
+
+  app.use(cors());
+  app.get('/deployment', (req, res) => {
+    res.json({
+      index: process.env.DATABASE_URL_INDEX ?? 'default'
+    });
+  });
+
+  app.use(
+    '/',
+    express.json({ limit: '50mb' }),
+    expressMiddleware(server, {
+      context: async () => {
+        const baseContext = checkpoint.getBaseContext();
+        return {
+          ...baseContext,
+          getLoader: createGetLoader(baseContext)
+        };
+      }
+    })
+  );
+
+  await new Promise<void>(resolve =>
+    httpServer.listen({ port: PORT }, resolve)
+  );
+
+  console.log(`Listening on port ${PORT}`);
+}
