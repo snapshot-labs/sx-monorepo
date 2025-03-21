db4ed5636ebc29935b1d4043b01cc115d1bb7541
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-03-21 16:50:57 +0100

    feat: add ability to add custom execution strategy

--- apps/ui/src/networks/evm/actions.ts
+++ apps/ui/src/networks/evm/actions.ts
@@ -679,6 +679,7 @@ export function createActions(
       votingStrategiesToAdd: StrategyConfig[],
       votingStrategiesToRemove: number[],
       validationStrategy: StrategyConfig,
+      executionStrategies: StrategyConfig[],
       votingDelay: number | null,
       minVotingDuration: number | null,
       maxVotingDuration: number | null
@@ -690,9 +691,15 @@ export function createActions(
 
       const pinned = await helpers.pin(
         createErc1155Metadata(metadata, {
-          execution_strategies: space.executors,
-          execution_strategies_types: space.executors_types,
-          execution_destinations: space.executors_destinations
+          execution_strategies: executionStrategies.map(
+            config => config.address
+          ),
+          execution_strategies_types: executionStrategies.map(
+            config => config.type
+          ),
+          execution_destinations: executionStrategies.map(
+            (_, i) => space.executors_destinations[i] ?? ''
+          )
         })
       );
 
