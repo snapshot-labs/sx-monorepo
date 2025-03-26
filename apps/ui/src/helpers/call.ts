0e961f91fdf196ac6474a797279e662749ff56d1
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-03-26 13:43:31 +0100

    feat: skip Alchemy for native token and token metadata (#1288)
    * feat: skip Alchemy for native token and token metadata
    
    This allows us to avoid issues on some networks (Apechain, Curtis),
    and makes it possible to display native token balance on unsupported
    networks (Mantle).
    
    * chore: add mantle Coingecko info
    
    * chore: update Mantle native token ticker
    
    * chore: fix typo in balance
    
    Co-authored-by: Less <bonustrack@users.noreply.github.com>
    
    * feat: add Gnosis Chain treasury support
    
    * feat: show error message on tokens/nfts error
    
    * feat: use Multicaller3 to fetch token metadata
    
    This enables usage of allowFailure flag.
    
    ---------
    
    Co-authored-by: Less <bonustrack@users.noreply.github.com>

--- apps/ui/src/helpers/call.ts
+++ apps/ui/src/helpers/call.ts
@@ -2,6 +2,13 @@ import { Interface } from '@ethersproject/abi';
 import { Contract } from '@ethersproject/contracts';
 import networks from '@snapshot-labs/snapshot.js/src/networks.json';
 
+const MULTICALL3_ADDRESSES = '0xcA11bde05977b3631167028862bE2a173976CA11';
+
+export const MULTICALL_ABI = [
+  'function aggregate(tuple(address target, bytes callData)[] calls) view returns (uint256 blockNumber, bytes[] returnData)',
+  'function aggregate3(tuple(address target, bool allowFailure, bytes callData)[] calls) view returns (tuple(bool success, bytes returnData)[] returnData)'
+];
+
 export async function call(provider, abi: any[], call: any[], options?) {
   const contract = new Contract(call[0], abi, provider);
   try {
@@ -19,12 +26,9 @@ export async function multicall(
   calls: any[],
   options?
 ) {
-  const multicallAbi = [
-    'function aggregate(tuple(address target, bytes callData)[] calls) view returns (uint256 blockNumber, bytes[] returnData)'
-  ];
   const multi = new Contract(
     networks[network].multicall,
-    multicallAbi,
+    MULTICALL_ABI,
     provider
   );
   const itf = new Interface(abi);
@@ -52,4 +56,46 @@ export async function multicall(
   } catch (e) {
     return Promise.reject(e);
   }
+}
+
+export async function multicall3(
+  provider,
+  abi: any[],
+  calls: [string, string, any[]?][],
+  allowFailure: boolean,
+  options?
+) {
+  const multi = new Contract(MULTICALL3_ADDRESSES, MULTICALL_ABI, provider);
+
+  const itf = new Interface(abi);
+  const max = options?.limit || 500;
+  const pages = Math.ceil(calls.length / max);
+
+  const promises: any = [];
+  Array.from(Array(pages)).forEach((x, i) => {
+    const callsInPage = calls.slice(max * i, max * (i + 1));
+
+    promises.push(
+      multi.aggregate3(
+        callsInPage.map(call => {
+          const target = call[0].toLowerCase();
+          const callData = itf.encodeFunctionData(call[1], call[2]);
+
+          return [target, allowFailure, callData];
+        }),
+        options || {}
+      )
+    );
+  });
+
+  const results = await Promise.all(promises);
+  return results.flat().map((call, i) => {
+    const [success, returnData] = call;
+
+    const decoded = success
+      ? itf.decodeFunctionResult(calls[i][1], returnData)
+      : null;
+
+    return [success, decoded];
+  });
 }
