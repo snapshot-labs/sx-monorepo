e5d1cce01b4a01bae094021557e6438bdbd14740
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-03-28 13:47:51 +0100

    feat: set up migrations for Mana

---
+++ apps/mana/migrations/20250328123755_create_registered_proposals_table.ts
@@ -0,0 +1,20 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  const exists = await knex.schema.hasTable('registered_proposals');
+  if (exists) return;
+
+  return knex.schema.createTable('registered_proposals', t => {
+    t.string('id').primary();
+    t.timestamps(true, true);
+    t.string('chainId');
+    t.integer('timestamp');
+    t.string('strategyAddress');
+    t.string('herodotusId');
+    t.boolean('processed').defaultTo(false).index();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('registered_proposals');
+}
