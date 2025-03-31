30d7b298d8dff99fb36463414376ee653524563d
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2025-03-31 05:15:28 +0400

    fix: improve create space page

--- apps/ui/src/components/Ui/Stepper.vue
+++ apps/ui/src/components/Ui/Stepper.vue
@@ -38,15 +38,15 @@ function goToStep(stepName: string) {
 </script>
 
 <template>
-  <div class="flex">
+  <div class="flex flex-col lg:flex-row lg:items-start">
     <div
-      class="flex fixed lg:sticky top-[72px] inset-x-0 p-3 border-b z-10 bg-skin-bg lg:top-auto lg:inset-x-auto lg:p-0 lg:pr-5 lg:border-0 lg:flex-col gap-1 min-w-[180px] overflow-auto"
+      class="flex z-40 top-[72px] inset-x-0 p-3 border-b bg-skin-bg lg:top-auto lg:inset-x-auto lg:p-0 lg:border-0 lg:flex-col gap-1 min-w-[180px] overflow-x-auto"
     >
       <button
         v-for="(step, stepName) in steps"
         :key="stepName"
         type="button"
-        class="px-3 py-1 flex items-center gap-2 lg:w-full text-skin-link rounded text-left scroll-mr-3 first:ml-auto last:mr-auto whitespace-nowrap hover:bg-skin-hover-bg"
+        class="px-3 py-1 flex items-center gap-2 text-skin-link rounded whitespace-nowrap hover:bg-skin-hover-bg"
         :class="{
           'bg-skin-active-bg': stepper.isCurrent(stepName)
         }"
@@ -58,13 +58,11 @@ function goToStep(stepName: string) {
       </button>
     </div>
     <div class="flex-1 space-y-4">
-      <div class="mt-8 lg:mt-0">
-        <slot
-          name="content"
-          :current-step="currentStep"
-          :go-to-next="stepper.goToNext"
-        />
-      </div>
+      <slot
+        name="content"
+        :current-step="currentStep"
+        :go-to-next="stepper.goToNext"
+      />
       <UiButton
         v-if="stepper.isLast.value"
         class="w-full primary"
