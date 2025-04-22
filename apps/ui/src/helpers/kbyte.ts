50d08893504745bf002ba691729798bf22fa6a55
less <fabien@bonustrack.co>
2025-04-22 16:58:35 +0700

    chore: replace bluebird with custom fn

--- apps/ui/src/helpers/kbyte.ts
+++ apps/ui/src/helpers/kbyte.ts
@@ -1,7 +1,21 @@
-import { promisifyAll } from 'bluebird';
 import kbyte from 'kbyte';
 
-promisifyAll(kbyte.Client.prototype);
+Object.getOwnPropertyNames(kbyte.Client.prototype)
+  .filter(
+    method =>
+      typeof kbyte.Client.prototype[method] === 'function' &&
+      method !== 'constructor'
+  )
+  .forEach(method => {
+    kbyte.Client.prototype[`${method}Async`] = function (...args) {
+      return new Promise((resolve, reject) => {
+        this[method](...args, (error, result) => {
+          if (error) reject(error);
+          else resolve(result);
+        });
+      });
+    };
+  });
 
 export const client = new kbyte.Client(
   import.meta.env.VITE_PULSE_API || 'ws://localhost:3000'
