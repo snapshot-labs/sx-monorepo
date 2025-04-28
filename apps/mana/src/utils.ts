e75a2e35e19cae123c08f11df38521156a79ceaf
ChaituVR <yourchaitu@gmail.com>
2025-04-28 16:58:35 +0530

    chore: remove previous function

--- apps/mana/src/utils.ts
+++ apps/mana/src/utils.ts
@@ -20,12 +20,6 @@ export function rpcError(res: Response, code: number, e: unknown, id: number) {
     id
   });
 }
-export function indexWithAddress(address: string): number {
-  const hash = crypto.createHash('sha256').update(address).digest('hex');
-  // Convert the hex hash to a number and limit to 9 digits
-  // Using modulo 1000000000 ensures number is between 0 and 999999999
-  return Number(BigInt(`0x${hash}`) % 1000000000n);
-}
 
 export const sleep = (ms: number) =>
   new Promise(resolve => setTimeout(resolve, ms));
