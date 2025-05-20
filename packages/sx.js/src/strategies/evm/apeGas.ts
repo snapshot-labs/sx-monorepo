22707c22600e7d28bb0d82f3e7fd3d10165f1d2c
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-05-20 16:49:27 +0200

    chore: update strategy with latest changes
    Matches new contract.
    
    Tries to ping API for proof (API is currently broken),
    if null block is used tries to return live balance (ignores delegations
    atm).

--- packages/sx.js/src/strategies/evm/apeGas.ts
+++ packages/sx.js/src/strategies/evm/apeGas.ts
@@ -2,7 +2,17 @@ import { defaultAbiCoder } from '@ethersproject/abi';
 import { Contract } from '@ethersproject/contracts';
 import { Provider } from '@ethersproject/providers';
 import ApeGasVotingStrategy from './abis/ApeGasVotingStrategy.json';
-import { Strategy } from '../../clients/evm/types';
+import SpaceAbi from '../../clients/evm/ethereum-tx/abis/Space.json';
+import {
+  ClientConfig,
+  Propose,
+  Strategy,
+  StrategyConfig,
+  Vote
+} from '../../clients/evm/types';
+import { VotingPowerDetailsError } from '../../utils/errors';
+
+const API_URL = 'https://apevote.api.herodotus.cloud';
 
 const MOCK_USER_PARAMS = {
   address: '0xfEDE39f346C1c65d07F2FA476d5f4727A0d7dC43',
@@ -54,13 +64,27 @@ const MOCK_USER_PARAMS = {
   voting_power: '0x12b2040c8b03190485c7'
 };
 
-function getUserParams() {
-  // Metadata: { delegationId, decimals }
+async function getUserParams({
+  viewId,
+  blockNumber,
+  voterAddress
+}: {
+  viewId: string;
+  blockNumber: number;
+  voterAddress: string;
+}) {
+  const res = await fetch(
+    `${API_URL}/votes/params/${viewId}/${blockNumber}/${voterAddress}`
+  );
 
-  // TODO: Fetch params from API/votes/params/0x0000000000000000000000000000000000000000000000000000000000000002/17400000/0xfede39f346c1c65d07f2fa476d5f4727a0d7dc43
-  // - DelegationID
-  // - Block
-  // - VoterAddress
+  const data = await res.json();
+  if (data.error) {
+    throw new VotingPowerDetailsError(
+      'Block is not cached',
+      'apeGas',
+      'NOT_READY_YET'
+    );
+  }
 
   return defaultAbiCoder.encode(
     [
@@ -81,18 +105,61 @@ function getUserParams() {
 export default function createApeGasStrategy(): Strategy {
   return {
     type: 'apeGas',
-    async getParams(): Promise<string> {
-      return getUserParams();
+    async getParams(
+      call: 'propose' | 'vote',
+      strategyConfig: StrategyConfig,
+      signerAddress: string,
+      metadata: Record<string, any> | null,
+      data: Propose | Vote,
+      clientConfig: ClientConfig
+    ): Promise<string> {
+      if (call === 'propose') {
+        throw new Error('Not supported for proposing');
+      }
+
+      if (!metadata) {
+        throw new Error('Invalid metadata.');
+      }
+
+      const { space, proposal } = data as Vote;
+      const spaceContract = new Contract(
+        space,
+        SpaceAbi,
+        clientConfig.provider
+      );
+
+      const { startBlockNumber } = await spaceContract.proposals(proposal);
+
+      return getUserParams({
+        viewId: metadata.delegationId,
+        blockNumber: startBlockNumber,
+        voterAddress: signerAddress
+      });
     },
     async getVotingPower(
       strategyAddress: string,
       voterAddress: string,
       metadata: Record<string, any> | null,
-      block: number,
+      block: number | null,
       params: string,
       provider: Provider
     ): Promise<bigint> {
-      const userParams = getUserParams();
+      if (block === null) {
+        // TODO: Handle delegations
+
+        const balance = await provider.getBalance(voterAddress);
+        return balance.toBigInt();
+      }
+
+      if (!metadata) {
+        throw new Error('Invalid metadata.');
+      }
+
+      const userParams = await getUserParams({
+        viewId: metadata.delegationId,
+        blockNumber: block,
+        voterAddress
+      });
 
       const apeGasVotingStrategyContract = new Contract(
         strategyAddress,
