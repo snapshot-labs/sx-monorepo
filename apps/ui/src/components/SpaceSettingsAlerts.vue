18fc7648dce246a46dd41f74adbf3543e722ae9b
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-06-23 17:40:16 +0400

    fix: always show space alerts on space settings

--- apps/ui/src/components/SpaceSettingsAlerts.vue
+++ apps/ui/src/components/SpaceSettingsAlerts.vue
@@ -5,10 +5,7 @@ import { Space } from '@/types';
 
 const props = defineProps<{ space: Space; activeTab: string }>();
 
-const { isController, isAdmin } = useSpaceSettings(toRef(props, 'space'));
 const { alerts } = useSpaceAlerts(toRef(props, 'space'));
-
-const isSpaceAdmin = computed(() => isController.value || isAdmin.value);
 
 const unsupportedProOnlyStrategies = computed(
   () =>
@@ -31,61 +28,58 @@ const deprecatedStrategies = computed(
       ?.strategies.map(s => `"${s}"`) || []
 );
 
-const hasAlerts = computed(
+const hasVotingStrategiesAlerts = computed(
   () =>
-    unsupportedProOnlyStrategies.value.length > 0 ||
+    (props.activeTab === 'voting-strategies' &&
+      unsupportedProOnlyStrategies.value.length > 0) ||
     deprecatedStrategies.value.length > 0 ||
     unsupportedProOnlyNetworks.value.length > 0
 );
 </script>
 
 <template>
-  <template v-if="activeTab === 'voting-strategies'">
-    <div v-if="isSpaceAdmin && hasAlerts" class="space-y-2" v-bind="$attrs">
-      <UiAlert v-if="deprecatedStrategies.length" type="error">
-        The {{ prettyConcat(deprecatedStrategies, 'and') }}
-        {{ deprecatedStrategies.length > 1 ? 'strategies are' : 'strategy is' }}
-        deprecated and no longer supported.
-        <AppLink
-          to="https://help.snapshot.box/en/articles/11638664-migrating-from-multichain-voting-strategy"
-          class="inline-flex items-center"
-        >
-          See migration guide
-          <IH-arrow-sm-right class="-rotate-45" />
-        </AppLink>
-      </UiAlert>
-      <UiAlert v-if="unsupportedProOnlyStrategies.length" type="error">
-        The
-        {{ prettyConcat(unsupportedProOnlyStrategies, 'and') }}
-        {{
-          unsupportedProOnlyStrategies.length > 1 ? 'strategies' : 'strategy'
-        }}
-        require Snapshot Pro.
-        <AppLink :to="{ name: 'space-pro' }">Upgrade to Pro</AppLink>
-        or
-        <AppLink
-          to="https://help.snapshot.box/en/articles/11568442-migrating-from-delegation-to-with-delegation-strategy"
-          class="inline-flex items-center"
-        >
-          follow migration guide
-          <IH-arrow-sm-right class="-rotate-45" />
-        </AppLink>
-      </UiAlert>
-      <UiAlert v-if="unsupportedProOnlyNetworks.length" type="error">
-        The
-        {{ prettyConcat(unsupportedProOnlyNetworks, 'and') }}
-        {{ unsupportedProOnlyNetworks.length > 1 ? 'networks' : 'network' }}
-        used by your space and/or its strategies require Snapshot Pro.
-        <AppLink :to="{ name: 'space-pro' }">Upgrade to Pro</AppLink>
-        or
-        <AppLink
-          to="https://help.snapshot.box/en/articles/10478752-what-are-the-premium-networks"
-          class="inline-flex items-center"
-        >
-          change to a premium network
-          <IH-arrow-sm-right class="-rotate-45" />
-        </AppLink>
-      </UiAlert>
-    </div>
-  </template>
+  <div v-if="hasVotingStrategiesAlerts" class="space-y-2" v-bind="$attrs">
+    <UiAlert v-if="deprecatedStrategies.length" type="error">
+      The {{ prettyConcat(deprecatedStrategies, 'and') }}
+      {{ deprecatedStrategies.length > 1 ? 'strategies are' : 'strategy is' }}
+      deprecated and no longer supported.
+      <AppLink
+        to="https://help.snapshot.box/en/articles/11638664-migrating-from-multichain-voting-strategy"
+        class="inline-flex items-center"
+      >
+        See migration guide
+        <IH-arrow-sm-right class="-rotate-45" />
+      </AppLink>
+    </UiAlert>
+    <UiAlert v-if="unsupportedProOnlyStrategies.length" type="error">
+      The
+      {{ prettyConcat(unsupportedProOnlyStrategies, 'and') }}
+      {{ unsupportedProOnlyStrategies.length > 1 ? 'strategies' : 'strategy' }}
+      require Snapshot Pro.
+      <AppLink :to="{ name: 'space-pro' }">Upgrade to Pro</AppLink>
+      or
+      <AppLink
+        to="https://help.snapshot.box/en/articles/11568442-migrating-from-delegation-to-with-delegation-strategy"
+        class="inline-flex items-center"
+      >
+        follow migration guide
+        <IH-arrow-sm-right class="-rotate-45" />
+      </AppLink>
+    </UiAlert>
+    <UiAlert v-if="unsupportedProOnlyNetworks.length" type="error">
+      The
+      {{ prettyConcat(unsupportedProOnlyNetworks, 'and') }}
+      {{ unsupportedProOnlyNetworks.length > 1 ? 'networks' : 'network' }}
+      used by your space and/or its strategies require Snapshot Pro.
+      <AppLink :to="{ name: 'space-pro' }">Upgrade to Pro</AppLink>
+      or
+      <AppLink
+        to="https://help.snapshot.box/en/articles/10478752-what-are-the-premium-networks"
+        class="inline-flex items-center"
+      >
+        change to a premium network
+        <IH-arrow-sm-right class="-rotate-45" />
+      </AppLink>
+    </UiAlert>
+  </div>
 </template>
