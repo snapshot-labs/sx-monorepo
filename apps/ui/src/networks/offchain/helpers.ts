b9bb52ca2d3cffc5a305d0c77ab89f267bb95fe5
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-07-07 18:59:43 +0400

    refactor: move offchain snapshot handing to helper

--- apps/ui/src/networks/offchain/helpers.ts
+++ apps/ui/src/networks/offchain/helpers.ts
@@ -1,4 +1,11 @@
+import { getProvider } from '@/helpers/provider';
+import { METADATA as STARKNET_METADATA } from '@/networks/starknet';
 import { Choice } from '@/types';
+import { EDITOR_SNAPSHOT_OFFSET } from './constants';
+
+const STARKNET_CHAIN_IDS: string[] = Object.values(STARKNET_METADATA).map(
+  metadata => metadata.chainId
+);
 
 export function getSdkChoice(
   type: string,
@@ -23,4 +30,21 @@ export function getSdkChoice(
   }
 
   throw new Error('Vote type not supported');
+}
+
+export function isStarknetChainId(chainId: string): boolean {
+  return STARKNET_CHAIN_IDS.includes(chainId);
+}
+
+export async function getLatestBlockNumber(chainId: string): Promise<number> {
+  // TODO: remove this check after implementing starknet support on getProvider
+  if (isStarknetChainId(chainId)) {
+    throw new Error(
+      'Proposal creation not supported for spaces on Starknet network'
+    );
+  }
+
+  const provider = getProvider(Number(chainId));
+
+  return (await provider.getBlockNumber()) - EDITOR_SNAPSHOT_OFFSET;
 }
