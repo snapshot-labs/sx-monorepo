b44a8401648fd6406b9e9aac70df2f1023231c25
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-07-15 19:12:40 +0400

    feat: get controller for .sonic domains
    Revert "feat: get controller for .sonic domains"
    
    This reverts commit 6a856b3822c0fdbc5e9c667019a1a8c050052e2b.

--- apps/ui/src/helpers/utils.ts
+++ apps/ui/src/helpers/utils.ts
@@ -1,6 +1,7 @@
 import { sanitizeUrl as baseSanitizeUrl } from '@braintree/sanitize-url';
 import { FunctionFragment } from '@ethersproject/abi';
 import { getAddress, isAddress } from '@ethersproject/address';
+import { namehash } from '@ethersproject/hash';
 import { Web3Provider } from '@ethersproject/providers';
 import { upload as pin } from '@snapshot-labs/pineapple';
 import Autolinker from 'autolinker';
@@ -17,11 +18,13 @@ import { RouteParamsRaw } from 'vue-router';
 import { getSpaceController as getEnsSpaceController } from '@/helpers/ens';
 import { VotingPowerItem } from '@/queries/votingPower';
 import { ChainId, Choice, NetworkID, Proposal, SpaceMetadata } from '@/types';
+import { call } from './call';
 import {
   EVM_EMPTY_ADDRESS,
   MAX_FILE_SIZE_BYTES,
   MAX_SYMBOL_LENGTH
 } from './constants';
+import { getProvider } from './provider';
 import { getOwner } from './stamp';
 import pkg from '@/../package.json';
 import ICCoingecko from '~icons/c/coingecko';
@@ -717,6 +720,30 @@ export function getUserFacingErrorMessage(
   return (e instanceof Error && e.message) || fallback;
 }
 
+async function getUnstoppableDomainsNameOwner(name: string, chainId: number) {
+  const registries = {
+    146: '0xDe1DAdcF11a7447C3D093e97FdbD513f488cE3b4' // Sonic
+  };
+
+  if (!registries[chainId]) {
+    throw new Error('Unsupported network');
+  }
+
+  const provider = getProvider(chainId);
+  const tokenId = BigInt(namehash(name));
+
+  return call(
+    provider,
+    [
+      'function ownerOf(uint256 tokenId) external view returns (address address)'
+    ],
+    [registries[chainId], 'ownerOf', [tokenId]],
+    {
+      blockTag: 'latest'
+    }
+  );
+}
+
 export async function getSpaceController(id: string, network: NetworkID) {
   const chainMapping = {
     ens: {
@@ -726,6 +753,9 @@ export async function getSpaceController(id: string, network: NetworkID) {
     shibarium: {
       s: 109,
       's-tn': 157
+    },
+    sonic: {
+      s: 146
     }
   };
 
@@ -733,6 +763,10 @@ export async function getSpaceController(id: string, network: NetworkID) {
     const owner = await getOwner(id, chainMapping.shibarium[network]);
 
     return owner || EVM_EMPTY_ADDRESS;
+  }
+
+  if (id.endsWith('.sonic')) {
+    return getUnstoppableDomainsNameOwner(id, chainMapping.sonic[network]);
   }
 
   return getEnsSpaceController(id, chainMapping.ens[network]);
