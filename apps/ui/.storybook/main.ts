8492ccb65f2055be14d81cfc7bebc9e5a9977093
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-07-24 13:34:09 +0200

    chore: remove storybook vite integration
    I don't think vite integration is essential for us (it only handles
    interaction tests), doing UI unit tests with it is not optimal.
    
    For now removing it for simplicity, we can bring it back if we want.

--- apps/ui/.storybook/main.ts
+++ apps/ui/.storybook/main.ts
@@ -14,7 +14,6 @@ const config: StorybookConfig = {
   addons: [
     getAbsolutePath('@storybook/addon-docs'),
     getAbsolutePath('@storybook/addon-a11y'),
-    getAbsolutePath('@storybook/addon-vitest'),
     getAbsolutePath('@storybook/addon-themes')
   ],
   framework: {
--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -1,8 +1,5 @@
-/// <reference types="vitest/config" />
-import { fileURLToPath } from 'node:url';
 import path from 'path';
 import inject from '@rollup/plugin-inject';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import vue from '@vitejs/plugin-vue';
 import { visualizer } from 'rollup-plugin-visualizer';
 import AutoImport from 'unplugin-auto-import/vite';
@@ -11,11 +8,6 @@ import IconsResolver from 'unplugin-icons/resolver';
 import Icons from 'unplugin-icons/vite';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
-
-const dirname =
-  typeof __dirname !== 'undefined'
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 const ELECTRON = process.env.ELECTRON || false;
@@ -99,33 +91,5 @@ export default defineConfig({
       buffer: path.resolve('../../node_modules/buffer')
     },
     dedupe: ['@popperjs/core']
-  },
-  test: {
-    projects: [
-      {
-        extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({
-            configDir: path.join(dirname, '.storybook')
-          })
-        ],
-        test: {
-          name: 'storybook',
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: 'playwright',
-            instances: [
-              {
-                browser: 'chromium'
-              }
-            ]
-          },
-          setupFiles: ['.storybook/vitest.setup.ts']
-        }
-      }
-    ]
   }
 });
