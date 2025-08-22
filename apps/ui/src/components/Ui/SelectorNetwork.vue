snapshot-labs/sx-monorepo

commit b263b5cb757fa526eae0c2df343a83358a4ff67f

fix: allow starknet network on offchain spaces (#1447)
* fix: update to latest snapshot.js package with starknet support

* fix: use starknet networks from networks.json

* fix: merge offchain and full network kind

* fix: premium networks list should use string keys

* fix: handle error when proposing on offchain spaces on starknet network

* fix: improve readability

* fix: better string conversion

apps/ui/src/components/Ui/SelectorNetwork.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { getUrl } from '@/helpers/utils';
 import { enabledNetworks, getNetwork } from '@/networks';
-import { METADATA as STARKNET_NETWORK_METADATA } from '@/networks/starknet';
 import { BaseDefinition, NetworkID } from '@/types';
 
 const network = defineModel<string | number | null>({
@@ -11,7 +10,7 @@ const network = defineModel<string | number | null>({
 const props = defineProps<{
   definition: BaseDefinition<string | number | null> & {
     networkId: NetworkID;
-    networksListKind?: 'full' | 'builtin' | 'offchain';
+    networksListKind?: 'full' | 'builtin';
     networksFilter?: unknown[];
   };
 }>();
@@ -20,8 +19,8 @@ const { networks } = useOffchainNetworksList(props.definition.networkId);
 
 const allOptions = computed(() => {
   const networksListKind = props.definition.networksListKind;
-  if (networksListKind === 'full' || networksListKind === 'offchain') {
-    const baseNetworks = networks.value
+  if (networksListKind === 'full') {
+    return networks.value
       .filter(network => {
         if (
           props.definition.networkId === 's' &&
@@ -34,7 +33,7 @@ const allOptions = computed(() => {
         return true;
       })
       .map(network => ({
-        id: network.chainId,
+        id: String(network.chainId),
         name: network.name,
         icon: h('img', {
           src: getUrl(network.logo),
@@ -42,31 +41,6 @@ const allOptions = computed(() => {
           class: 'rounded-full'
         })
       }));
-    if (networksListKind === 'offchain') return baseNetworks;
-
-    return [
-      ...baseNetworks,
-      ...Object.values(STARKNET_NETWORK_METADATA)
-        .filter(metadata => {
-          if (
-            props.definition.networkId === 's' &&
-            metadata.name.includes('Sepolia')
-          ) {
-            return false;
-          }
-
-          return true;
-        })
-        .map(metadata => ({
-          id: metadata.chainId,
-          name: metadata.name,
-          icon: h('img', {
-            src: getUrl(metadata.avatar),
-            alt: metadata.name,
-            class: 'rounded-full'
-          })
-        }))
-    ];
   }
 
   return enabledNetworks

apps/ui/src/components/ButtonFollow.vue
@@ -3,7 +3,7 @@ import { getUserFacingErrorMessage } from '@/helpers/utils';
 import { NetworkID } from '@/types';
 
 const props = defineProps<{
-  space: { id: string; network: NetworkID; snapshot_chain_id?: number };
+  space: { id: string; network: NetworkID; snapshot_chain_id?: string };
 }>();
 
 const { isSafeWallet } = useSafeWallet(

apps/ui/src/components/FormSpaceStrategies.vue
@@ -2,7 +2,7 @@
 import { StrategyConfig } from '@/networks/types';
 import { NetworkID, Space } from '@/types';
 
-const snapshotChainId = defineModel<number>('snapshotChainId', {
+const snapshotChainId = defineModel<string>('snapshotChainId', {
   required: true
 });
 const strategies = defineModel<StrategyConfig[]>('strategies', {
@@ -48,7 +48,7 @@ function handleTestStrategies(strategies: StrategyConfig[]) {
           'The default network used for this space. Networks can also be specified in individual strategies',
         examples: ['Select network'],
         networkId,
-        networksListKind: 'offchain'
+        networksListKind: 'full'
       }"
     />
   </div>

apps/ui/src/components/Modal/Delegate.vue
@@ -177,22 +177,7 @@ function isValidDelegation(delegation?: SpaceMetadataDelegation): boolean {
 }
 
 function getNetworkDetails(chainId: number | string) {
-  if (typeof chainId === 'number') {
-    return networks[chainId];
-  }
-
-  const starknetNetwork = Object.entries(STARKNET_NETWORK_METADATA).find(
-    ([, { chainId: starknetChainId }]) => starknetChainId === chainId
-  )?.[0];
-
-  if (!starknetNetwork) {
-    return { name: 'Unknown network', logo: '' };
-  }
-
-  return {
-    name: STARKNET_NETWORK_METADATA[starknetNetwork].name,
-    logo: STARKNET_NETWORK_METADATA[starknetNetwork].avatar
-  };
+  return networks[chainId] || { name: 'Unknown network', logo: '' };
 }
 
 async function handleSubmit() {

apps/ui/src/components/Modal/EditStrategy.vue
@@ -167,7 +167,7 @@ watchEffect(() => {
           title: 'Network',
           examples: ['Select network'],
           networkId,
-          networksListKind: 'offchain'
+          networksListKind: 'full'
         }"
       />
       <UiForm

apps/ui/src/components/Modal/TestStrategy.vue
@@ -114,7 +114,7 @@ async function handleSubmit() {
       null,
       {
         id: props.spaceId,
-        snapshot_chain_id: props.chainId as number | undefined,
+        snapshot_chain_id: props.chainId as string | undefined,
         network: props.networkId,
         voting_power_symbol: props.votingPowerSymbol
       },

apps/ui/src/components/Ui/BadgeNetwork.vue
@@ -34,7 +34,7 @@ const networkData = computed<NetworkData | null>(() => {
     } catch {}
   }
 
-  if (props.chainId && typeof props.chainId === 'number') {
+  if (props.chainId) {
     try {
       const network: SnapshotJSNetwork | undefined =
         networks[String(props.chainId)];

apps/ui/src/components/Ui/InputAddress.vue
@@ -1,18 +1,7 @@
 <script setup lang="ts">
 import snapshotJsNetworks from '@snapshot-labs/snapshot.js/src/networks.json';
 import { getUrl } from '@/helpers/utils';
-import { METADATA as STARKNET_NETWORK_METADATA } from '@/networks/starknet';
 import { BaseDefinition } from '@/types';
-
-const STARKNET_NETWORKS = Object.fromEntries(
-  Object.values(STARKNET_NETWORK_METADATA).map(metadata => [
-    metadata.chainId,
-    {
-      name: metadata.name,
-      logoUrl: getUrl(metadata.avatar)
-    }
-  ])
-);
 
 type NetworkDetails = {
   name: string;
@@ -44,9 +33,7 @@ const networkDetails = computed<NetworkDetails | null>(() => {
 
   if (!chainId) return null;
 
-  if (typeof chainId === 'string' && chainId in STARKNET_NETWORKS) {
-    return STARKNET_NETWORKS[chainId];
-  } else if (chainId in snapshotJsNetworks) {
+  if (chainId in snapshotJsNetworks) {
     const network = snapshotJsNetworks[chainId];
     return {
       name: network.name,

commit a8cd0410147d66a929c15b14dffe90a2a0e11c3e

fix: keep original type for chainId on onchain treasuries
Regression from https://github.com/snapshot-labs/sx-monorepo/pull/1447

Refactoring it elsewhere is quite difficult, so to keep it simple for
now we just revert part of original PR to only affect offchain spaces.

apps/ui/src/components/Ui/SelectorNetwork.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { getUrl } from '@/helpers/utils';
-import { enabledNetworks, getNetwork } from '@/networks';
+import { enabledNetworks, getNetwork, offchainNetworks } from '@/networks';
 import { BaseDefinition, NetworkID } from '@/types';
 
 const network = defineModel<string | number | null>({
@@ -33,7 +33,10 @@ const allOptions = computed(() => {
         return true;
       })
       .map(network => ({
-        id: String(network.chainId),
+        // NOTE: onchain networks should keep default chainId type
+        id: offchainNetworks.includes(props.definition.networkId)
+          ? String(network.chainId)
+          : network.chainId,
         name: network.name,
         icon: h('img', {
           src: getUrl(network.logo),