d7e62bbfac255ff6a89d8cf5a8f8d50f702e5838
ChaituVR <yourchaitu@gmail.com>
2025-10-01 01:09:07 +0530

    fix:  open modal only when provider is not set
    ### Summary
    - adjusted logic to open modal only when provider is not set

--- apps/ui/src/helpers/connectors/walletconnect.ts
+++ apps/ui/src/helpers/connectors/walletconnect.ts
@@ -83,13 +83,8 @@ export default class Walletconnect extends Connector {
         projectId
       });
 
-      // This is needed in case the user changes the theme mode
-      // otherwise modal will be opened with half light and half dark theme
-      await this.modal.setThemeMode(currentTheme.value);
-
-      if (!isAutoConnect) {
+      if (!isAutoConnect && !this.provider) {
         await this.disconnect();
-
         await this.modal.open();
       }
 
