253f838f251930af5719e9f0ade22a8bc423c9eb
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-10-10 14:31:42 +0200

    chore: remove extra as const

--- apps/api/src/evm/utils.ts
+++ apps/api/src/evm/utils.ts
@@ -105,7 +105,7 @@ export async function getTimestampFromBlock({
           stateMutability: 'view',
           type: 'function'
         }
-      ] as const,
+      ],
       functionName: 'getBlockNumber',
       blockNumber: BigInt(currentBlockNumber)
     });
