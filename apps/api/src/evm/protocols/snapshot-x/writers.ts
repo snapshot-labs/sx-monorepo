3639b71fcafbcd59d979ec16d34f19a0eac01390
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-10-10 10:32:56 +0200

    refactor(api): replace @ethersproject/keccak256 with viem

--- apps/api/src/evm/protocols/snapshot-x/writers.ts
+++ apps/api/src/evm/protocols/snapshot-x/writers.ts
@@ -1,10 +1,9 @@
 import { defaultAbiCoder } from '@ethersproject/abi';
 import { BigNumber } from '@ethersproject/bignumber';
 import { Contract } from '@ethersproject/contracts';
-import { keccak256 } from '@ethersproject/keccak256';
 import { StaticJsonRpcProvider } from '@ethersproject/providers';
 import { evm } from '@snapshot-labs/checkpoint';
-import { getAddress } from 'viem';
+import { getAddress, keccak256 } from 'viem';
 import AxiomExecutionStrategyAbi from './abis/AxiomExecutionStrategy';
 import L1AvatarExecutionStrategyAbi from './abis/L1AvatarExecutionStrategy';
 import L1AvatarExecutionStrategyFactoryAbi from './abis/L1AvatarExecutionStrategyFactory';
