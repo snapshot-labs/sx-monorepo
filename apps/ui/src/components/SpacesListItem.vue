3f9778c283131665db615b85091261beb900c1d7
less <fabien@bonustrack.co>
2025-10-11 05:18:20 +0700

    refactor(ui): update layout of SpacesListItem component for improved styling and structure

--- apps/ui/src/components/SpacesListItem.vue
+++ apps/ui/src/components/SpacesListItem.vue
@@ -10,43 +10,41 @@ const compositeSpaceId = `${props.space.network}:${props.space.id}`;
 <template>
   <AppLink
     :to="{ name: 'space-overview', params: { space: compositeSpaceId } }"
-    class="text-skin-text px-4 block relative group overflow-hidden"
+    class="text-skin-text mx-4 group overflow-hidden flex border-b items-center py-3.5 space-x-2"
   >
-    <div class="flex border-b items-center py-3.5">
-      <div class="mr-2 shrink-0">
-        <UiBadgeNetwork
-          :id="space.network"
-          :size="!offchainNetworks.includes(space.network) ? 16 : 0"
-        >
-          <SpaceAvatar :space="space" :size="32" class="rounded-md" />
-        </UiBadgeNetwork>
-      </div>
-      <div class="grow flex items-center truncate mr-2">
-        <h3 class="truncate" v-text="space.name" />
-        <UiBadgeVerified
-          class="ml-1"
-          :verified="space.verified"
-          :turbo="space.turbo"
-        />
-      </div>
-      <ButtonFollow :space="space" class="hidden group-hover:block -my-2" />
-      <div class="text-[21px] font-bold flex text-center">
-        <span
-          v-if="space.protocol === 'snapshot'"
-          class="w-[50px] md:w-[100px]"
-          :class="{ 'text-skin-success': (space.active_proposals ?? 0) > 0 }"
-          v-text="_n(space.active_proposals ?? 0, 'compact')"
-        />
-        <span
-          class="text-skin-link w-[100px] hidden md:block"
-          v-text="_n(space.proposal_count, 'compact')"
-        />
-        <span
-          v-if="space.follower_count !== undefined"
-          class="text-skin-link w-[100px] hidden md:block"
-          v-text="_n(space.follower_count, 'compact')"
-        />
-      </div>
+    <div class="shrink-0">
+      <UiBadgeNetwork
+        :id="space.network"
+        :size="!offchainNetworks.includes(space.network) ? 16 : 0"
+      >
+        <SpaceAvatar :space="space" :size="32" class="rounded-md" />
+      </UiBadgeNetwork>
+    </div>
+    <div class="grow flex items-center truncate">
+      <h3 class="truncate" v-text="space.name" />
+      <UiBadgeVerified
+        class="ml-1"
+        :verified="space.verified"
+        :turbo="space.turbo"
+      />
+    </div>
+    <ButtonFollow :space="space" class="hidden group-hover:block -my-2" />
+    <div class="text-[21px] font-bold flex text-center">
+      <span
+        v-if="space.protocol === 'snapshot'"
+        class="w-[50px] md:w-[100px]"
+        :class="{ 'text-skin-success': (space.active_proposals ?? 0) > 0 }"
+        v-text="_n(space.active_proposals ?? 0, 'compact')"
+      />
+      <span
+        class="text-skin-link w-[100px] hidden md:block"
+        v-text="_n(space.proposal_count, 'compact')"
+      />
+      <span
+        v-if="space.follower_count !== undefined"
+        class="text-skin-link w-[100px] hidden md:block"
+        v-text="_n(space.follower_count, 'compact')"
+      />
     </div>
   </AppLink>
 </template>
