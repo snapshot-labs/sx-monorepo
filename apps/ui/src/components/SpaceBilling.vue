ca0af4bf6639938b8d9e455be178afd61c1c838f
Chaitanya <yourchaitu@gmail.com>
2025-10-29 00:21:53 +0530

    feat: add billing page (#1642)
    * feat: add billing page
    
    * changed amount_raw to amount_decimal
    
    * fix titlecase
    
    * enhance button navigation and use same style for  all status badges
    
    * add usePayments composable
    
    * refactor
    
    * refactor, more refactor and add pagination
    
    * refactor
    
    * Changes from design review
    
    * Update apps/ui/src/components/SpaceBilling.vue
    
    Co-authored-by: Wan <495709+wa0x6e@users.noreply.github.com>
    
    * Update apps/ui/src/components/SpaceBilling.vue
    
    Co-authored-by: Wan <495709+wa0x6e@users.noreply.github.com>
    
    * Update apps/ui/src/components/SpaceBilling.vue
    
    Co-authored-by: Wan <495709+wa0x6e@users.noreply.github.com>
    
    * remove composable and other review comments
    
    * fix
    
    * add check for turbo expiration date
    
    * use UiStateWarning
    
    * move the "Billing" menu item after "Controller"
    
    ---------
    
    Co-authored-by: Wan <495709+wa0x6e@users.noreply.github.com>

---
+++ apps/ui/src/components/SpaceBilling.vue
@@ -0,0 +1,173 @@
+<script setup lang="ts">
+import dayjs from 'dayjs';
+import { getGenericExplorerUrl } from '@/helpers/generic';
+import { _n } from '@/helpers/utils';
+import { getNetwork } from '@/networks';
+import { usePaymentsQuery } from '@/queries/payments';
+import { Space } from '@/types';
+
+const props = defineProps<{ space: Space }>();
+
+const {
+  data,
+  isPending,
+  isError,
+  fetchNextPage,
+  hasNextPage,
+  isFetchingNextPage
+} = usePaymentsQuery(
+  toRef(() => props.space.id),
+  toRef(() => props.space.network)
+);
+
+const chainId = computed(() => getNetwork(props.space.network).chainId);
+const turboExpirationDate = computed(() =>
+  dayjs(props.space.turbo_expiration * 1000)
+);
+const payments = computed(() => data.value?.pages.flat() || []);
+const statusText = computed(() => {
+  if (!props.space.turbo) return 'Free';
+
+  const daysUntilExpiration = turboExpirationDate.value.diff(dayjs(), 'day');
+
+  if (!props.space.turbo_expiration) {
+    return '';
+  }
+
+  if (daysUntilExpiration === 0) {
+    return 'Expires today';
+  }
+
+  if (daysUntilExpiration < 0) {
+    return 'Expired';
+  }
+
+  if (daysUntilExpiration < 30) {
+    return `${daysUntilExpiration} day${daysUntilExpiration !== 1 ? 's' : ''} left`;
+  }
+
+  return 'Active';
+});
+</script>
+
+<template>
+  <div>
+    <UiContainerSettings
+      class="px-4 pt-4"
+      title="Billing"
+      description="Manage your subscription and view billing information."
+    >
+      <UiEyebrow class="mb-3 font-medium">Current plan</UiEyebrow>
+
+      <div class="border rounded-xl p-4">
+        <div class="flex flex-col gap-3 sm:flex-row sm:items-center">
+          <div class="flex items-start sm:items-center sm:flex-1">
+            <div class="flex-1">
+              <h5 class="text-lg font-semibold text-skin-heading">
+                {{ props.space.turbo ? 'Snapshot Pro' : 'Basic plan' }}
+              </h5>
+              <p class="text-sm text-skin-text">
+                <template
+                  v-if="props.space.turbo && props.space.turbo_expiration"
+                >
+                  Valid until
+                  <span class="font-semibold">{{
+                    turboExpirationDate.format('MMM D, YYYY')
+                  }}</span>
+                </template>
+                <template
+                  v-else-if="props.space.turbo && !props.space.turbo_expiration"
+                >
+                  Unknown expiration date
+                </template>
+                <template v-else>
+                  Upgrade to Pro for premium features
+                </template>
+              </p>
+            </div>
+            {{ statusText }}
+          </div>
+
+          <UiButton
+            :to="{ name: 'space-pro' }"
+            class="primary w-full sm:w-auto"
+          >
+            {{ props.space.turbo ? 'Extend plan' : 'Upgrade to Pro' }}
+          </UiButton>
+        </div>
+      </div>
+    </UiContainerSettings>
+
+    <UiSectionHeader class="mt-4" label="Payment history" sticky />
+    <UiColumnHeader class="space-x-3">
+      <div class="w-[190px] grow sm:grow-0 text-left">Date</div>
+      <div class="hidden sm:flex grow">Type</div>
+      <div class="w-[150px] flex justify-end">Amount</div>
+      <div class="min-w-3.5" />
+    </UiColumnHeader>
+
+    <UiLoading v-if="isPending" class="px-4 py-3 block" />
+    <UiStateWarning v-else-if="isError" class="px-4 py-3">
+      Failed to load payment history.
+    </UiStateWarning>
+    <UiStateWarning
+      v-else-if="!payments || payments.length === 0"
+      class="px-4 py-3"
+    >
+      No payment history available.
+    </UiStateWarning>
+    <UiContainerInfiniteScroll
+      v-else
+      class="px-4"
+      :loading-more="isFetchingNextPage"
+      @end-reached="hasNextPage && fetchNextPage()"
+    >
+      <div
+        v-for="payment in payments"
+        :key="payment.id"
+        class="border-b flex space-x-3 py-3"
+      >
+        <div class="flex grow sm:grow-0 w-[190px] items-center">
+          {{ dayjs((payment.timestamp || 0) * 1000).format('MMM D, YYYY') }}
+        </div>
+        <div class="hidden sm:flex grow w-0 text-[17px] capitalize">
+          {{ payment.type === 'turbo' ? 'snapshot pro' : payment.type }}
+        </div>
+        <div
+          class="w-[150px] flex flex-col sm:shrink-0 text-right justify-center"
+        >
+          <span class="text-skin-link font-semibold">
+            {{ _n(payment.amount_decimal) }}
+            {{ payment.token_symbol }}
+          </span>
+        </div>
+
+        <UiDropdown>
+          <template #button>
+            <div class="flex items-center h-full">
+              <button type="button" class="text-skin-link">
+                <IH-dots-horizontal />
+              </button>
+            </div>
+          </template>
+          <template #items>
+            <UiDropdownItem v-slot="{ active }">
+              <a
+                :href="
+                  getGenericExplorerUrl(chainId, payment.id, 'transaction') ||
+                  ''
+                "
+                target="_blank"
+                class="flex items-center gap-2"
+                :class="{ 'opacity-80': active }"
+              >
+                <IH-arrow-sm-right class="-rotate-45" :width="16" />
+                View transaction
+              </a>
+            </UiDropdownItem>
+          </template>
+        </UiDropdown>
+      </div>
+    </UiContainerInfiniteScroll>
+  </div>
+</template>
