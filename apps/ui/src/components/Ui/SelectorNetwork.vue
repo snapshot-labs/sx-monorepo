abb88fe0bd6405744a54573ee282d4eb1a248a02
ChaituVR <yourchaitu@gmail.com>
2025-11-14 16:41:07 +0530

    fix: ensure networksFilter uses string

--- apps/ui/src/components/Ui/SelectorNetwork.vue
+++ apps/ui/src/components/Ui/SelectorNetwork.vue
@@ -11,7 +11,7 @@ const props = defineProps<{
   definition: BaseDefinition<string | number | null> & {
     networkId: NetworkID;
     networksListKind?: 'full' | 'builtin';
-    networksFilter?: unknown[];
+    networksFilter?: string[];
   };
 }>();
 
