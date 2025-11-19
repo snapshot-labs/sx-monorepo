15647b9ce988026500655ca2643c18620292f8cf
Chaitanya <yourchaitu@gmail.com>
2025-11-19 19:26:34 +0530

    fix: use string in network selector  (#1745)
    * fix: always use string for treasury networks
    
    * fix: ensure networksFilter uses string
    
    * fix chain id in onchain delegation spaces

--- apps/ui/src/networks/common/graphqlApi/index.ts
+++ apps/ui/src/networks/common/graphqlApi/index.ts
@@ -284,7 +284,7 @@ function formatSpace(
           apiType: api_type,
           apiUrl: api_url,
           contractAddress: address === 'null' ? null : address,
-          chainId: CHAIN_IDS[network]
+          chainId: String(CHAIN_IDS[network])
         };
       }
 
@@ -293,7 +293,7 @@ function formatSpace(
         apiType: api_type,
         apiUrl: api_url,
         contractAddress: contract,
-        chainId: chain_id
+        chainId: String(chain_id)
       };
     }),
     executors: space.metadata.executors,
