c3eb892bb1e2764a1ab458108e67e009cd42cb0e
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-11-20 12:19:33 +0100

    refactor: add graphql-codegen for gnosis auction
    Moved helpers/auction.ts to helpers/auction/index.ts so we can separate
    code in there and have good place for generated gql/
    
    Hopefully no more conflicting changes in the future there.

--- apps/ui/codegen.ts
+++ apps/ui/codegen.ts
@@ -1,24 +1,34 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
+const baseConfig: CodegenConfig['config'] = {
+  preset: 'client',
+  config: {
+    enumsAsTypes: true,
+    skipTypename: true,
+    avoidOptionals: {
+      field: true,
+      inputValue: false
+    }
+  },
+  presetConfig: {
+    gqlTagName: 'gql',
+    fragmentMasking: false
+  }
+};
+
 const config: CodegenConfig = {
-  schema: '../api/.checkpoint/schema.gql',
-  documents: ['src/networks/common/graphqlApi/queries.ts'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
     './src/networks/common/graphqlApi/gql/': {
-      preset: 'client',
-      config: {
-        enumsAsTypes: true,
-        skipTypename: true,
-        avoidOptionals: {
-          field: true,
-          inputValue: false
-        }
-      },
-      presetConfig: {
-        gqlTagName: 'gql',
-        fragmentMasking: false
-      }
+      schema: '../api/.checkpoint/schema.gql',
+      documents: ['src/networks/common/graphqlApi/queries.ts'],
+      ...baseConfig
+    },
+    './src/helpers/auction/gql/': {
+      schema:
+        'https://subgrapher.snapshot.org/subgraph/arbitrum/98f9T2v1KtNnZyexiEgNLMFnYkXdKoZq9Pt1EYQGq5aH',
+      documents: ['src/helpers/auction/queries.ts'],
+      ...baseConfig
     }
   }
 };
