beb285881bc436430c14920cde268d44b6b57c84
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-11-24 21:46:35 -0500

    fix: set previous order ID when placing an order

--- apps/ui/src/helpers/auction/queries.ts
+++ apps/ui/src/helpers/auction/queries.ts
@@ -28,6 +28,7 @@ gql(`
 gql(`
   fragment order on Order {
     id
+    userId
     sellAmount
     buyAmount
     userAddress
@@ -49,6 +50,16 @@ export const ordersQuery = gql(`
   query GetOrders($id: ID!, $skip: Int, $first: Int, $orderBy: Order_orderBy, $orderDirection: OrderDirection) {
     auctionDetail(id: $id) {
       orders(orderBy: $orderBy, orderDirection: $orderDirection, skip: $skip, first: $first) {
+        ...order
+      }
+    }
+  }
+`);
+
+export const previousOrderQuery = gql(`
+  query GetPreviousOrder($id: ID!, $price: BigDecimal) {
+    auctionDetail(id: $id) {
+      ordersWithoutClaimed(orderBy: price, orderDirection: asc, where: {price_gt: $price}) {
         ...order
       }
     }
