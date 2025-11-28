c9b9b3183c1e54ee6e29129cc2e2ff9ceda94196
Chaitanya <yourchaitu@gmail.com>
2025-11-28 16:02:45 +0530

    fix: sticky header positioning on profile page (#1696)
    * fix: sticky behavior of columnHeader with and without section
    
    ### Summary
    
    - implemented sticky behavior for the column header
    - User page to utilize the new sticky behavior without section
    - adjusted tailwind config to fix naming
    
    * adjust actual height of section
    
    * simplified column header usage in User view
    
    * fix
    
    * fix: increase second-row offset
    
    * fix: increase z-index on SectionHeader
    
    * refactor: add explaination to offset
    
    * fix: use default z-index on ColumnHeaders
    
    ---------
    
    Co-authored-by: Wan <495709+wa0x6e@users.noreply.github.com>
    Co-authored-by: Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>

--- apps/ui/src/components/Ui/ColumnHeader.vue
+++ apps/ui/src/components/Ui/ColumnHeader.vue
@@ -20,7 +20,7 @@ defineExpose({
     ref="container"
     class="flex px-4 bg-skin-bg border-b w-full font-medium"
     :class="{
-      'sticky z-10  top-header-with-section-height lg:top-header-with-section-height-with-offset':
+      'sticky z-10 top-header-with-section-height-with-offset lg:top-header-with-section-height':
         sticky
     }"
   >
