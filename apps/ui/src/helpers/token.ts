47a5364e6b48c7eba1b0221f1eb6626bf44c30d5
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-12-05 17:11:44 +0100

    refactor: make token helpers more reusable
    This removes need for passing any special objects, just tokenAddress.
    
    Also you can also use it to get allowance instead of only checking if
    some budget is approved.

--- apps/ui/src/helpers/token.ts
+++ apps/ui/src/helpers/token.ts
@@ -2,39 +2,29 @@ import { Contract } from '@ethersproject/contracts';
 import { Web3Provider } from '@ethersproject/providers';
 import { abis } from '@/helpers/abis';
 
-export type Token = {
-  contractAddress: string;
-  decimals: number;
-  symbol: string;
-  name?: string;
-};
-
-export async function getIsApproved(
-  token: Token,
+export async function getTokenAllowance(
   web3: Web3Provider,
-  spenderAddress: string,
-  amount: bigint
-): Promise<boolean> {
+  tokenAddress: string,
+  spenderAddress: string
+): Promise<bigint> {
   const signer = web3.getSigner();
-  const contract = new Contract(token.contractAddress, abis.erc20, signer);
+  const contract = new Contract(tokenAddress, abis.erc20, signer);
+
   const allowance = await contract.allowance(
     await signer.getAddress(),
     spenderAddress
   );
 
-  return allowance.toBigInt() >= amount;
+  return allowance.toBigInt();
 }
 
 export async function approve(
-  token: Token,
   web3: Web3Provider,
+  tokenAddress: string,
   spenderAddress: string,
   amount: bigint
 ) {
-  const contract = new Contract(
-    token.contractAddress,
-    abis.erc20,
-    web3.getSigner()
-  );
+  const contract = new Contract(tokenAddress, abis.erc20, web3.getSigner());
+
   return contract.approve(spenderAddress, amount);
 }
