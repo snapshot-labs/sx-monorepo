c1374b3586f6950ac9d26a01f4177c71ca6e827e
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-12-18 12:12:30 -0500

    refactor: use function instead of computed value

--- apps/ui/src/components/Ui/ChartTime.vue
+++ apps/ui/src/components/Ui/ChartTime.vue
@@ -137,24 +137,24 @@ const seriesInstances = shallowRef<ISeriesApi<SeriesType>[]>([]);
 
 const { currentTheme } = useTheme();
 
-// Convert UTC timestamp to local timezone offset
-// (effectively makes the chart interpret UTC timestamps as local time)
-// see https://github.com/tradingview/lightweight-charts/blob/7104e9a4fb399f18db7a2868a91b3246014c4324/docs/time-zones.md
-// All further data manipulations should be done in UTC, to avoid double offsets
-const timeToLocale = computed(() => {
-  const timezoneOffset = new Date().getTimezoneOffset() * 60; // offset in seconds
-  return (utcTimestamp: number) => utcTimestamp - timezoneOffset;
-});
-
 const localizedSeries = computed<ChartSeries[]>(() => {
   return props.series.map(config => ({
     ...config,
     data: config.data.map(point => ({
       ...point,
-      time: timeToLocale.value(point.time as number) as SingleValueData['time']
+      time: convertTimeToLocale(point.time as number) as SingleValueData['time']
     }))
   }));
 });
+
+// Convert UTC timestamp to local timezone offset
+// (effectively makes the chart interpret UTC timestamps as local time)
+// see https://github.com/tradingview/lightweight-charts/blob/7104e9a4fb399f18db7a2868a91b3246014c4324/docs/time-zones.md
+// All further data manipulations should be done in UTC, to avoid double offsets
+function convertTimeToLocale(utcTimestamp: number): number {
+  const timezoneOffset = new Date().getTimezoneOffset() * 60; // offset in seconds
+  return utcTimestamp - timezoneOffset;
+}
 
 function createSeries(series: ChartSeries[]) {
   if (!chart.value) return;
