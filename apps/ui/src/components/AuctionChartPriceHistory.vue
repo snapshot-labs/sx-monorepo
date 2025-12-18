07d4faa124f70ddf41803d1a1a0e9061d90ac8c7
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-12-18 17:58:09 -0500

    fix: improve chart loading status

--- apps/ui/src/components/AuctionChartPriceHistory.vue
+++ apps/ui/src/components/AuctionChartPriceHistory.vue
@@ -28,7 +28,6 @@ const props = defineProps<{
 }>();
 
 const offset = ref<number | null>(null);
-const areDataReady = ref(false);
 
 const granularity = computed<ChartGranularity>(() => {
   const auctionDuration =
@@ -132,13 +131,9 @@ const chartSeries = computed<ChartSeries[]>(() => [
 watch(
   data,
   async () => {
-    areDataReady.value = false;
-
     while (hasNextPage.value && !isError.value) {
       await fetchNextPage();
     }
-
-    areDataReady.value = true;
   },
   { immediate: true }
 );
@@ -147,17 +142,13 @@ watch(
 <template>
   <div class="flex flex-col">
     <div
-      v-if="isPending || isError"
+      v-if="isPending || isError || hasNextPage"
       class="flex items-center justify-center flex-1"
     >
-      <UiLoading v-if="isPending" />
-      <template v-else-if="isError">Error while loading chart</template>
+      <template v-if="isError">Error while loading chart</template>
+      <UiLoading v-else-if="isPending || hasNextPage" />
     </div>
-    <UiChartTime
-      v-else-if="areDataReady"
-      class="flex-1"
-      :series="chartSeries"
-    />
+    <UiChartTime v-else class="flex-1" :series="chartSeries" />
     <div class="space-x-2">
       <button
         v-for="(targetOffset, label) in TIME_OFFSET"
