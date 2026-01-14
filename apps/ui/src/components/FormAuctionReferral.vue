5021e95eb4fed8360fb492da1e1220f10690ade2
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2026-01-14 16:48:19 +0100

    fix: check loading state for userRefereesOrders
    This query is not always enabled so we want to only show loading
    when it's loading, not where it's pending.

--- apps/ui/src/components/FormAuctionReferral.vue
+++ apps/ui/src/components/FormAuctionReferral.vue
@@ -63,7 +63,7 @@ const {
 const {
   data: userRefereesOrders,
   isError: isUserRefereesOrdersError,
-  isPending: isUserRefereesOrdersPending
+  isLoading: isUserRefereesOrdersLoading
 } = useBidsSummaryQuery({
   network: () => props.network,
   auction: () => props.auction,
@@ -223,7 +223,7 @@ async function handleConfirmed() {
       <div v-if="web3Account && userReferees">
         <h4 class="py-2">Your rewards</h4>
         <UiLoading
-          v-if="isUserRefereesPending || isUserRefereesOrdersPending"
+          v-if="isUserRefereesPending || isUserRefereesOrdersLoading"
           class="py-3 block"
         />
         <UiStateWarning
