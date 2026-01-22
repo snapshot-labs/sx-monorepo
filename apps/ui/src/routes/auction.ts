fb2e1c55fcf80904fc98105f748798bb8a1e3620
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2026-01-22 06:25:13 +0100

    feat(auction): move verification to separate route (#1863)
    * feat(auction): move verification to separate route
    
    * fix: extract Loading indicator to VerifyWithProvider
    
    Before Sumsub had loading indicator but ZKPassport didn't.
    
    Now they don't include loading indicator and it's provided externally.
    
    * refactor: remove /verify-$PROVIDER routes
    
    Now everything is under /verify route

--- apps/ui/src/routes/auction.ts
+++ apps/ui/src/routes/auction.ts
@@ -1,4 +1,5 @@
 import AuctionOverview from '@/views/Auction/Overview.vue';
+import AuctionVerify from '@/views/Auction/Verify.vue';
 import Auction from '@/views/Auction.vue';
 import Auctions from '@/views/Auctions.vue';
 
@@ -13,7 +14,8 @@ export default [
     name: 'auction',
     component: Auction,
     children: [
-      { path: '', name: 'auction-overview', component: AuctionOverview }
+      { path: '', name: 'auction-overview', component: AuctionOverview },
+      { path: 'verify', name: 'auction-verify', component: AuctionVerify }
     ]
   }
 ];
