f65d18604db1a30c177827587b9cc2e8758984d3
Chaitanya <yourchaitu@gmail.com>
2026-02-02 00:22:09 +0530

    fix: show error if Safe network is not same as space network (#1790)
    * fix: show error if Safe network is not same as space network
    
    * update messages to include network name

--- apps/ui/src/views/Space/Editor.vue
+++ apps/ui/src/views/Space/Editor.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { sanitizeUrl } from '@braintree/sanitize-url';
+import networks from '@snapshot-labs/snapshot.js/src/networks.json';
 import { useQueryClient } from '@tanstack/vue-query';
 import { LocationQueryValue } from 'vue-router';
 import { StrategyWithTreasury } from '@/composables/useTreasuries';
@@ -59,6 +60,10 @@ const { alerts } = useSpaceAlerts(toRef(props, 'space'), {
   isEditor: true
 });
 const { isController, isAdmin } = useSpaceSettings(toRef(props, 'space'));
+const { isInvalidNetwork: isSafeInvalidNetwork } = useSafeWallet(
+  props.space.network,
+  props.space.snapshot_chain_id
+);
 
 const modalOpen = ref(false);
 const modalOpenTerms = ref(false);
@@ -252,7 +257,8 @@ const canSubmit = computed(() => {
     hasUnsupportedNetworks ||
     hasFormErrors ||
     disabledStrategiesList.value.length ||
-    unsupportedPremiumStrategiesList.value.length
+    unsupportedPremiumStrategiesList.value.length ||
+    isSafeInvalidNetwork.value
   ) {
     return false;
   }
@@ -646,6 +652,15 @@ watchEffect(() => {
               Learn more
               <IH-arrow-sm-right class="-rotate-45" /> </AppLink
             >.
+          </UiAlert>
+          <UiAlert
+            v-else-if="space.snapshot_chain_id && isSafeInvalidNetwork"
+            type="error"
+            class="mb-4"
+          >
+            Please use a Safe on
+            {{ networks[space.snapshot_chain_id]?.name ?? 'this network' }} to
+            create proposals.
           </UiAlert>
           <template v-else>
             <template v-if="proposalLimitReached">
