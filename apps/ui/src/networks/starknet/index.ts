fa5ae323554cef42600a6d2eb31c95c8787d8eb4
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2026-02-25 16:04:59 +0100

    refactor: extract metadata to separate file to avoid cycles

--- apps/ui/src/networks/starknet/index.ts
+++ apps/ui/src/networks/starknet/index.ts
@@ -1,5 +1,4 @@
-import { LibraryError, constants as starknetConstants } from 'starknet';
-import { API_TESTNET_URL, API_URL } from '@/helpers/constants';
+import { LibraryError } from 'starknet';
 import { getRelayerInfo } from '@/helpers/mana';
 import { pin } from '@/helpers/pin';
 import { formatAddress } from '@/helpers/utils';
@@ -7,47 +6,11 @@ import { Network } from '@/networks/types';
 import { NetworkID, Space } from '@/types';
 import { createActions } from './actions';
 import { createConstants } from './constants';
+import { METADATA } from './metadata';
 import { createProvider } from './provider';
 import { STARKNET_CONNECTORS } from '../common/constants';
 import { createApi } from '../common/graphqlApi';
 import { awaitIndexedOnApi } from '../common/helpers';
-
-type Metadata = {
-  name: string;
-  chainId: starknetConstants.StarknetChainId;
-  baseChainId: number;
-  baseNetworkId: NetworkID;
-  rpcUrl: string;
-  ethRpcUrl: string;
-  explorerUrl: string;
-  apiUrl: string;
-  avatar: string;
-};
-
-export const METADATA: Partial<Record<NetworkID, Metadata>> = {
-  sn: {
-    name: 'Starknet',
-    chainId: starknetConstants.StarknetChainId.SN_MAIN,
-    baseChainId: 1,
-    baseNetworkId: 'eth',
-    rpcUrl: 'https://rpc.snapshot.org/sn',
-    ethRpcUrl: 'https://rpc.snapshot.org/1',
-    apiUrl: API_URL,
-    explorerUrl: 'https://voyager.online',
-    avatar: 'ipfs://bafkreihbjafyh7eud7r6e5743esaamifcttsvbspfwcrfoc5ykodjdi67m'
-  },
-  'sn-sep': {
-    name: 'Starknet Sepolia',
-    chainId: starknetConstants.StarknetChainId.SN_SEPOLIA,
-    baseChainId: 11155111,
-    baseNetworkId: 'sep',
-    rpcUrl: 'https://rpc.snapshot.org/sn-sep',
-    ethRpcUrl: 'https://rpc.snapshot.org/11155111',
-    apiUrl: API_TESTNET_URL,
-    explorerUrl: 'https://sepolia.voyager.online',
-    avatar: 'ipfs://bafkreihbjafyh7eud7r6e5743esaamifcttsvbspfwcrfoc5ykodjdi67m'
-  }
-};
 
 export function createStarknetNetwork(networkId: NetworkID): Network {
   const metadata = METADATA[networkId];
