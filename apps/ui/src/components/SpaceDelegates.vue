snapshot-labs/sx-monorepo

commit a6d53fdfd83b5feae906d7df4b2303b8bb1b8a9c

feat: add UiStateWarning component (#1664)
This will be used in warning-like states, eg. no results funds, errors.

apps/ui/src/components/SpaceDelegates.vue
@@ -159,13 +159,9 @@ watchEffect(() => setTitle(`Delegates - ${props.space.name}`));
 </script>
 
 <template>
-  <div
-    v-if="!delegation.apiUrl"
-    class="px-4 py-3 flex items-center text-skin-link space-x-2"
-  >
-    <IH-exclamation-circle class="shrink-0" />
-    <span>Invalid delegation settings.</span>
-  </div>
+  <UiStateWarning v-if="!delegation.apiUrl" class="px-4 py-3">
+    Invalid delegation settings.
+  </UiStateWarning>
   <template v-else>
     <div v-if="delegation.contractAddress" class="p-4 space-x-2 flex">
       <UiButton
@@ -274,15 +270,12 @@ watchEffect(() => setTitle(`Delegates - ${props.space.name}`));
           </div>
         </div>
       </div>
-      <div v-else class="flex space-x-2 border-b py-3 mx-4">
-        <IH-exclamation-circle class="shrink-0 mt-[5px]" />
-        <div>
-          You are not delegating your voting power yet.
-          <span v-if="isUpdatableDelegation">
-            If you just delegated, it may take up to 5 minutes to show up.
-          </span>
-        </div>
-      </div>
+      <UiStateWarning v-else class="border-b py-3 mx-4">
+        You are not delegating your voting power yet.
+        <template v-if="true">
+          If you just delegated, it may take up to 5 minutes to show up.
+        </template>
+      </UiStateWarning>
     </div>
 
     <UiSectionHeader label="Delegates" sticky />
@@ -342,19 +335,16 @@ watchEffect(() => setTitle(`Delegates - ${props.space.name}`));
       </div>
       <UiLoading v-if="isPending" class="px-4 py-3 block" />
       <template v-else>
-        <div
+        <UiStateWarning
           v-if="data?.pages.flat().length === 0 || isError"
-          class="px-4 py-3 flex items-center space-x-1"
+          class="px-4 py-3"
         >
-          <IH-exclamation-circle class="shrink-0" />
-          <span v-if="error?.message.includes('Row not found')">
+          <template v-if="error?.message.includes('Row not found')">
             Delegates are being computed, please come back later.
-          </span>
-          <span v-else-if="isError">Failed to load delegates.</span>
-          <span v-else-if="data?.pages.flat().length === 0">
-            There are no delegates.</span
-          >
-        </div>
+          </template>
+          <template v-else-if="isError"> Failed to load delegates. </template>
+          <template v-else> There are no delegates. </template>
+        </UiStateWarning>
         <UiContainerInfiniteScroll
           :loading-more="isFetchingNextPage"
           class="px-4"

apps/ui/src/components/FormSpaceExecution.vue
@@ -71,10 +71,9 @@ function getConfigureUrl(treasury: SpaceMetadataTreasury) {
   <template v-if="isOSnapPluginEnabled && enableOSnap">
     <UiEyebrow class="font-medium mt-3 mb-2">Treasuries</UiEyebrow>
     <UiLoading v-if="isPending" />
-    <div v-else-if="isError" class="flex items-center space-x-2">
-      <IH-exclamation-circle class="inline-block" />
-      <span>Failed to load treasuries.</span>
-    </div>
+    <UiStateWarning v-else-if="isError">
+      Failed to load treasuries.
+    </UiStateWarning>
     <div v-else>
       <div
         v-for="(treasury, i) in treasuries"

apps/ui/src/components/Modal/SelectValidation.vue
@@ -273,13 +273,9 @@ watch(
     </template>
     <div class="p-4 flex flex-col gap-2.5">
       <UiLoading v-if="isLoading" class="m-auto" />
-      <div
-        v-else-if="hasError"
-        class="flex w-full justify-center items-center gap-2 text-skin-text"
-      >
-        <IH-exclamation-circle class="inline-block shrink-0" />
-        <span>Failed to load strategies.</span>
-      </div>
+      <UiStateWarning v-else-if="hasError" class="justify-center">
+        Failed to load strategies.
+      </UiStateWarning>
       <div v-else-if="selectedValidation" class="s-box">
         <UiForm
           v-if="definition"

apps/ui/src/components/Modal/Space.vue
@@ -74,10 +74,9 @@ watch(
             </div>
           </button>
         </UiContainerInfiniteScroll>
-        <div v-else class="flex items-center space-x-2 m-4">
-          <IH-exclamation-circle class="inline-block shrink-0" />
-          <span>No results found for your search</span>
-        </div>
+        <UiStateWarning v-else class="m-4">
+          No results found for your search.
+        </UiStateWarning>
       </template>
     </div>
   </UiModal>

apps/ui/src/components/Modal/Strategies.vue
@@ -75,13 +75,9 @@ watch(
     </template>
     <div class="p-4 flex">
       <UiLoading v-if="isLoading" class="m-auto" />
-      <div
-        v-else-if="hasError"
-        class="flex w-full justify-center items-center gap-2 text-skin-text"
-      >
-        <IH-exclamation-circle class="inline-block shrink-0" />
-        <span>Failed to load strategies.</span>
-      </div>
+      <UiStateWarning v-else-if="hasError" class="justify-center">
+        Failed to load strategies.
+      </UiStateWarning>
       <div v-else class="flex flex-col flex-1 gap-3 overflow-hidden">
         <button
           v-for="strategy in filteredStrategies"

apps/ui/src/components/ProposalsList.vue
@@ -37,13 +37,9 @@ const currentLimit = computed(() => {
   <div>
     <UiSectionHeader v-if="title" :label="title" sticky />
     <UiLoading v-if="loading" class="block px-4 py-3" />
-    <div
-      v-else-if="isError"
-      class="px-4 py-3 flex items-center text-skin-link gap-2"
-    >
-      <IH-exclamation-circle />
-      <span v-text="'Failed to load proposals.'" />
-    </div>
+    <UiStateWarning v-else-if="isError" class="px-4 py-3">
+      Failed to load proposals.
+    </UiStateWarning>
     <div v-else>
       <UiContainerInfiniteScroll
         :loading-more="loadingMore"
@@ -57,13 +53,9 @@ const currentLimit = computed(() => {
           :show-author="showAuthor"
         />
       </UiContainerInfiniteScroll>
-      <div
-        v-if="!proposals.length"
-        class="px-4 py-3 flex items-center text-skin-link gap-2"
-      >
-        <IH-exclamation-circle />
-        <span v-text="'There are no proposals here.'" />
-      </div>
+      <UiStateWarning v-if="!proposals.length" class="px-4 py-3">
+        There are no proposals here.
+      </UiStateWarning>
       <AppLink
         v-else-if="route && proposals.length > currentLimit"
         :to="{ name: route.name }"

apps/ui/src/components/RelayerBalance.vue
@@ -47,13 +47,9 @@ const {
       <div v-if="isPending" class="flex flex-col">
         <UiLoading class="text-skin-text" :size="16" :loading="true" />
       </div>
-      <div
-        v-else-if="isError || !relayerInfo"
-        class="flex items-center text-skin-link space-x-2"
-      >
-        <IH-exclamation-circle class="inline-block shrink-0" />
-        <span>Failed to load relayer balance.</span>
-      </div>
+      <UiStateWarning v-else-if="isError || !relayerInfo">
+        Failed to load relayer balance.
+      </UiStateWarning>
       <template v-else>
         <div class="flex flex-col">
           <a

apps/ui/src/components/SpaceTreasury.vue
@@ -131,10 +131,9 @@ watchEffect(() => setTitle(`Treasury - ${props.space.name}`));
 </script>
 
 <template>
-  <div v-if="!treasury" class="p-4 flex items-center text-skin-link space-x-2">
-    <IH-exclamation-circle class="inline-block shrink-0" />
-    <span>No treasury configured.</span>
-  </div>
+  <UiStateWarning v-if="!treasury" class="p-4">
+    No treasury configured.
+  </UiStateWarning>
   <template v-else>
     <div class="p-4 space-x-2 flex">
       <div class="flex-auto" />
@@ -255,32 +254,26 @@ watchEffect(() => setTitle(`Treasury - ${props.space.name}`));
             <UiLabel :is-active="page === 'nfts'" text="NFTs" />
           </AppLink>
         </div>
-        <div
+        <UiStateWarning
           v-if="
             (page === 'tokens' && !treasury.supportsTokens) ||
             (page === 'nfts' && !treasury.supportsNfts)
           "
-          class="p-4 flex items-center text-skin-link space-x-2"
+          class="p-4"
         >
-          <IH-exclamation-circle class="inline-block shrink-0" />
-          <span>This treasury network is not supported.</span>
-        </div>
+          This treasury network is not supported.
+        </UiStateWarning>
         <div v-else-if="page === 'tokens'">
           <UiLoading v-if="isPending" class="px-4 py-3 block" />
-          <div
+          <UiStateWarning
             v-else-if="isSuccess && assets.length === 0"
-            class="px-4 py-3 flex items-center text-skin-link space-x-2"
+            class="px-4 py-3"
           >
-            <IH-exclamation-circle class="inline-block shrink-0" />
-            <span>There are no tokens in treasury.</span>
-          </div>
-          <div
-            v-else-if="isError"
-            class="px-4 py-3 flex items-center text-skin-link space-x-2"
-          >
-            <IH-exclamation-circle class="inline-block shrink-0" />
-            <span>Failed to load treasury tokens.</span>
-          </div>
+            There are no tokens in treasury.
+          </UiStateWarning>
+          <UiStateWarning v-else-if="isError" class="px-4 py-3">
+            Failed to load treasury tokens.
+          </UiStateWarning>
           <a
             v-for="(asset, i) in assets"
             v-else
@@ -372,20 +365,15 @@ watchEffect(() => setTitle(`Treasury - ${props.space.name}`));
           </a>
         </div>
         <div v-else-if="page === 'nfts'">
-          <div
+          <UiStateWarning
             v-if="isNftsSuccess && nfts.length === 0"
-            class="px-4 py-3 flex items-center text-skin-link space-x-2"
+            class="px-4 py-3"
           >
-            <IH-exclamation-circle class="inline-block shrink-0" />
-            <span>There are no NFTs in treasury.</span>
-          </div>
-          <div
-            v-else-if="isNftsError"
-            class="px-4 py-3 flex items-center text-skin-link space-x-2"
-          >
-            <IH-exclamation-circle class="inline-block shrink-0" />
-            <span>Failed to load treasury NFTs.</span>
-          </div>
+            There are no NFTs in treasury.
+          </UiStateWarning>
+          <UiStateWarning v-else-if="isNftsError" class="px-4 py-3">
+            Failed to load treasury NFTs.
+          </UiStateWarning>
           <UiLoading v-if="isNftsPending" class="px-4 py-3 block" />
           <div
             class="grid grid-cols-1 minimum:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7 3xl:grid-cols-9 gap-4 gap-y-2 max-w-fit mx-auto p-4"

commit e495971f6d22b9927081dec6deefa3a4d965a7a2

fix: restore isUpdatable check for delegation message
Regression from: https://github.com/snapshot-labs/sx-monorepo/pull/1664

apps/ui/src/components/SpaceDelegates.vue
@@ -267,7 +267,7 @@ watchEffect(() => setTitle(`Delegates - ${props.space.name}`));
       </div>
       <UiStateWarning v-else class="border-b py-3 mx-4">
         You are not delegating your voting power yet.
-        <template v-if="true">
+        <template v-if="isUpdatableDelegation">
           If you just delegated, it may take up to 5 minutes to show up.
         </template>
       </UiStateWarning>