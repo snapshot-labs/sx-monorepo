c93561efd03763cc241ca07c0fb902d960037734
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2026-03-30 19:40:04 +0900

    chore: enforce separate type imports in tune for clean built output

--- packages/tune/src/components/Ui/SelectorCard.vue
+++ packages/tune/src/components/Ui/SelectorCard.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
 import { type Component } from 'vue';
 import IHCode from '~icons/heroicons-outline/code';
 
