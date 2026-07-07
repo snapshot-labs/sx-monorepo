8546d64709df8e84791f3ed851eca0a0d44f2017
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2026-07-07 11:25:00 +0200

    feat(api): always run all protocol indexers (#2214)
    Removing `ENABLE_SNAPSHOT_X`, `ENABLE_GOVERNOR_BRAVO` and
    `ENABLE_OPENZEPPELIN` so all protocols are ran unconditionally.
    
    Those protocols are now officially supported so it makes sense to run
    them all at once and avoid extra configuration (whose naming was
    confusing).

--- apps/api/src/indexer.ts
+++ apps/api/src/indexer.ts
@@ -1,11 +1,6 @@
 import 'dotenv/config';
 import Checkpoint from '@snapshot-labs/checkpoint';
-import {
-  addEvmIndexers,
-  ENABLE_GOVERNOR_BRAVO,
-  ENABLE_OPENZEPPELIN,
-  ENABLE_SNAPSHOT_X
-} from './evm';
+import { addEvmIndexers } from './evm';
 import { addStarknetIndexers } from './starknet';
 
 const GIT_COMMIT = process.env.GIT_COMMIT;
@@ -14,9 +9,7 @@ export async function startIndexer(checkpoint: Checkpoint) {
   addStarknetIndexers(checkpoint);
   addEvmIndexers(checkpoint);
 
-  const versionTag = GIT_COMMIT
-    ? `commit:${GIT_COMMIT}|snapshotX:${ENABLE_SNAPSHOT_X}|governorBravo:${ENABLE_GOVERNOR_BRAVO}|openZeppelin:${ENABLE_OPENZEPPELIN}`
-    : undefined;
+  const versionTag = GIT_COMMIT ? `commit:${GIT_COMMIT}` : undefined;
 
   await checkpoint.syncVersion(versionTag);
 
